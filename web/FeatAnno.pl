c1cf8ceb1bcf59165b4c62a7a6b09ede56574e6b
Eric Lyons <lyonsden@gmail.com>
2006-05-23 22:58:50 +0000

    tightened code

--- web/FeatAnno.pl
+++ web/FeatAnno.pl
@@ -15,17 +15,16 @@ $DEBUG = 0;
 $TEMPDIR = "/opt/apache/CoGe/tmp";
 $TEMPURL = "/CoGe/tmp";
 $| = 1; # turn off buffering
-$DATE = sprintf( "%04d-%02d-%02d %02d:%02d:%02d",
-		 sub { ($_[5]+1900, $_[4]+1, $_[3]),$_[2],$_[1],$_[0] }->(localtime));
 
 $FORM = new CGI;
 $FID = $FORM->param('fid');
 $DI = $FORM->param('di');# || 61;
 $CHR = $FORM->param('chr');# || 7;
 $LOC = $FORM->param('loc') || $FORM->param('pos') || $FORM->param('x');# || 6049802;
-$USER = CoGe::Accessory::LogUser->get_user();
 $DB = new CoGe::Genome;
-gen_html(featid=>$FID, loc=>$LOC, chr=>$CHR, di=>$DI);
+print "Content-Type: text/html\n\n";
+print gen_html(featid=>$FID, loc=>$LOC, chr=>$CHR, di=>$DI);
+
 
 sub gen_html
   {
@@ -34,25 +33,32 @@ sub gen_html
     my $loc = $args{loc};
     my $chr = $args{chr};
     my $di = $args{di};
+    my ($dio) = $DB->get_data_info_obj->search({data_information_id=>$di});
     my @feats;
-    push @feats, $DB->get_feat_obj->get_features_in_region(info_id => $di, 
-							   chr => $chr,
-							   start => $loc,
-							   stop => $loc,
-							  ) if ($di && $chr && $loc);
+    foreach my $tdio ($dio->get_associated_data_infos)
+      {
+	push @feats, $DB->get_feat_obj->get_features_in_region(info_id => $tdio->id, 
+							       chr => $chr,
+							       start => $loc,
+							       stop => $loc,
+							      ) if ($chr && $loc);
+      }
     push @feats, $DB->get_feat_obj->search(feature_id=>$featid) if $featid;
+    my $html;
     foreach my $feat (@feats)
       {
-	print qq{<font class="annotation">Organism</font>};
-	print qq{<li>};
-	print $feat->data_info->org->name,"\n";
-	print qq{<br>};
-	print qq{<font class="annotation">Type</font>};
-	print qq{<li>};
-	print $feat->feat_type->name,"\n";
-	print qq{<br>};
+	$html .= $feat->annotation_pretty_print_html();
+	$html .= qq{<font class="annotation">Organism</font>};
+	$html .= qq{<li>};
+	$html .= $feat->data_info->org->name."\n";
+	$html .= qq{<br>};
+	$html .= qq{<font class="annotation">Type</font>};
+	$html .= qq{<li>};
+	$html .= $feat->feat_type->name."\n";
+	$html .= qq{<br>};
 	
-	print $feat->annotation_pretty_print_html();
-	print qq{<HR>};
+
+	$html .= qq{<HR>};
       }
+    return $html;
   }
