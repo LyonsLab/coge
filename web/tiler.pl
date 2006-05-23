810e2256afc33b5644cd4d3716d2201c0fba33c2
Eric Lyons <lyonsden@gmail.com>
2006-05-23 22:58:25 +0000

    new version of tiler

--- web/tiler.pl
+++ web/tiler.pl
@@ -2,11 +2,10 @@
 
 use strict;
 use CoGe::Accessory::Tile::Cache;
-use CGI;
-use Data::Dumper;
+use CGI qw/:standard/;
+
 
 my $form = new CGI;
-my $img_generator_url = 'http://biocon.berkeley.edu/CoGe/GenomePNG.pl?';
 my $x = $form->param('x') || $form->param('start');
 my $z = $form->param('z');
 my $iw = $form->param('iw') || $form->param('width') || $form->param('tile size')|| $form->param('tile_size');
@@ -14,7 +13,6 @@ my $di = $form->param('di');
 my $chr = $form->param('chr');
 my $org_id = $form->param('o') || $form->param('org') ||$form->param('organism') || $form->param('org_id') || $form->param('oid');
 my $version = $form->param('v') || $form->param('version');
-my $normalize = $form->param('normalize');
 my $cache_file        = "/opt/apache/CoGe/cache/tile";
 
 $cache_file .= ".iw";
@@ -30,33 +28,33 @@ $cache_file .= $org_id if $org_id;
 $cache_file .= ".v";
 $cache_file .= $version if $version;
 
-my $base_units_per_pixel = 10/ $iw;
 
-my $cache_object = CoGe::Accessory::Tile::Cache->new(
-      $img_generator_url,$base_units_per_pixel,$cache_file
-    );
+my $cache_object = CoGe::Accessory::Tile::Cache->new( $cache_file);
 
 # any parameters that should not affect the url used to generate the
 # image are accessorized. all other parameters should go to the
 # anonymous hash in get_tile()
 $cache_object->DEBUG(1);
 
+
+#$cache_object->delete_config();
+
+
 # to turn on the indexing by tile so &x=20
 # finds the 20th tile
 # see Tile::Cache for other accessors
-$cache_object->normalize($normalize || 1);
+$cache_object->normalize(0);
 
-print "Content-type: image/png\n\n";
-
+$cache_object->force_retile(param('force') || 0);
 
 # only parameters that are required to generate
 # the image should go here
-
 my %opts;
 $opts{chr}   = $chr     if $chr;
 $opts{di}    = $di      if $di;
 $opts{org_id}= $org_id  if $org_id;
 $opts{v}     = $version if $version;
+print "Content-type: " . $cache_object->config()->{IMG_TYPE} ."\n\n";
 print $cache_object->get_tile({
 			       'x'         => $x,  # required
 			       'z'         => $z,  # required
