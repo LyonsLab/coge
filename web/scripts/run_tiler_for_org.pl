92745c9be8ac57d29ed26087659f8dc98b720d9a
Eric Lyons <lyonsden@gmail.com>
2006-05-24 17:25:18 +0000

    added zoom option

--- web/scripts/run_tiler_for_org.pl
+++ web/scripts/run_tiler_for_org.pl
@@ -5,23 +5,30 @@ use Data::Dumper;
 use Getopt::Long;
 use CoGe::Genome;
 use LWP::Simple;
+use POSIX;
 
-my ($org_id, $version);
+my ($org_id, $version, $zoom);
 
 GetOptions("oid=s"=>\$org_id,
 	   "v=s" => \$version,
+	   "z|zoom=s" => \$zoom,
 	   );
+
 
 my $db = new CoGe::Genome;
 my ($org) = $db->get_org_obj->search(organism_id=>$org_id);
+my $chars = 10 * 2^$zoom;
 foreach my $di ($org->data_information)
   {
     next if $version && $di->version ne $version;
     my $go = $di->genomic_sequences->next;
     my $chr_len = $db->get_genomic_sequence_obj->get_last_position($di);
-    
-    foreach (my $i=0; $i<= $chr_len; $i+=1280)
+    my $tot = ceil ($chr_len/$chars);
+#    print "Total number of images to be generated: ", $chr_len/1280,"\n";
+    my $count = 0;
+    foreach (my $i=0; $i<= $chr_len; $i+=$chars)
       {
+	$count++;
 	my $cmd = "http://toxic.berkeley.edu/CoGe/tiler.pl?";
 	$cmd .= "&x=$i";
 	$cmd .= "&z=7";
@@ -32,6 +39,7 @@ foreach my $di ($org->data_information)
 #	$cmd .= " v=$version ";
 #	$cmd .= " | /dev/null";
 	print "getting $cmd\n";
+	print "\timage $count / $tot\n";
 	get("$cmd");
       }
   }
