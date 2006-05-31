2c9584558ee48f358e92903d18976275bdafe099
Eric Lyons <lyonsden@gmail.com>
2006-05-31 02:01:37 +0000

    extensive additions for a front page that allows one to navigate through datasets and organisms

--- web/GeLo.pl
+++ web/GeLo.pl
@@ -5,10 +5,12 @@ use CGI::Carp 'fatalsToBrowser';
 use CoGe::Accessory::LogUser;
 use HTML::Template;
 use Data::Dumper;
+use CGI::Ajax;
+use CoGe::Genome;
 
 $ENV{PATH} = "/opt/apache2/CoGe/";
 
-use vars qw( $DATE $DEBUG $TEMPDIR $TEMPURL $USER $FORM $NEATO $DOT);
+use vars qw( $DATE $DEBUG $TEMPDIR $TEMPURL $USER $FORM $NEATO $DOT $DB);
 
 # set this to 1 to print verbose messages to logs
 $DEBUG = 0;
@@ -22,13 +24,22 @@ $DATE = sprintf( "%04d-%02d-%02d %02d:%02d:%02d",
 
 $FORM = new CGI;
 $USER = CoGe::Accessory::LogUser->get_user();
-print "Content-Type: text/html\n\n";
-print gen_html($FORM);
+$DB = new CoGe::Genome;
+
+my $pj = new CGI::Ajax(
+		       get_data_info => \&get_data_info,
+		       get_data_info_info => \&get_data_info_info,
+		       gen_data => \&gen_data,
+		      );
+$pj->JSDEBUG(0);
+$pj->DEBUG(0);
+print $pj->build_html($FORM, \&gen_html);
+#print "Content-Type: text/html\n\n";
+#print gen_html($FORM);
 
 sub gen_html
   {
-    my $form = shift;
-    my ($body, $seq_names, $seqs) = gen_body($form);
+    my ($body, $seq_names, $seqs) = gen_body();
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/generic_page.tmpl');
 
     $template->param(TITLE=>'CoGe: Genome Location Viewer');
@@ -46,23 +57,104 @@ sub gen_html
     $template->param(DATE=>$DATE);
     $template->param(LOGO_PNG=>"GeLo-logo.png");
     $template->param(BODY=>$body);
-    my $html;# =  "Content-Type: text/html\n\n";
+    $template->param(HEAD=>'<SCRIPT language="JavaScript" type="text/javascript" src="./js/kaj.stable.js"></SCRIPT>');
+    my $html;
     $html .= $template->output;
     return $html;
   }
 
 sub gen_body
   {
-    my $form = shift;
+    my $form = shift || $FORM;
     my $chr = $form->param('chr');# || 1;
     my $di = $form->param('di');# || 6;
     my $z = $form->param('z');# || 7;
     my $x = $form->param('x');# || 1;
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/GeLo.tmpl');
-    $template->param(FRONT_PAGE => 1) unless ($chr && $di);
-    $template->param(CHR=>$chr);
-    $template->param(DI=>$di);
-    $template->param(Z=>$z);
-    $template->param(X=>$x);
+    if ($chr && $di)
+      {
+	$template->param(CHR=>$chr);
+	$template->param(DI=>$di);
+	$template->param(Z=>$z);
+	$template->param(X=>$x);
+      }
+    else
+      {
+	$template->param(FRONT_PAGE => 1);
+	$template->param(ORG_LIST=>get_orgs());
+      }
     return $template->output;
   }
+
+sub get_orgs
+  {
+    my $oid = shift;
+    my @opts = sort map {"<OPTION value=\"$_->id\">".$_->name."</OPTION>"} $DB->get_org_obj->retrieve_all();
+    my $html;
+    $html .= qq{<FONT CLASS ="small">Organism count: }.scalar @opts.qq{</FONT>\n<BR>\n};
+    $html .= qq{<SELECT id="org_id" SIZE="5" MULTIPLE onChange="dataset_chain()" >\n};
+    $html .= join ("\n", @opts);
+    $html .= "\n</SELECT>\n";
+    $html =~ s/OPTION/OPTION SELECTED/;
+    return $html;
+  }
+
+sub get_data_info
+  {
+    my $oid = shift;
+    return unless $oid;
+    my $org = $DB->get_org_obj->retrieve($oid);
+    return unless $org;
+    my @opts = map {"<OPTION value=\"".$_->id."\">".$_->name. " (v".$_->version.")</OPTION>"} sort {$b->version cmp $a->version || $a->name cmp $b->name} $org->data_info;
+    my $html;
+    $html .= qq{<FONT CLASS ="small">Dataset count: }.scalar @opts.qq{</FONT>\n<BR>\n};
+    $html .= qq{<SELECT id="di_id" SIZE="5" MULTIPLE onChange="gen_data(['args__loading. . .'],['di_info']); get_data_info_info(['di_id'],['di_info'])" >\n};
+    $html .= join ("\n", @opts);
+    $html .= "\n</SELECT>\n";
+    $html =~ s/OPTION/OPTION SELECTED/;
+    return $html;
+  }
+
+sub get_data_info_info
+  {
+    my $did = shift;
+    return unless $did;
+    
+    my $di = $DB->get_data_info_obj->retrieve($did);
+    return unless $di;
+    my $html .= "<table>";;
+    my $ds = $di->data_source->name ." ". $di->data_source->description;
+    my $link = $di->data_source->link;
+    $link = "http://".$link if ($link && $link !~ /http/);
+    $ds = "<a href =\"".$link."\">".$ds."</a>" if $di->data_source->link;
+    $html .= qq{<TR><TD>Data Source:<TD>$ds}."\n";
+    $html .= qq{<tr><td>Version:<td>}.$di->version."\n";
+    my @chr = $DB->get_genomic_seq_obj->get_chromosome_for_data_information($di);
+    push @chr, "no genomic sequence" unless @chr;
+    $html .= qq{<tr><td>Chromosome};
+    $html .= "s" if scalar @chr > 1;
+    $html .= ":";
+    $html .= "<td>".join (" ", sort {$a cmp $b} @chr)."\n";
+    my $length = commify( $DB->get_genomic_seq_obj->get_last_position($di) );
+    $html .= qq{<tr><td>Nucleotides:<td>$length} if defined $length;
+    my $feats = $di->get_feature_type_count;
+    print STDERR Dumper $feats;
+    $html .= qq{<tr><td valign=top>Features:<td><table>};
+    $html .= join ("\n<tr>",map {"<td>$_<td>".$feats->{$_} } sort {$feats->{$b}<=> $feats->{$a}} keys %$feats);
+    $html .= "</table>";
+    return $html;
+  }
+
+sub gen_data
+  {
+    my $message = shift;
+    return qq{<font class="loading">$message. . .</font>};
+  }
+
+sub commify 
+    {
+      my $text = reverse $_[0];
+      $text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+      return scalar reverse $text;
+    }
+1;
