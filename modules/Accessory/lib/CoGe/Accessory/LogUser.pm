14744da32649ef99b824c246638a11050de962f8
Eric Lyons <lyonsden@gmail.com>
2006-07-07 18:49:39 +0000

    added support for locally generated cookies for user login

--- modules/Accessory/lib/CoGe/Accessory/LogUser.pm
+++ modules/Accessory/lib/CoGe/Accessory/LogUser.pm
@@ -2,21 +2,48 @@ package CoGe::Accessory::LogUser;
 
 use strict;
 use CGI::Cookie;
+use Data::Dumper;
+
+use vars qw($cookie_name);
+
+$cookie_name = "CoGe";
 
 sub get_user
   {
+    my $self = shift;
     my %cookies = fetch CGI::Cookie;
-    my $email = "not logged in";
-    if (ref $cookies{"Session"})
+    my ($user, $uid, $session);# = "Public";
+    if (ref $cookies{$cookie_name})
       {
-	my %session = $cookies{"Session"}->value;
-	$email = $session{email} if $session{"email"};
-      } 
-    else
-      {
-	$email = $ENV{'REMOTE_USER'} if $ENV{'REMOTE_USER'};
+	my %session = $cookies{$cookie_name}->value;
+	$user = $session{user_name} if $session{user_name};
+	$uid = $session{uid} if $session{uid};
+	$session = $session{session} if $session{session};
       }
-    return $email;
+    return ($user, $uid, $session);
   }
+
+sub gen_cookie
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $user_name = $opts{user_name};
+    my $uid = $opts{uid};
+    my $expires = $opts{exp} || "+24h";
+    my $session = $opts{session};
+    $expires = "+".$expires unless $expires =~ /^\+/;
+    
+    my $c = new CGI::Cookie(-name=>$cookie_name,
+			    -expires=>$expires,
+			    -values=>{
+				      user_name=>$user_name,
+				      uid => $uid,
+				      session=>$session,
+				     },
+			    );
+    return $c;
+  }
+
+
 
 1;
