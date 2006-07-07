d4b35d1c5efabab6e845c42c31f9c5dac4f02d2d
Eric Lyons <lyonsden@gmail.com>
2006-07-07 19:12:41 +0000

    extensive modification to add user login

--- web/index.pl
+++ web/index.pl
@@ -3,46 +3,112 @@
 use strict;
 use CGI;
 use CGI::Carp 'fatalsToBrowser';
+use CGI::Ajax;
 use HTML::Template;
 use Data::Dumper;
 use CoGe::Accessory::LogUser;
+use CoGe::Genome;
 
-my $email = CoGe::Accessory::LogUser->get_user();
-my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/index.tmpl');
-my $DATE = sprintf( "%04d-%02d-%02d %02d:%02d:%02d",
+use vars qw($USER $UID $LAST_LOGIN $FORM $DATE);
+
+$FORM = new CGI;
+#if ($FORM->param())
+#  {
+#    my $user = $FORM->param('u');
+#    my $pwd = $FORM->param('p');
+#    my ($valid_user, $cookie) = login(user_name=>$user, pwd=>$pwd);
+#    
+#  }
+($USER, $UID, $LAST_LOGIN) = CoGe::Accessory::LogUser->get_user();
+$DATE = sprintf( "%04d-%02d-%02d %02d:%02d:%02d",
 		 sub { ($_[5]+1900, $_[4]+1, $_[3]),$_[2],$_[1],$_[0] }->(localtime));
-
-$template->param(TITLE=>'Comparative Genomics Homepage');
-$template->param(USER=>$email);
-$template->param(DATE=>$DATE);
-
-my @actions = (
-	       {
-		ACTION=>qq{<a href="./SynView.pl">Synteny Viewer</a>},
-		DESC => qq{Allows uses to compare two sequences using the bl2seq algorithm to find regions of similarity and displays the results graphically.  This program has several unqiue options to aid in the finding of evolutionary conserved non-coding sequences and large syntenic regions.},
-	       },
-	       {
-
-		ACTION=>qq{<a href="./FeatView.pl">Genomic Feature Viewer</a>},
-		DESC => qq{Allows users to find and display information about a genomic feature.  A <font class=oblique>feature</font> in terms of sequence or genomic data denotes some genomic region that has something known about it.  For example, if some genomic region is known to be a gene, and that gene makes an mRNA transcript, and that mRNA is translated into a functional protein, and that protein has several functional domains, it will be likely that there will be several genomic features in that region:  One to denote the gene, one or more to denote the mRNA, and one or more to denote the protein, and one or more to denote the functional domains.  },
-	       },
-	       {
-		ACTION => qq{<a href="./MSAView.pl">Multiple Sequence Alignment Viewer</a>},
-		DESC   => qq{Allows users to submit a multiple sequence alignment in FASTA format (if people would like additional formats, please request via e-mail) in order to quickly check the alignment, find conserved regions, etc.  This program also generates a consensus sequence from the alignment and displays some basic statistics about the alignment.},
-	       },
-	       {
-		ACTION => qq{<a href="./TreeView.pl">Phylogenetic Tree Viewer</a>},
-		DESC   => qq{Allows users to submit a tree file and get a graphical view of their tree.  There is support for drawing rooted and unrooted trees, zooming and unzooming functions, and coloring and shaping nodes based on user specifications.},
-	       },
-	       {
-		ACTION => qq{<a href="./GeLo.pl">Genome Location Viewer</a>},
-		DESC   => qq{Allows users to get an overview of organisms in genomes database and provides a dynamic, interactive browser of genomic information.},
-	       },
-	      );
-$template->param(ACTIONS=>[@actions]);
+my $pj = new CGI::Ajax(
+		       gen_html=>\&gen_html,
+		       login=>\&login,
+		      );
+print $pj->build_html($FORM, \&gen_html);
 
 
 
-my $html =  "Content-Type: text/html\n\n";
-$html .= $template->output;
-print $html;
+
+sub gen_html
+  {
+    my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/generic_page.tmpl');
+    $template->param(TITLE=>'Comparative Genomics Homepage');
+    $template->param(HEAD=>'<SCRIPT language="JavaScript" type="text/javascript" src="./js/kaj.stable.js"></SCRIPT>');
+    $template->param(USER=>$USER);
+    $template->param(DATE=>$DATE);
+    $template->param(BOX_NAME=>"Welcome!");
+    $template->param(LOGO_PNG=>"CoGe-logo.png");
+    $template->param(BODY=>gen_body());
+
+    my $html;
+    $html .= $template->output;
+    return $html;
+  }
+
+sub gen_body
+  {
+    my $tmpl = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/index.tmpl');
+    if ($USER)
+      {
+	$tmpl->param(ACTIONS=>actions());
+      }
+    else
+      {
+	$tmpl->param(LOGIN=>1);
+      }
+    return $tmpl->output;
+  }
+
+sub actions
+  {
+    my @actions = (
+		   {
+		    ACTION=>qq{<a href="./SynView.pl">Synteny Viewer</a>},
+		    DESC => qq{Allows uses to compare two sequences using the bl2seq algorithm to find regions of similarity and displays the results graphically.  This program has several unqiue options to aid in the finding of evolutionary conserved non-coding sequences and large syntenic regions.},
+		   },
+		   {
+		    
+		    ACTION=>qq{<a href="./FeatView.pl">Genomic Feature Viewer</a>},
+		    DESC => qq{Allows users to find and display information about a genomic feature.  A <font class=oblique>feature</font> in terms of sequence or genomic data denotes some genomic region that has something known about it.  For example, if some genomic region is known to be a gene, and that gene makes an mRNA transcript, and that mRNA is translated into a functional protein, and that protein has several functional domains, it will be likely that there will be several genomic features in that region:  One to denote the gene, one or more to denote the mRNA, and one or more to denote the protein, and one or more to denote the functional domains.  },
+		   },
+		   {
+		    ACTION => qq{<a href="./MSAView.pl">Multiple Sequence Alignment Viewer</a>},
+		    DESC   => qq{Allows users to submit a multiple sequence alignment in FASTA format (if people would like additional formats, please request via e-mail) in order to quickly check the alignment, find conserved regions, etc.  This program also generates a consensus sequence from the alignment and displays some basic statistics about the alignment.},
+		   },
+		   {
+		    ACTION => qq{<a href="./TreeView.pl">Phylogenetic Tree Viewer</a>},
+		    DESC   => qq{Allows users to submit a tree file and get a graphical view of their tree.  There is support for drawing rooted and unrooted trees, zooming and unzooming functions, and coloring and shaping nodes based on user specifications.},
+		   },
+		   {
+		    ACTION => qq{<a href="./GeLo.pl">Genome Location Viewer</a>},
+		    DESC   => qq{Allows users to get an overview of organisms in genomes database and provides a dynamic, interactive browser of genomic information.},
+		   },
+		  );
+    return \@actions;
+  }
+
+sub login
+  {
+    my ($name, $pwd) = @_;
+    my $db = new CoGe::Genome;
+    my ($u) = $db->get_user_obj->search(user_name=>$name);
+    my $pwdc = $u->check_passwd(pwd=>$pwd) if $u;
+    if ($pwdc)
+      {
+	my $sid = $db->get_user_session_obj->log_user(user=>$u);
+	my $c = CoGe::Accessory::LogUser->gen_cookie(user_name=>$name, uid=>$u->id, session=>$sid);
+	return (1, $c, $FORM->url() );
+      }
+    elsif ($name =~ /^public$/i)
+      {
+	my $c = CoGe::Accessory::LogUser->gen_cookie(user_name=>$name);
+	return (1, $c, $FORM->url() );
+      }
+    else
+      {
+	return (0,undef, $FORM->url);
+      }
+  }
+
