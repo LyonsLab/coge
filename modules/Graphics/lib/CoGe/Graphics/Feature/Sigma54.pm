7511023c0967689e65f0e9fd186fb0b4c9acb69d
Eric Lyons <lyonsden@gmail.com>
2006-07-11 18:46:13 +0000

    initial import

---
+++ modules/Graphics/lib/CoGe/Graphics/Feature/Sigma54.pm
@@ -0,0 +1,146 @@
+package CoGe::Graphics::Feature::Sigma54;
+use strict;
+use base qw(CoGe::Graphics::Feature);
+
+
+BEGIN {
+    use vars qw($VERSION $HEIGHT $WIDTH);
+    $VERSION     = '0.1';
+    $HEIGHT = 5;
+    $WIDTH = 5;
+    __PACKAGE__->mk_accessors(
+"nt",
+);
+}
+
+sub _initialize
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $h = $HEIGHT; #total image height 
+    my $w = $WIDTH;
+    $self->image_width($w);
+    $self->image_height($h);
+    $self->bgcolor([255,255,255]) unless $self->bgcolor;
+    $self->fill(1);
+    $self->order(1);
+    $self->stop($self->start + length $self->nt-1) unless $self->stop;
+    $self->skip_overlap_search(1); #make sure to skip searching for overlap for these guys.  Search can be slow
+    my $ga = 0;
+    my $ct = 0;
+    my $sum = 0;
+    my $seq = $self->nt;
+    while ($seq=~ /GTGG.A.{5,15}TTGCA/ig)
+      {
+	$sum += length $1;
+	$ga++;
+      }
+    while ($seq =~ /TGCAA.{5,15}T.CCAC/ig)
+      {
+	$sum += length $1;
+	$ct++;
+      }
+
+
+    
+    my $pga = $ga/(length ($self->nt) / 2);
+    my $pct = $ct/(length ($self->nt) / 2);
+    my $p = ($sum)/(length ($self->nt));
+    my @color;
+    my $red = 255;
+    $red -= ($p*200);
+    my $blue = 255;
+    my $green = 255;
+#    $green -= ($pga*250);
+
+    @color = ($blue, $red, $red);
+
+
+    $self->color(\@color);
+    $self->label($self->nt) if $self->nt;
+  }
+
+sub _post_initialize
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $gd = $self->gd;
+    $gd->fill(0,0, $self->get_color($self->color));
+  }
+
+#################### subroutine header begin ####################
+
+=head2 sample_function
+
+ Usage     : How to use this function/method
+ Purpose   : What it does
+ Returns   : What it returns
+ Argument  : What it wants to know
+ Throws    : Exceptions and other anomolies
+ Comment   : This is a sample subroutine header.
+           : It is polite to include more pod and fewer comments.
+
+See Also   : 
+
+=cut
+
+#################### subroutine header end ####################
+
+
+
+
+#################### main pod documentation begin ###################
+## Below is the stub of documentation for your module. 
+## You better edit it!
+
+
+=head1 NAME
+
+CoGe::Graphics::Feature::Base
+
+=head1 SYNOPSIS
+
+  use CoGe::Graphics::Feature::Base
+
+
+=head1 DESCRIPTION
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+	Eric Lyons
+	elyons@nature.berkeley.edu
+
+=head1 COPYRIGHT
+
+This program is free software licensed under the...
+
+	The Artistic License
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
