f098be70d040d3972019d25c97a928b37dd529f3
Eric Lyons <lyonsden@gmail.com>
2006-12-05 18:30:38 +0000

    added type to object

--- modules/Graphics/lib/CoGe/Graphics/Feature/Domain.pm
+++ modules/Graphics/lib/CoGe/Graphics/Feature/Domain.pm
@@ -61,6 +61,7 @@ sub _initialize
     $self->font_size(.5);
     $self->block_height($BLOCK_HEIGHT) unless $self->block_height;
     $self->print_label(0) unless defined $self->print_label();
+    $self->type('domain');
   }
 
 sub _post_initialize
