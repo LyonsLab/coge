4ff2ef8df94889e0bfeee536f23252a2af84a38f
bcthomas <bcthomas@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-02-07 22:24:33 +0000

    Updated Feature object with 3 new methods: protein_sequence(), genome_sequence(), and reverse_comp() [and an alias method, revcomp() which calls reverse_comp() ]

--- modules/Database/lib/CoGeX/Feature.pm
+++ modules/Database/lib/CoGeX/Feature.pm
@@ -27,14 +27,64 @@ __PACKAGE__->has_many( 'annotations' => "CoGeX::Annotation", 'feature_id');
 # feature has many locations
 __PACKAGE__->has_many( 'locations' => "CoGeX::Location", 'feature_id');
 
-# feature has many sequences
+# feature has many sequences - note this is non-genomic sequence (see
+# genomic_sequence() and sequence
 __PACKAGE__->has_many( 'sequences' => "CoGeX::Sequence", 'feature_id');
-
 
 # feature_type has many features
 __PACKAGE__->belongs_to("feature_type" => "CoGeX::FeatureType", 'feature_type_id');
 
 # dataset has many features
 __PACKAGE__->belongs_to("dataset" => "CoGeX::Dataset", 'dataset_id');
+
+sub protein_sequence {
+  my $self = shift;
+  my $siter = $self->sequences();
+  my @sequence_objects;
+  while ( my $seq = $siter->next() ) {
+    push @sequence_objects, $seq;
+  }
+
+  if (@sequence_objects == 1) {
+    return $sequence_objects[0]->sequence_data();
+  } elsif ( @sequence_objects > 1 )  {
+    return \@sequence_objects;
+  } else {
+    return undef;
+  }
+}
+
+sub genome_sequence {
+  my $self = shift;
+  my $dataset = $self->dataset();
+  my @sequences;
+  my $strand = "+";
+  my $lociter = $self->locations();
+  while ( my $loc = $lociter->next() ) {
+    my $fseq = $dataset->get_genome_sequence(
+                                             $loc->chromosome(),
+                                             $loc->start,
+                                             $loc->stop );
+    if ( $loc->strand eq "" or $loc->strand eq "-1" ) {
+      push @sequences, $self->revcomp($fseq);
+    } else {
+      push @sequences, $fseq;
+    }
+  }
+  return wantarray ? @sequences : join( "", @sequences );
+}
+
+sub reverse_comp {
+  my $self = shift;
+  my $seq = shift;
+  my $revseq = uc(reverse $seq);
+  $revseq =~ tr/ATCG/TAGC/;
+  return $revseq;
+}
+
+sub revcomp {
+  my $self = shift;
+  return $self->reverse_comp( @_ );
+}
 
 1;
