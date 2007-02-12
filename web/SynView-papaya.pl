ee4cce5d8abd880d52701171a97a9ef55e2b18bc
Eric Lyons <lyonsden@gmail.com>
2007-02-12 23:41:56 +0000

    initial import

---
+++ web/SynView-papaya.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+print "Content-Type: text/html\n\n";
+
+print qq{
+<head>
+<META HTTP-EQUIV="Refresh" CONTENT="0; URL=./help/SynView%20Papaya/Welcome.html">
+</head>
+};
