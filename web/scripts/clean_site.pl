5a0482bfb9d2685741c7df72b7d113c15f3181dd
Eric Lyons <lyonsden@gmail.com>
2007-02-12 23:42:30 +0000

    initial import

---
+++ web/scripts/clean_site.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $DIR = shift || ".";
+my $new_dir = shift || "new";
+
+mkdir $new_dir unless -d $new_dir;
+mkdir "$new_dir/$DIR" unless -d "$new_dir/$DIR";
+process_dir($DIR);
+
+sub process_dir
+  {
+    my $dir = shift;
+    my @files;
+    opendir (DIR, $dir) || die;
+    while (my $file = readdir(DIR))
+      {
+	next if $file =~ /^\.\.?$/;
+	push @files, "$dir/$file";
+      }
+    closedir DIR;
+    foreach my $file (@files)
+      {
+	if (-d $file)
+	  {
+	    mkdir "$new_dir/$file";
+	    process_dir($file);
+	  }
+	else
+	  {
+	    process_file($file);
+	  }
+      }
+  }
+
+sub process_file
+  {
+    my $file = shift;
+    if ($file =~ /\.html$/)
+      {
+	open (IN, $file) || die "can't open $file for reading";
+	open (OUT, ">$new_dir/$file") || die;
+	while (<IN>)
+	  {
+	    s/�//g;
+	    s/’/'/g;
+	    s/“/"/g;
+	    s/”/"/g;
+	    s/overflow: hidden/overflow: visible/g;
+	    s/http:\/\/\/?CoGe/\/CoGe/ig;
+#	    s/http:\/\/\/?coge/\/CoGe/ig;
+	    print OUT $_;
+	  }
+	close IN;
+	close OUT;
+      }
+    else
+      {
+	`cp "$file" "$new_dir/$file"`;
+      }
+  }
