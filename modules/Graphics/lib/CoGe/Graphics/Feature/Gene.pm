4d982d322c66debfa93759a8eb8b46739b171b86
jkane <jkane@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-03-07 23:02:55 +0000

    Added no_three_D to accessor. Made default lines and arrows in 3d. Made option to switch from 3d back to flat images.

--- modules/Graphics/lib/CoGe/Graphics/Feature/Gene.pm
+++ modules/Graphics/lib/CoGe/Graphics/Feature/Gene.pm
@@ -13,6 +13,7 @@ BEGIN {
 "segments", 
 "print_label", #flag for printing feature label in gene
 "add_type", #flag to set if gene type should be appended to label
+"no_three_D", #switch between flat images and "3D" images, if given value, use flat images
 );
 }
 
@@ -95,9 +96,25 @@ sub _post_initialize
 	my $x1 = $seg->[0] - $s;
 	my $x2 = $seg->[1] - $s;
 	my $y1 = $c-$bh;
-	my $y2 = $c+$bh;;
-	$gd->filledRectangle($x1,$y1, $x2, $y2, $color);
+	my $y2 = $c+$bh;
+	my $gray_lvl = 15 || $opts{gray_lvl};
+	unless ($self->no_three_D)
+	{
+	  my @colors = $gd->rgb($color);
+	  my ($r, $g, $b) = ($colors[0],$colors[1],$colors[2]);
+	  if ($r -75 >= 0)
+	     {$r -= 75;}
+	  if ($g - 75 >= 0)
+	     {$g -= 75;}
+	  if ($b -75 >= 0)
+	     {$b -= 75;}
+	  $self->_make_3d(r=>$r, g=>$g, b=>$b, x1=>$x1, x2=>$x2, y1=>$y1, y2=>$y2, gray_lvl=>$gray_lvl);
+	}
+	else {
+	 $gd->filledRectangle($x1,$y1, $x2, $y2, $color);}
+	#$gd->filledRectangle($x1,$y1, $x2, $y2, $black);
 	$gd->rectangle($x1,$y1, $x2, $y2, $border);
+	#$gd->rectangle($x1,$y1, $x2, $y2, $black);
 	$gd->setStyle($black, $black, $black, GD::gdTransparent, GD::gdTransparent);
 	if ($last)
 	  {
@@ -127,7 +144,9 @@ sub draw_arrow
 #    print STDERR Dumper $seg;
 #    print STDERR $self->start, "-", $self->stop,"\n";
     my $x = $seg->[0] - $self->start; #start of segment
-    my $poly = GD::Polygon->new;
+    
+    
+    my $gray_lvl = 15;
     my $arrow_width = $self->ih*4;
     $arrow_width = $w if $arrow_width > $w;
 #    print STDERR "Arrowhead: X: $x, width: $arrow_width\n";
@@ -135,7 +154,50 @@ sub draw_arrow
     my $gdb = new GD::Image(1,2);
     $gdb->fill(0,0,$gdb->colorResolve(1,1,1));
     $gd->setBrush($gdb);
-    if ($self->strand =~ /-/i)
+    unless ($self->no_three_D) {
+      my $poly1 = GD::Polygon->new;
+      my $poly2 = GD::Polygon->new;
+      my @colors = $gd->rgb($self->get_color($self->color));
+      my ($r, $g, $b) = ($colors[0],$colors[1],$colors[2]);
+      if ($r -75 >= 0)
+	     {$r -= 75;}
+      if ($g - 75 >= 0)
+	     {$g -= 75;}
+      if ($b -75 >= 0)
+	     {$b -= 75;}
+      if ($self->strand =~ /-/i)
+      {
+	$self->_make_3d(r=>$r, g=>$g, b=>$b, x1=>$x, x2=>$x+($arrow_width), y1=>0, y2=>$y, gray_lvl=>15);
+	$poly1->addPt($x, 0);
+	$poly1->addPt($x+($arrow_width), 0);
+	$poly1->addPt($x, $c);
+	$poly2->addPt($x, $c);
+	$poly2->addPt($x, $y);
+	$poly2->addPt($x+($arrow_width), $y);
+	$arrow_end = $x+($arrow_width);
+      }
+    else
+      {
+	#$gd->filledRectangle($x+($w-$arrow_width),0, $x+$w, $y, $self->get_color(0,0,0));
+	$self->_make_3d(r=>$r, g=>$g, b=>$b, x1=>$x+($w-$arrow_width), x2=>$x+$w, y1=>0, y2=>$y, gray_lvl=>15);
+	#$poly->addPt($x+($w-$arrow_width), 0);
+	#$poly->addPt($x+($w-$arrow_width), $y);
+	#$poly->addPt($x+($w), $c);
+	$poly1->addPt($x+($w-$arrow_width), 0);
+	$poly1->addPt($x+$w, 0);
+	$poly1->addPt($x+($w), $c);
+	$poly2->addPt($x+($w), $c);
+	$poly2->addPt($x+($w), $y);
+	$poly2->addPt($x+($w-$arrow_width), $y);
+	$arrow_end=2;
+      }
+    #$gd->filledPolygon($poly, $self->get_color($self->color));
+    $gd->filledPolygon($poly1, $self->get_color(255,255,255));
+    $gd->filledPolygon($poly2, $self->get_color(255,255,255));
+    }
+    else {
+      my $poly = GD::Polygon->new;
+      if ($self->strand =~ /-/i)
       {
 	$gd->filledRectangle($x,0, $x+($arrow_width), $y, $self->get_color(255,255,255));
 	$poly->addPt($x+($arrow_width), 0);
@@ -143,15 +205,15 @@ sub draw_arrow
 	$poly->addPt($x, $c);
 	$arrow_end = $x+($arrow_width);
       }
-    else
+      else
       {
-	$gd->filledRectangle($x+($w-$arrow_width),0, $x+$w, $y, $self->get_color(255,255,255));
+	$gd->filledRectangle($x+($w-$arrow_width),0, $x+$w, $y, $self->get_color(0,0,0));
 	$poly->addPt($x+($w-$arrow_width), 0);
 	$poly->addPt($x+($w-$arrow_width), $y);
 	$poly->addPt($x+($w), $c);
 	$arrow_end=2;
       }
-    $gd->filledPolygon($poly, $self->get_color($self->color));
+    
     my @tmp;
     foreach my $c (@{$self->color})
       {
@@ -160,7 +222,8 @@ sub draw_arrow
 	push @tmp, $c;
       }
     $gd->openPolygon($poly, $self->get_color([@tmp]));
-    return $arrow_end;
+    }
+   return $arrow_end;
   }
 
 sub _draw_join
@@ -169,6 +232,42 @@ sub _draw_join
     my $s = shift;
     my $e = shift;
   }
+  
+sub _make_3d
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $gd = $self->gd;
+    my $r = $opts{r};
+    my $g = $opts{g};
+    my $b = $opts{b};
+    my ($x1, $x2, $y1, $y2, $gray_lvl) = ($opts{x1},$opts{x2},$opts{y1},$opts{y2},$opts{gray_lvl} || 15);
+    my $draw_lines = $y1;
+    my $color;
+    while ($draw_lines <= $y2)
+	{
+	  #print STDERR "r: ", $r, ", g: ", $g, ", b: ", $b, "\n";
+	  $color = $self->get_color($r,$g,$b);
+	  $gd->line($x1, $draw_lines, $x2, $draw_lines, $color);
+	  if ($draw_lines / ($y2 - $y1) < 0.34) {
+	    if ($r + $gray_lvl <=255)
+	     {$r += $gray_lvl;}
+	    if ($g + $gray_lvl <=255)
+	     {$g += $gray_lvl;}
+	    if ($b + $gray_lvl <=255)
+	     {$b += $gray_lvl;}
+	    }
+	  else {
+	    if ($r - $gray_lvl >=0)
+	     {$r -= $gray_lvl;}
+	    if ($g - $gray_lvl >=0)
+	     {$g -= $gray_lvl;}
+	    if ($b - $gray_lvl >=0)
+	     {$b -= $gray_lvl;}
+	    }
+	  $draw_lines++;
+	}
+}
 #################### subroutine header begin ####################
 
 =head2 sample_function
