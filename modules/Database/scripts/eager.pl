2b3635363543935e8eb60922e2cb36fe3ea4071e
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-03-12 16:51:24 +0000

    added example script to demonstrate eager loading with esearch.

---
+++ modules/Database/scripts/eager.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+use CoGeX;
+use strict;
+use Data::Dumper;
+$| = 1;
+
+
+my $connstr = 'dbi:mysql:genomes:biocon:3306';
+my $s = CoGeX->connect($connstr, 'cnssys', 'CnS' );
+
+$s->storage->debug(1);
+
+
+my $rs = $s->resultset('Feature')->esearch( { 
+                    'feature_type.name' =>   'gene' ,
+                    'feature_names.name' => ['-and' , 
+                            { '-like' => 'At2g26%'} ,
+                            { '-not_like' => ['%.%'] }
+                        ]
+                    } 
+                    );
+
+#print "got resultset\n";
+while (my $feat = $rs->next()){
+    my $fn = $feat->feature_names;
+    my $type = $feat->feature_type->name;
+    print $type . "\n";
+}
+
