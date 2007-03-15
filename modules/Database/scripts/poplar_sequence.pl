41b03b461a26ae2872d3f353b59163b4d38ac298
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-03-15 21:31:14 +0000

    fixed poplar_sequence script

--- modules/Database/scripts/poplar_sequence.pl
+++ modules/Database/scripts/poplar_sequence.pl
@@ -10,8 +10,9 @@ my $i = 1;
 map { $cmap{$chrs[$_ -1 ]} = ($_ < 10) ? '0' . $_ : $_  } 1..19;
 
 my %FH;
+
 foreach my $chr (values %cmap){
-    open($FH{$chr},">","/tmp/poplar/poplarchr" .$chr . ".fasta");
+    open($FH{$chr},">","/tmp/poplar_/poplarchr" .$chr . ".fasta");
 }
 
 
@@ -26,59 +27,40 @@ my $org = $s->resultset('Dataset')->search(
     { 'organism.name' => {like => "%Poplar%" },
       'version'       => '1.1' 
     },
-    { join => 'organism' }
+    { prefetch => 'organism' }
 );
+
 my $did = $org->next()->dataset_id;
 
 my $rs = $s->resultset('Feature')->search(
         { 
-            'dataset_id' => $did,
+            'dataset.dataset_id' => $did,
             'feature_names.name' =>  {like => '%proteinId%'}, 
         },
         {
-            join => ['feature_names','locations'],
-            prefetch => ['feature_names'],
-            order_by => ['me.feature_id'],
+            'order_by' => 'me.feature_id', 
+            'join' => 'dataset',
+            'prefetch' => ['feature_names']
+        });
 
-        }
-);
 
 my $currentpid = '';
 my $sequence = "";
-my $currentgene = 1;
-
+my $currentgene = 0;
+my $pid = 1;
 
 while( my $feat = $rs->next()){
-    my $name = ($feat->feature_names())[0];
-    $name = $name->name;
-    my ($pid) = $name =~ /(proteinId\s\d+)/;
-    $currentpid ||=$pid;
-    if ($pid eq $currentpid){
-        $sequence .= $feat->genome_sequence();
-        next;
-    }else{
-        my $genenum = "0" x (6 - length($currentgene)) . $currentgene;
-        my $chrnum = $cmap{(($feat->locations())[0])->chromosome};
-        my $fh = $FH{$chrnum};
-        my $gname = "P" . $chrnum . "G" .  $genenum;
-        print $fh ">",$gname , "\n";
-        print $fh $sequence, "\n\n";
-        $sequence = $feat->genome_sequence();
-        $currentpid = $pid;
-        ++$currentgene;
-    }
+    my $name = ($feat->names())[0];
+    $sequence = $feat->genome_sequence();
 
-}
+    my $genenum = "0" x (6 - length($currentgene)) . $currentgene;
+    my $chrnum = $cmap{$feat->chromosome};
 
-while( my $feature = $rs->next()){
-    my $name;
-    #my $name = (grep { $_->name =~ /P\d+CDS/ } $feature->feature_names)[0]->name;
-    map { print $_->name . "\t" } $feature->feature_names ;
-    print "\n";
-    next;
-#    print $name . "\n";
-#    my $chr = substr($name,1,2);
-#    my $fh = $FH{$chr};
-#    print $fh  ">" . $name . "\n";
-#    print $fh $feature->genome_sequence . "\n\n";
+    my $fh = $FH{$chrnum};
+
+    my $gname = "P" . $chrnum . "G" .  $genenum;
+    print $fh ">",$gname , "\n";
+    print $fh $sequence, "\n\n";
+    ++$currentgene;
+
 }
