97303df7f995c194e4dc49d1c81b5de9aa00e238
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-03-19 16:24:42 +0000

    created a dir for poplar related scripts

---
+++ modules/Database/scripts/poplar/new_names_to_old.pl
@@ -0,0 +1,31 @@
+#! /usr/bin/perl -w
+
+use strict;
+use CoGeX;
+use Data::Dumper;
+
+my $connstr = 'dbi:mysql:genomes:biocon:3306';
+my $s = CoGeX->connect($connstr, 'cnssys', 'CnS' );
+
+
+$s->storage->debug(0);
+
+my $rs = $s->resultset('FeatureName')->search(
+            {
+            'description' =>  'poplar_gene_name',
+            },
+        { });
+
+
+my %seen;
+while( my $name = $rs->next()){
+    next if $seen{$name->name};
+    $seen{$name->name} = 1;
+    my $ns = $s->resultset('FeatureName')->search({
+        'feature_id'   => $name->feature_id
+        ,'description' => 'poplar_group'
+       },{});
+    my $oldname = $ns->next();
+    print $name->name . "," . $oldname->name . "\n";
+
+}
