09301587f42f7e1ac6d338db12e04361f6edc82b
jkane <jkane@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-06-01 00:53:12 +0000

    Changed everything. Does lots of stuff now.

--- web/CoGeBlast.pl
+++ web/CoGeBlast.pl
@@ -42,6 +42,8 @@ my $pj = new CGI::Ajax(
 		       check_seq=>\&check_seq,
 		       set_seq=>\&set_seq,
 		       find_radio=>\&find_radio,
+		       get_pretty_print=>\&get_pretty_print,
+		       blast_param=>\&blast_param,
 		       #%ajax,
 			);
 $pj->js_encode_function('escape');
@@ -70,15 +72,12 @@ sub gen_body
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/CoGeBlast.tmpl');
     my $form = $FORM;
     my $featid = $form->param('featid') || 0;
+    my $chr = $form->param('chr') || 0;
     my $upstream = $form->param('upstream') || 0;
     my $downstream = $form->param('downstream') || 0;
-    if ($featid)
-     {
-       #print STDERR "featid: ", $featid, ", up: ", $upstream, ", down: ", $downstream, "\n";
-       my $seq = get_sequence($featid, $upstream, $downstream, 1);
-       $template->param(SEQUENCE=>$seq);
-     }
-
+    my $dsid = $form->param('dsid') || 0;
+    my $feat_name = $form->param('featname');
+    $template->param(JAVASCRIPT=>1);
     $template->param(BLAST_FRONT_PAGE=>1);
     $template->param(TYPE_LOOP=> [{TYPE=>"<OPTION VALUE=0>All</OPTION>"},map {{TYPE=>"<OPTION value=\"".$_->id."\">".$_->name."</OPTION>"}} sort {uc($a->name) cmp uc($b->name)} $DB->all_feature_types]);
     my @orgs;
@@ -87,6 +86,34 @@ sub gen_body
 	push @orgs, $org unless $restricted_orgs{$org->name};
       }
     $template->param(ORG_LOOP=> [{ORG=>"<OPTION VALUE=0>All</OPTION>"},map {{ORG=>"<OPTION value=\"".$_->id."\">".$_->name."</OPTION>"}} sort {uc($a->name) cmp uc($b->name)} @orgs]);
+    #my $param = blast_param('blast_type_n');
+    #print STDERR $param;
+    if ($featid)
+    {
+    	$template->param(FEATID=>$featid);
+    	$template->param(UPSTREAM=>$upstream);
+    	$template->param(DOWNSTREAM=>$downstream);
+    	$template->param(DSID=>$dsid);
+    	$template->param(FEATNAME=>$feat_name);
+    	$template->param(SEQVIEW=>1);
+    	my $seq = get_sequence($feat_name,$featid, $dsid, 0, 1, $upstream, $downstream);
+        $template->param(SEQUENCE=>$seq);
+    }
+    if ($chr)
+    {
+    	$template->param(CHR=>$chr);
+    	$template->param(UPSTREAM=>$upstream);
+    	$template->param(DOWNSTREAM=>$downstream);
+    	$template->param(DSID=>$dsid);
+    	$template->param(SEQVIEW=>2);
+    	my $seq = get_sequence(0,$chr, $dsid, 0, 2, $upstream, $downstream);
+    	$template->param(SEQUENCE=>$seq);
+    }
+    
+    
+    $template->param(USER_NAME=>$USER);
+    #$template->param(DEFAULT_PARAM=>$param);
+    $template->param(REST=>1);
     $template->output;
   }
   
@@ -99,7 +126,7 @@ sub get_types
     my @opts = sort map {"<OPTION>$_</OPTION>"} grep {! $seen{$_}++} map {$_->type->name} $DB->get_features_by_name_and_dataset_id(name=>$accn, id=>$dsid);
 
     $html .= "<font class=small>Type count: ".scalar @opts."</font>\n<BR>\n";
-    $html .= qq{<SELECT id="Type_name" SIZE="5" MULTIPLE onChange="get_sequence(['accn_select','Type_name', 'dsid'],['seq_box'])" >\n};
+    $html .= qq{<SELECT id="Type_name" SIZE="5" MULTIPLE onChange="get_sequence(['accn_select','Type_name', 'dsid'],['seq_box']);get_pretty_print(['accn_select','Type_name', 'dsid'],['pretty_print_anno']);check_seq_for_blast();" >\n};
     $html .= join ("\n", @opts);
     $html .= "\n</SELECT>\n";
     $html =~ s/OPTION/OPTION SELECTED/;
@@ -173,12 +200,25 @@ sub get_sequence
     my $accn = shift;
     my $type = shift;
     my $dsid = shift;
-    my $seqview = shift;
+    my $blast_type = shift || 0;
+    my $seqview = shift || 0;
+    my $upstream = shift || 0;
+    my $downstream = shift || 0;
     my $seq;
     my $featid;
-    my $upstream = 0;
-    my $downstream = 0;
-    #print STDERR "type: ", $type, ", dsid: ", $dsid, ", accn: ", $accn, "\n";
+    #print STDERR "type: ", $type, ", dsid: ", $dsid, ", accn: ", $accn, ", blast_type: ", $blast_type, "\n";
+    my $fasta;
+    if ($seqview == 2)
+    {
+    	$seq = $DB->get_genomic_sequence(start=>$upstream,
+					 stop=>$downstream,
+					 chr=>$type,
+					 dataset_id=>$dsid);
+	$fasta = generate_fasta_without_featid(chr=>$type, dsid=>$dsid, start=>$upstream, stop=>$downstream);
+	$seq = join ("\n", wrap('','',$seq));
+	$seq = ($fasta. $seq);
+	return $seq;
+    }
     unless ($seqview)
     {
      my @feats;
@@ -191,101 +231,108 @@ sub get_sequence
       $featid = $feat->id;
      }
     }
-    my $fasta;
-    #print STDERR "dsid: ", $dsid, "\n";
-    unless ($seqview) 
-    {
-     $fasta = generate_fasta(featid=>$featid, dsid=>$dsid, feat_name=>$accn);
-    }
-    #print STDERR "\nfeatid: ", $featid, ", up: ", $upstream, ", down: ", $downstream, "\n";
-    if ($seqview)
-     {
-      my ($feat) = $DB->get_feat_obj->retrieve($accn);
-      unless (ref($feat) =~ /Feature/i)
-	{
-	 return "Unable to retrieve Feature object for id: $accn $type";
-	}
-      else {	  
-      $seq = $feat->genomic_sequence(upstream=>$type, downstream=>$dsid);
-      $seq = join ("\n", wrap('','',$seq));
-      $seq = ($fasta. $seq);
-     }
-    return $seq;
-    }
     else
-     {
-      my ($feat) = $DB->get_feat_obj->retrieve($featid);
-         unless (ref($feat) =~ /Feature/i)
-	  {
-	    return "Unable to retrieve Feature object for id: $accn $type";
-	  }
-     else {	  
-      $seq = $feat->genomic_sequence(upstream=>$upstream, downstream=>$downstream);
+     {$featid = $type;}
+    #print STDERR "dsid: ", $dsid, "\n";
+    #print STDERR "\nfeatid: ", $featid, ", up: ", $upstream, ", down: ", $downstream, "\n";
+     $fasta = generate_fasta_with_featid(featid=>$featid, dsid=>$dsid, feat_name=>$accn);
+    
+#     if ($seqview)
+#      {
+#       my ($feat) = $DB->get_feat_obj->retrieve($featid);
+#       unless (ref($feat) =~ /Feature/i)
+# 	{
+# 	 return "Unable to retrieve Feature object for id: $accn";
+# 	}
+#       else {	  
+#       $seq = $feat->genomic_sequence(upstream=>$upstream, downstream=>$downstream);
+#       $seq = join ("\n", wrap('','',$seq));
+#       $seq = ($fasta. $seq);
+#      }
+#     return $seq;
+#     }
+#     else
+#      {
+
+    my ($feat) = $DB->get_feat_obj->retrieve($featid);
+    unless (ref($feat) =~ /Feature/i)
+    {
+      return "Unable to retrieve Feature object for id: $accn $type";
+    }
+    else 
+    {	  
+      if ($blast_type eq  "blast_type_p") {
+       ($seq) = $DB->get_protein_sequence_for_feature($feat);
+       $seq = "No sequence available" unless $seq;
+      }
+      else {
+       $seq = $feat->genomic_sequence(upstream=>$upstream, downstream=>$downstream);}
       $seq = join ("\n", wrap('','',$seq));
       $seq = ($fasta. $seq);
-     }
-     return $seq;
     }
+    return $seq;
     #print STDERR "accn select: ",$featid, ", type name: ",$type, ", dsid: ", $dsid, "\n";
   }
   
-sub check_seq
-  {
-    my $blast_type = shift;
-    my $accn = shift;
-    my $dsid = shift;
-    my $type = shift;
-    #print STDERR "type: ", $type, ", dsid: ", $dsid, ", accn: ", $accn, "\n";
-    my $featid;
-    my @feats;
-    foreach my $feat ($DB->get_features_by_name_and_dataset_id(name=>$accn, id=>$dsid))
-      {
-	push @feats, $feat if ($feat->type->name eq $type);
-      }
-    foreach my $feat (@feats) 
-    {
-      $featid = $feat->id;
-    }
-    my ($feat) = $DB->get_feat_obj->retrieve($featid);
-     #print STDERR "featid: ", $featid, "\n";
-    my $seq;
-    my $fasta = generate_fasta(featid=>$featid, dsid=>$dsid, feat_name=>$accn);
-    #print STDERR "blast_type: ", $blast_type, ", blast_p: ", $blast_p, "\n";
-    if ($blast_type == 1) {
-      ($seq) = $DB->get_protein_sequence_for_feature($feat);
-      #print STDERR "AAs: ", $seq, "\n";
-      $seq = "No sequence available" unless $seq;
-    }
-    else {
-      $seq = $feat->genomic_sequence();
-    }
-    $seq = join ("\n", wrap('','',$seq));
-    $seq = ($fasta. $seq);
-    #print STDERR "Seq is: ", $seq, "\n";
-    return $seq;
-  }
+# sub check_seq
+#   {
+#     my $blast_type = shift;
+#     my $accn = shift;
+#     my $dsid = shift;
+#     my $type = shift;
+#     my $seqview
+#     #print STDERR "Blast_type: ", $blast_type, ", Accn : ", $accn, ", dsid: ", $dsid, ", type is: ", $type, "\n";
+#     #print STDERR "Hello, World!\n";
+#     #print STDERR "type is: ", $type, ", dsid: ", $dsid, ", accn: ", $accn, "\n";
+#     my $featid;
+#     my @feats;
+#     foreach my $feat ($DB->get_features_by_name_and_dataset_id(name=>$accn, id=>$dsid))
+#       {
+# 	push @feats, $feat if ($feat->type->name eq $type);
+#       }
+#     foreach my $feat (@feats) 
+#     {
+#       $featid = $feat->id;
+#     }
+#     my ($feat) = $DB->get_feat_obj->retrieve($featid);
+#      #print STDERR "featid: ", $featid, "\n";
+#     my $seq;
+#     my $fasta = generate_fasta(featid=>$featid, dsid=>$dsid, feat_name=>$accn);
+#     #print STDERR "blast_type: ", $blast_type, ", blast_p: ", $blast_p, "\n";
+#     if ($blast_type eq  "blast_type_p") {
+#       ($seq) = $DB->get_protein_sequence_for_feature($feat);
+#       #print STDERR "AAs: ", $seq, "\n";
+#       $seq = "No sequence available" unless $seq;
+#     }
+#     else {
+#       $seq = $feat->genomic_sequence();
+#     }
+#     $seq = join ("\n", wrap('','',$seq));
+#     $seq = ($fasta. $seq);
+#     #print STDERR "Seq is: ", $seq, "\n";
+#     return $seq;
+#   }
 
 sub get_url
   {
     my $url = shift;
-    if ($url eq "megablast") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?ALIGNMENTS=50&ALIGNMENT_VIEW=Pairwise&AUTO_FORMAT=Semiauto&CLIENT=web&DATABASE=Test%2Fgpipe%2F9606%2Fallcontig_and_rna&DESCRIPTIONS=100&ENTREZ_QUERY=All+organisms&EXPECT=10&FILTER=L&FILTER=R&FORMAT_BLOCK_ON_RESPAGE=None&FORMAT_ENTREZ_QUERY=All+organisms&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&GET_SEQUENCE=on&HITLIST_SIZE=100&LAYOUT=TwoWindows&MASK_CHAR=2&MASK_COLOR=1&MEGABLAST=on&MYNCBI_USER=4389532342&NEW_VIEW=on&NUM_OVERVIEW=100&PAGE=MegaBlast&PERC_IDENT=None,+1,+-2&PROGRAM=blastn&REPEATS=repeat_9606&SEARCH_NAME=mb&SERVICE=plain&SET_DEFAULTS=Yes&SET_DEFAULTS.x=47&SET_DEFAULTS.y=12&SHOW_LINKOUT=on&SHOW_OVERVIEW=on&USER_TYPE=2&WORD_SIZE=28&WWW_BLAST_TYPE=mapview&dbtype=hc&END_OF_HTTPGET=Yes&QUERY=";}
-    elsif ($url eq "blastn") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=50&ALIGNMENT_VIEW=Pairwise&CLIENT=web&DESCRIPTIONS=100&ENTREZ_QUERY=%28none%29&EXPECT=10&FILTER=L&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&NCBI_GI=off&PAGE=Nucleotides&PROGRAM=blastn&SERVICE=plain&SET_DEFAULTS.x=34&SET_DEFAULTS.y=8&SHOW_OVERVIEW=on&END_OF_HTTPGET=yes&GET_SEQUENCE=yes&NEW_VIEW=yes&SEARCH_NAME=bn&QUERY=";}
+    my $expect = shift;
+    my $db = shift;
+    #print STDERR "expect: ", $expect, "\n";
+    if ($url eq "blastn") {
+      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?PAGE=Nucleotides&PROGRAM=blastn&MEGABLAST=on&BLAST_PROGRAMS=megaBlast&PAGE_TYPE=BlastSearch&EXPECT=$expect&DATABASE=$db";}
     elsif ($url eq "blastp") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=250&ALIGNMENT_VIEW=Pairwise&CDD_SEARCH=on&CLIENT=web&COMPOSITION_BASED_STATISTICS=on&DATABASE=nr&DESCRIPTIONS=500&ENTREZ_QUERY=%28none%29&EXPECT=10&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&I_THRESH=0.005&MATRIX_NAME=BLOSUM62&NCBI_GI=on&PAGE=Proteins&PROGRAM=blastp&SERVICE=plain&SET_DEFAULTS.x=41&SET_DEFAULTS.y=5&SHOW_OVERVIEW=on&END_OF_HTTPGET=Yes&SHOW_LINKOUT=yes&GET_SEQUENCE=yes&SEARCH_NAME=bp&QUERY=";}
+      return "http://www.ncbi.nlm.nih.gov/BLAST/Blast.cgi?PAGE=Proteins&PROGRAM=blastp&BLAST_PROGRAMS=blastp&PAGE_TYPE=BlastSearch&EXPECT=$expect&DATABASE=$db";}
       elsif ($url eq "blastx") {
-        return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=50&ALIGNMENT_VIEW=Pairwise&CLIENT=web&DATABASE=nr&DESCRIPTIONS=100&ENTREZ_QUERY=%28none%29&EXPECT=10&FILTER=L&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&GENETIC_CODE=1&NCBI_GI=on&PAGE=Translations&PROGRAM=blastx&SERVICE=plain&SET_DEFAULTS.x=37&SET_DEFAULTS.y=5&SHOW_OVERVIEW=on&UNGAPPED_ALIGNMENT=no&END_OF_HTTPGET=Yes&SHOW_LINKOUT=yes&GET_SEQUENCE=yes&SEARCH_NAME=bx&QUERY=";}
+        return "http://www.ncbi.nlm.nih.gov/BLAST/Blast.cgi?PAGE=Translations&PROGRAM=blastx&BLAST_PROGRAMS=blastx&PAGE_TYPE=BlastSearch&EXPECT=$expect&DATABASE=$db";}
     elsif ($url eq "tblastn") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=50&ALIGNMENT_VIEW=Pairwise&CLIENT=web&DATABASE=nr&DESCRIPTIONS=100&ENTREZ_QUERY=%28none%29&EXPECT=10&FILTER=L&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&GENETIC_CODE=0&NCBI_GI=on&PAGE=Translations&PROGRAM=tblastn&SERVICE=plain&SET_DEFAULTS.x=23&SET_DEFAULTS.y=10&SHOW_OVERVIEW=on&UNGAPPED_ALIGNMENT=no&END_OF_HTTPGET=Yes&SHOW_LINKOUT=yes&GET_SEQUENCE=yes&SEARCH_NAME=tbn&QUERY=";}
+      return "http://www.ncbi.nlm.nih.gov/BLAST/Blast.cgi?PAGE=Translations&PROGRAM=tblastn&BLAST_PROGRAMS=tblastn&PAGE_TYPE=BlastSearch&EXPECT=$expect&DATABASE=$db";}
     elsif ($url eq "tblastx") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=50&ALIGNMENT_VIEW=Pairwise&CLIENT=web&DATABASE=nr&DESCRIPTIONS=100&ENTREZ_QUERY=%28none%29&EXPECT=10&FILTER=L&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&GENETIC_CODE=1&NCBI_GI=on&PAGE=Translations&PROGRAM=tblastx&SERVICE=plain&SET_DEFAULTS.x=21&SET_DEFAULTS.y=9&SHOW_OVERVIEW=on&UNGAPPED_ALIGNMENT=yes&END_OF_HTTPGET=Yes&SHOW_LINKOUT=yes&SEARCH_NAME=tbx&QUERY=";}
-    elsif ($url eq "psiblast") {
-      return "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Web&LAYOUT=TwoWindows&AUTO_FORMAT=Semiauto&ALIGNMENTS=250&ALIGNMENT_VIEW=Pairwise&CLIENT=web&COMPOSITION_BASED_STATISTICS=on&DATABASE=nr&CDD_SEARCH=on&DESCRIPTIONS=500&ENTREZ_QUERY=%28none%29&EXPECT=10&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&I_THRESH=0.005&MATRIX_NAME=BLOSUM62&NCBI_GI=on&PAGE=Proteins&PROGRAM=blastp&RUN_PSIBLAST=on&SERVICE=plain&SET_DEFAULTS.x=36&SET_DEFAULTS.y=5&SHOW_OVERVIEW=on&END_OF_HTTPGET=Yes&SHOW_LINKOUT=yes&GET_SEQUENCE=yes&SEARCH_NAME=psi_bp&QUERY=";}
+      return "http://www.ncbi.nlm.nih.gov/BLAST/Blast.cgi?PAGE=Translations&PROGRAM=tblastx&BLAST_PROGRAMS=tblastx&PAGE_TYPE=BlastSearch&EXPECT=$expect&DATABASE=$db";}
     else {
       return 1;}
   }
-  
+#   
 # sub aa_or_nu_seq 
 # {
 #   my $blast = shift;
@@ -303,7 +350,7 @@ sub get_url
 #   return $type;
 # }
 
-sub generate_fasta
+sub generate_fasta_with_featid
   {
     my %opts = @_;
     my $featid = $opts{featid};
@@ -315,6 +362,17 @@ sub generate_fasta
     return $fasta;
   }
   
+sub generate_fasta_without_featid
+  {
+    my %opts = @_;
+    my $chr = $opts{chr};
+    my $dsid = $opts{dsid};
+    my $start = $opts{start};
+    my $stop = $opts{stop};
+    my $ds = $DB->get_dataset_obj->retrieve($dsid);
+    my $fasta = ">".$ds->org->name.", Location: ".$start."-".$stop.", Chromosome: ".$chr."\n";
+    return $fasta;
+  }
 # sub ncbi_blast
 #   {
 #     my $blank = shift;
@@ -329,5 +387,44 @@ sub find_radio
   {
     my %opts = @_;
     my $radio = $opts{'bn'};
+    my $expect = $opts{'expect'};
+    my $db = $opts{'db'};
+    #print STDERR "radio: ",$radio,", expect: ",$expect, ", db: ",$db,"\n";
     return $radio;
   }
+
+sub get_pretty_print
+{
+    my $accn = shift;
+    my $type = shift;
+    my $dsid = shift;
+    my $featid;
+    my @feats;
+    foreach my $feat ($DB->get_features_by_name_and_dataset_id(name=>$accn, id=>$dsid))
+      {
+	push @feats, $feat if ($feat->type->name eq $type);
+      }
+     foreach my $feat (@feats) 
+     {
+      $featid = $feat->id;
+     }
+    my ($feat) = $DB->get_feat_obj->retrieve($featid);
+    unless (ref($feat) =~ /Feature/i)
+    {
+      return "Unable to retrieve Feature object for id: $accn $type";
+    }
+    my $html = $feat->annotation_pretty_print_html();
+    return $html;
+}
+
+sub blast_param
+{
+    my $seq_type = shift || "blast_type_n";
+    my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/CoGeBlast.tmpl');
+    if ($seq_type eq "blast_type_n") {
+        $template->param(BLAST_NU=>1);}
+    else {
+	$template->param(BLAST_PRO=>1);}
+    my $html = $template->output;
+    return $html;
+}
