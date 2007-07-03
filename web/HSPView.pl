4cd97fdd037cd7239f1df7a551baaa056e7fcd42
Eric Lyons <lyonsden@gmail.com>
2007-07-03 02:51:24 +0000

    now using sqlite db for hsp info

--- web/HSPView.pl
+++ web/HSPView.pl
@@ -7,11 +7,12 @@ use GS::LogUser;
 use HTML::Template;
 use Data::Dumper;
 use CoGe::Genome;
-use CoGe::Accessory::bl2seq_report;
-use CoGe::Accessory::blastz_report;
-use CoGe::Accessory::chaos_report;
-use CoGe::Accessory::lagan_report;
-use CoGe::Accessory::dialign_report;
+#use CoGe::Accessory::bl2seq_report;
+#use CoGe::Accessory::blastz_report;
+#use CoGe::Accessory::chaos_report;
+#use CoGe::Accessory::lagan_report;
+#use CoGe::Accessory::dialign_report;
+use DBI;
 
 # for security purposes
 $ENV{PATH} = "/opt/apache2/CoGe/";
@@ -46,20 +47,21 @@ sub gen_html
     my $form = shift || $FORM;
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/generic_page.tmpl');
     $template->param(LOGO_PNG=>"HSPView-logo.png");
-    $template->param(TITLE=>'Blast HSP Viewer');
+    $template->param(TITLE=>'HSP Viewer');
     $template->param(USER=>$USER);
     $template->param(DATE=>$DATE);
-    my $bfile = $form->param('blast_report') || $form->param('report');
-    if ($bfile && -r $bfile)
+    my $report_file = $form->param('blast_report') || $form->param('report');
+    my $dbfile = $form->param('db');
+    if ($report_file && -r $report_file)
       {
 	my $hsp_num = $form->param('hsp_num') || $form->param('num');
-	$bfile =~ /([^\/]*$)/;
+	$report_file =~ /([^\/]*$)/;
         $template->param(BOX_NAME=>qq{<a href=/CoGe/tmp/$1>$1</a>}. " HSP: $hsp_num") if $1;
-        $template->param(BODY=>gen_body($bfile, $hsp_num));
+        $template->param(BODY=>gen_body(report_file=>$report_file, hsp_num=>$hsp_num, db_file=>$dbfile));
       }
     else
       {
-	$template->param(BODY=>"Can't read or find file $bfile");
+	$template->param(BODY=>"Can't read or find file $report_file");
       }
     my $html;
     $html .= $template->output;
@@ -68,43 +70,51 @@ sub gen_html
 
 sub gen_body
   {
-    my $blast_file = shift;
-    my $hsp_num = shift;
-    my $blast;
-    $blast= new CoGe::Accessory::bl2seq_report({file=>$blast_file}) if $blast_file =~ /bl2seq/i;
-    $blast= new CoGe::Accessory::blastz_report({file=>$blast_file}) if $blast_file =~ /blastz/i;
-    $blast= new CoGe::Accessory::lagan_report({file=>$blast_file}) if $blast_file =~ /lagan/i;
-    $blast= new CoGe::Accessory::chaos_report({file=>$blast_file}) if $blast_file =~ /chaos/i;
-    $blast= new CoGe::Accessory::dialgn_report({file=>$blast_file}) if $blast_file =~ /dialign/i;
-#    print STDERR Dumper $blast;
+    my %opts = @_;
+    my $report_file = $opts{report_file};
+    my $db_file = $opts{db_file};
+    my $hsp_num = $opts{hsp_num};
+#    my $report;
+#    $report= new CoGe::Accessory::bl2seq_report({file=>$report_file}) if $report_file =~ /bl2seq/i;
+#    $report= new CoGe::Accessory::blastz_report({file=>$report_file}) if $report_file =~ /blastz/i;
+#    $report= new CoGe::Accessory::lagan_report({file=>$report_file}) if $report_file =~ /lagan/i;
+#    $report= new CoGe::Accessory::chaos_report({file=>$report_file}) if $report_file =~ /chaos/i;
+#    $report= new CoGe::Accessory::dialgn_report({file=>$report_file}) if $report_file =~ /dialign/i;
+#    print STDERR Dumper $report;
+
+    my $hsps = get_info_from_db(db_file=>$db_file, hsp_num=>$hsp_num, report_file=>$report_file);
+    my ($qname, $sname) = $hsps->[0]{hsp}=~ /\((.*?)-(.*?)\)/;
+
     my $hsp;
-    foreach my $item (@{$blast->hsps})
-      {
-	$hsp = $item if $item->number eq $hsp_num;
-      }
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/HSPView.tmpl');
-    $template->param(Query=>$blast->query);
-    $template->param(Subject=>$blast->subject);
-    $template->param(QGap=>$hsp->qgap);
-    $template->param(SGap=>$hsp->sgap);
-    my $qseq = $hsp->qseq;
+    $template->param(Query=>$qname);
+    $template->param(Subject=>$sname);
+    my $qgap = $hsps->[0]{alignment} =~ tr/-/-/;
+    my $sgap = $hsps->[1]{alignment} =~ tr/-/-/;
+
+    $template->param(QGap=>$qgap);
+    $template->param(SGap=>$sgap);
+    my $qseq = $hsps->[0]{alignment};
     $qseq =~ s/-//g;
-    my $sseq = $hsp->sseq;
+    my $sseq = $hsps->[1]{alignment};
     $sseq =~ s/-//g;
-    $template->param(QLoc=>$hsp->qstart."-".$hsp->qstop);
-    $template->param(SLoc=>$hsp->sstart."-".$hsp->sstop);
-    $template->param(QIdent=>sprintf("%.1f",$hsp->match/length($qseq)*100)."%");
-    $template->param(SIdent=>sprintf("%.1f",$hsp->match/length($sseq)*100)."%");
-    $template->param(QSim=>sprintf("%.1f",$hsp->positive/length($qseq)*100)."%");
-    $template->param(SSim=>sprintf("%.1f",$hsp->positive/length($sseq)*100)."%");
+    $template->param(QLoc=>$hsps->[0]{start}."-".$hsps->[0]{stop});
+    $template->param(SLoc=>$hsps->[1]{start}."-".$hsps->[1]{stop});
+    $template->param(QIdent=>sprintf("%.1f",$hsps->[0]{match}/length($qseq)*100)."%");
+    $template->param(SIdent=>sprintf("%.1f",$hsps->[1]{match}/length($sseq)*100)."%");
+    $template->param(QSim=>sprintf("%.1f",$hsps->[0]{match}/length($qseq)*100)."%");
+    $template->param(SSim=>sprintf("%.1f",$hsps->[1]{match}/length($sseq)*100)."%");
+#    $template->param(QSim=>sprintf("%.1f",$hsp->positive/length($qseq)*100)."%");
+#    $template->param(SSim=>sprintf("%.1f",$hsp->positive/length($sseq)*100)."%");
     $template->param(QLen=>length($qseq));
     $template->param(SLen=>length($sseq));
-    $template->param(QMis=>length($qseq)-$hsp->positive);
-    $template->param(SMis=>length($sseq)-$hsp->positive);
-    $template->param(strand=>$hsp->strand);
-    $template->param(eval=>$hsp->eval);
-    $template->param(Match=>$hsp->match);
-    $template->param(Sim=>$hsp->positive);
+    $template->param(QMis=>length($qseq)-$hsps->[0]{match}-$qgap);
+    $template->param(SMis=>length($sseq)-$hsps->[1]{match}-$sgap);
+    $template->param(strand=>$hsps->[0]{orientation});
+    $template->param(eval=>$hsps->[0]{eval});
+    $template->param(Match=>$hsps->[0]{match});
+    $template->param(Sim=>$hsps->[0]{match});
+
     my $qstart = $FORM->param('qstart');
     my $qstop = $FORM->param('qstop');
     my $qchr = $FORM->param('qchr');
@@ -117,18 +127,32 @@ sub gen_body
       
     $template->param(qseq=>$qseq_out);
     $template->param(sseq=>"<pre>".seqwrap($sseq)."</pre>");
-    my @qln = split /\n/,seqwrap($hsp->qseq,100);
-    my @sln = split /\n/,seqwrap($hsp->sseq,100);
-    my @aln = split /\n/,seqwrap($hsp->align,100);
+    my @qln = split /\n/,seqwrap($hsps->[0]{alignment},100);
+    my @sln = split /\n/,seqwrap($hsps->[1]{alignment},100);
+    my @aln = split /\n/,seqwrap(get_alignment($hsps->[0]{alignment},$hsps->[1]{alignment}),100);
+#    my @aln = split /\n/,seqwrap($hsp->align,100);
     my $align;
     for (my $i=0; $i<@aln; $i++)
       {
 	$align .= join ("\n", $qln[$i],$aln[$i],$sln[$i])."\n\n";
       }
     $template->param(align=>"<pre>".$align."</pre>");
+#    $template->param(newstuff=>get_info_from_db(db_file=>$db_file, hsp_num=>$hsp_num, report_file=>$report_file));
     my $html;
     $html .= $template->output;
     return $html;
+  }
+
+sub get_alignment
+  {
+    my @seq1 = split //, shift;
+    my @seq2 = split //, shift;
+    my $aln;
+    for (my $i = 0; $i < @seq1; $i++)
+      {
+	$aln .= $seq1[$i] eq $seq2[$i] ? "|" : " ";
+      }
+    return $aln;
   }
 
 sub seqwrap {
@@ -148,3 +172,44 @@ sub seqwrap {
 	}
 	return($newseq);
 }
+
+
+sub get_info_from_db
+  {
+    my %opts = @_;
+
+    my $db = $opts{db_file};
+    my $hsp_num = $opts{hsp_num};
+    my $report_file = $opts{report_file};
+    my ($base) = $db =~ /^(.*?)\./;
+    my ($set1, $set2) = $report_file=~ /_(\d+)-(\d+)/;
+    my $output;
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$TEMPDIR/$db","","");
+    my $statement = qq{SELECT annotation from image_data where name like "$hsp_num-%" and (image = "$base}."_".qq{$set1.png" or image = "$base}."_".qq{$set2.png")};
+    print STDERR $statement,"\n";
+    my $sth = $dbh->prepare($statement);
+    $sth->execute();
+    my @hsps;
+    while(my $item = $sth->fetchrow_arrayref)
+      {
+	my ($hsp, $location, $alignment, $match, $length, $identity, $eval) = split /<br>/, $item->[0];
+	my ($start, $stop, $orientation) = $location =~ /(\d+)-(\d+)\s+\((.*?)\)/;
+	$match =~ s/match:\s+//i;
+	$length =~ s/length:\s+//i;
+	$identity =~ s/identity:\s+//i;
+	$eval =~ s/e_val:\s+//i;
+	$eval =~ s/<.*?>//g;
+	push @hsps, {
+		     hsp=>$hsp,
+		     start=>$start,
+		     stop=>$stop,
+		     orientation=>$orientation,
+		     alignment=>$alignment,
+		     match=>$match,
+		     length=>$length,
+		     identity=>$identity,
+		     eval=>$eval,
+		    };
+      }
+    return \@hsps;
+  }
