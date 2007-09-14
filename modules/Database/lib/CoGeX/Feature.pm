5dcf527625e1fd821d9dffe5317c6b00e2a97559
Eric Lyons <lyonsden@gmail.com>
2007-09-14 18:28:18 +0000

    Corrected serious bug where genomic_sequence was not assembled correctly.

--- modules/Database/lib/CoGeX/Feature.pm
+++ modules/Database/lib/CoGeX/Feature.pm
@@ -508,19 +508,20 @@ sub genomic_sequence {
   my $self = shift;
   my $dataset = $self->dataset();
   my @sequences;
-  my $lociter = $self->locations();
-  while ( my $loc = $lociter->next() ) {
-    my $fseq = $dataset->get_genome_sequence(
-                                             chromosome=>$loc->chromosome(),
-                                             skip_length_check=>1,
-                                             start=>$loc->start,
-                                             stop=>$loc->stop );
-    if ( $loc->strand == -1 ) {
-      push @sequences, $self->reverse_complement($fseq);
-    } else {
-      push @sequences, $fseq;
+  foreach my $loc (sort {$a->start <=> $b->start} $self->locations())
+    {
+      #  while ( my $loc = $lociter->next() ) {
+      my $fseq = $dataset->get_genome_sequence(
+					       chromosome=>$loc->chromosome(),
+					       skip_length_check=>1,
+					       start=>$loc->start,
+					       stop=>$loc->stop );
+      if ( $loc->strand == -1 ) {
+	push @sequences, $self->reverse_complement($fseq);
+      } else {
+	push @sequences, $fseq;
+      }
     }
-  }
   return wantarray ? @sequences : join( "", @sequences );
 }
 
