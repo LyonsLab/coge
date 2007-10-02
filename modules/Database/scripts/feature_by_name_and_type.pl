d343133a0d02283998a5ec96c217f5b353e7fbc1
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2007-10-02 20:56:33 +0000

    add DBIx::Class as prereq to makefile

--- modules/Database/scripts/feature_by_name_and_type.pl
+++ modules/Database/scripts/feature_by_name_and_type.pl
@@ -5,28 +5,18 @@ use CoGeX;
 my $connstr = 'dbi:mysql:genomes:biocon:3306';
 my $s = CoGeX->connect($connstr, 'cnssys', 'CnS' );
 
-#$s->storage->debug(1);
+$s->storage->debug(1);
 
 
 my $rs = $s->resultset('Feature')->search(
-                    { 
-                    'feature_names.name' => { 'like' => 'At1%' },
-                    'feature_type.name' =>   'CDS' 
-                    },
-                    {
+                    { 'feature_names.name' => { 'like' => 'At1g16%' },
+                    'feature_type.name' =>   'CDS' }, {
                         join => [ 'feature_names','feature_type'] 
-                    },
+                    });
 
-                );
-
-
-my %seen;
 while (my $feat =$rs->next()){
-    my $fn = $feat->feature_names;
-    my $type = $feat->feature_type->name;
-    my @names = grep { ! $seen{$_->name} } $fn->next();
-    map { $seen{$_->name}=1 }  $feat->feature_names->next(); 
-    map { print $_->name  . "\n" } @names;
+    my $s =  $feat->genomic_sequence();
+    print $s . "\n";
 }
    
 
