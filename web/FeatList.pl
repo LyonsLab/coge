918ffdbc2269541c2c8dcba75d5b52c030dc610d
Eric Lyons <lyonsden@gmail.com>
2007-11-09 22:50:52 +0000

    updated handling of featids and files

--- web/FeatList.pl
+++ web/FeatList.pl
@@ -23,11 +23,12 @@ $coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
 
 my $pj = new CGI::Ajax(
 		       gen_html=>\&gen_html,
+		       feats_parse=>\&feats_parse,
 			);
 $pj->js_encode_function('escape');
-#print $pj->build_html($FORM, \&gen_html);
-print $FORM->header;
-print gen_html();
+print $pj->build_html($FORM, \&gen_html);
+#print $FORM->header;
+#print gen_html();
 
 sub gen_html
   {
@@ -55,34 +56,90 @@ sub gen_body
   {
     my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/FeatList.tmpl');
     my $form = $FORM;
+    my $no_values;
     $BASEFILE = $form->param('basename');
-    my $table = generate_table();
-    $template->param(INFO=>$table);
-    $template->output;
+    my $feat_list = [];
+    $feat_list = read_file() if $BASEFILE;#: $opts{feature_list};
+    foreach my $item ($form->param('fid'))
+      {
+	push @$feat_list, $item if $item =~ /^\d+$/;
+      }
+    my $table = generate_table(feature_list=>$feat_list);
+    if ($table)
+      {
+	$template->param(INFO=>$table);
+	return $template->output;
+      }
+    else
+      {
+	return "No feature ids were specified.";
+      }
   }
   
 sub generate_table
   {
+    my %opts = @_;
+    my $feat_list = $opts{feature_list};
+    return unless @$feat_list;
     my @table;
-    my @feat_list;
-    my $file = "$TEMPDIR/$BASEFILE.featlist";
-    open (IN, $file) || die "can't open $file for reading: $!";
-    while (<IN>)
-    {
-     push @feat_list,$_;
-    }
-    close IN;
-    
-    foreach my $info (@feat_list)
+    my $count = 1;
+    foreach my $info (@$feat_list)
     {
       my $featid = $info;
-      print STDERR $featid,"\n";
-   	 my ($feat) = $coge->resultset("Feature")->find($featid);
-   	 
-   	 my ($name) = sort $feat->names;
-   	 my $hpp = $feat->annotation_pretty_print_html();
-   	 
-   	 push @table,{NAME=>$name,TYPE=>$feat->type->name,LOC=>$feat->start."-".$feat->stop,CHR=>$feat->chr,STRAND=>$feat->strand,ORG=>$feat->organism->name."(v ".$feat->version.")",HPP=>$hpp};
+      #print STDERR $featid,"\n";
+      my ($feat) = $coge->resultset("Feature")->find($featid);
+      unless ($feat)
+	{
+	  warn "feature id $featid failed to return a valid feature object\n";
+	  next;
+	}
+      my ($name) = sort $feat->names;
+      my $hpp = $feat->annotation_pretty_print_html();
+      my $row_style = $count%2 ? "even" : "odd";
+      push @table,{FEATID=>$info,NAME=>$name,TYPE=>$feat->type->name,LOC=>$feat->start."-".$feat->stop,CHR=>$feat->chr,STRAND=>$feat->strand,ORG=>$feat->organism->name."(v ".$feat->version.")",HPP=>$hpp, TABLE_ROW=>$row_style};
+      $count++;
     }
    return \@table;
+  }
+  
+  sub read_file
+  {
+    my $file = "$TEMPDIR/$BASEFILE.featlist";
+    my @featlist;
+    unless (-r $file)
+      {
+	warn "unable to read file $file for feature ids\n";
+	return \@featlist;
+      }
+    open (IN, $file) || die "can't open $file for reading: $!";
+    while (<IN>)
+      {
+	chomp;
+	push @featlist,$_;
+      }
+    close IN;
+    return \@featlist;
+  }
+  
+  sub feats_parse #Send to GEvo
+  {
+    my $accn_list = shift;
+    print STDERR $accn_list,"\n";
+    $accn_list =~ s/^,//;
+    $accn_list =~ s/,$//;
+    my @list;
+    my $url = "/CoGe/GEvo.pl?";
+    my $count = 1;
+    #print STDERR $url,"\n";
+    foreach my $featid (split /,/,$accn_list)
+      {
+		my ($feat) = $coge->resultset("Feature")->find($featid);
+		my ($feat_name) = sort $feat->names;#something
+		$url .= "accn$count=$feat_name&";
+		$count ++;
+      }
+    $count--;
+    return ("alert",$count) if $count > 10;
+    $url .= "num_seqs=$count";
+    return $url;
   }
