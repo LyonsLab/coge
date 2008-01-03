68f4fe98d815a3e7fd79837c92351b328c70d3a6
Eric Lyons <lyonsden@gmail.com>
2008-01-03 19:06:21 +0000

    changed default genome view zoom level

--- web/FeatView.pl
+++ web/FeatView.pl
@@ -203,7 +203,7 @@ sub get_anno
 	my $pro = 0;
 	my $ds = $feat->dataset->id;
 	my $x = $feat->start;
-	my $z = 7;
+	my $z = 4;
 	$anno .= join "\n<BR><HR><BR>\n", $feat->annotation_pretty_print_html(loc_link=>0);
 	$anno .= qq{<DIV id="loc$i"><input type="button" value = "Click for Genome view" onClick="window.open('GeLo.pl?chr=$chr&ds=$ds&x=$x&z=$z');"></DIV>};
 #	$anno .= qq{<DIV id="exp$i"><input type="button" value = "Click for expression tree" onClick="gen_data(['args__Generating expression view image'],['exp$i']);show_express(['args__}.$accn.qq{','args__}.'1'.qq{','args__}.$i.qq{'],['exp$i']);"></DIV>};
