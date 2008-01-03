855ea3ac18d9dae7d108b2d456d844c2cf2b07bd
Eric Lyons <lyonsden@gmail.com>
2008-01-03 19:10:18 +0000

    initial import

---
+++ modules/Database/scripts/delete_organism.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+use CoGeX;
+
+my $orgid = shift;
+unless ($orgid)
+  {
+    print qq{
+Usage: $0 <organism name or id>
+
+This program will remove the specified organism and all related data from the CoGe genomes database.
+};
+    exit;
+  }
+
+my $connstr = 'dbi:mysql:dbname=genomes;host=biocon;port=3306';
+my $coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
+my $org = $coge->resultset('Organism')->resolve($orgid);
+
+print "Do you want to delete:  \n\t".$org->name.": ".$org->description."?\n";
+my $var = <STDIN>;
+if ($var =~ /y/i)
+  {
+    print "Deleteing. . .";
+    $org->delete;
+    print "completed!\n";
+  }
+else
+  {
+    print "Delete aborted.\n";
+  }
