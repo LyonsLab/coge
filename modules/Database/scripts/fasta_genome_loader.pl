7b1b77e0f5a7450234454e5972418725fb8b66c0
Eric Lyons <lyonsden@gmail.com>
2008-01-03 00:32:24 +0000

    initial import

---
+++ modules/Database/scripts/fasta_genome_loader.pl
@@ -0,0 +1,335 @@
+#!/usr/bin/perl
+
+# script name: genomes_dbfill.pl
+# function: fills the genomes database with info from a genbank
+# genbank file, uses GFDB::GBlite to parse, and Eric's db api to fill
+# the db
+
+# bct (01/15/06)
+
+# get files as args...
+#(@ARGV > 0 ) or die "usage: $! <file name> [file name] [...]\n";
+
+use DBI;
+use strict;
+use CoGeX;
+use CoGeX::DataSource;
+use CoGeX::Dataset;
+use CoGeX::Organism;
+use CoGeX::GenomicSequence;
+use Roman;
+use Data::Dumper;
+use Getopt::Long;
+
+use vars qw($DEBUG $coge $GENOMIC_SEQ_LEN $GO $ERASE);
+
+
+my ($nt_file, $nt_dir, $org_name, $org_desc, $org_id, $ds_name, $ds_desc, $ds_link, $ds_id, $di_name, $di_desc, $di_link, $di_version, $use_contigs_as_features, $chr, $seq_type_name, $seq_type_desc, $seq_type_id);
+
+GetOptions ( "debug=s" => \$DEBUG,
+	     "go=s"    => \$GO,
+	     "erase|e" => \$ERASE, 
+	     "fasta_file|fasta|faa|nt_file|nt=s" => \$nt_file,
+	     "fasta_dir|nt_dir|dir=s"=>\$nt_dir,
+	     "fasta_dir=s"=>\$nt_dir,
+	     "org_name=s" => \$org_name,
+	     "org_desc=s" => \$org_desc,
+	     "org_id=s"   => \$org_id,
+	     "ds_name=s" => \$ds_name,
+	     "ds_desc=s" => \$ds_desc,
+	     "ds_link=s" => \$ds_link,
+	     "ds_id=s"   => \$ds_id,
+	     "di_name=s" => \$di_name,
+	     "di_desc=s" => \$di_desc,
+	     "di_link=s" => \$di_link,
+	     "di_version=s" => \$di_version,
+	     "chr=s"=>\$chr,
+	     "use_contigs_as_features=i" => \$use_contigs_as_features,
+	     "seq_type_name=s" => \$seq_type_name,
+	     "seq_type_desc=s" => \$seq_type_desc,
+	     "seq_type_id=i"=>\$seq_type_id,
+	   );
+
+$DEBUG = 1 unless defined $DEBUG; # set to '1' to get updates on what's going on
+$GO = 1 unless defined $GO; #set to 1 to actually make db calls.
+
+
+
+$use_contigs_as_features =0 unless defined $use_contigs_as_features;
+
+
+
+my $GENOMIC_SEQ_LEN = 10000; #length to break up genomic sequence
+my $connstr = 'dbi:mysql:dbname=genomes;host=biocon;port=3306';
+$coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
+#$coge->storage->debugobj(new DBIxProfiler());
+#$coge->storage->debug(1);
+
+
+
+my $di = generate_di(
+		     org_name => $org_name,
+		     org_desc => $org_desc,
+		     org_id=>$org_id,
+		     ds_name => $ds_name,
+		     ds_desc => $ds_desc,
+		     ds_link => $ds_link,
+		     ds_id=>$ds_id,
+		     di_name => $di_name,
+		     di_desc => $di_desc,
+		     di_link => $di_link,
+		     di_version => $di_version,
+		    );
+unless ($di)
+  {
+    warn "dataset object not initialized.  Exiting.";
+    exit;    
+  }
+
+my $gstype = generate_gstype(id=>$seq_type_id, name=>$seq_type_name, desc=>$seq_type_desc);
+unless ($gstype)
+  {
+    warn "genomic sequence type object not initialized.  Exiting.";
+    exit;
+  }
+
+if ($ERASE)
+  {
+#    my $di = $DB->get_dataset_obj->resolve_dataset($di;
+    print "Clearing database of entries associated with ".$di->name.". . .";
+    $di->delete();
+    print "finished!\n";
+    exit;
+  }
+
+process_nt (file=>$nt_file, di=>$di, dir=>$nt_dir) if $nt_file || $nt_dir;
+sub add_location
+  {
+    my %opts = @_;
+    my $chr=$opts{chr};
+    my $start=$opts{start};
+    my $stop=$opts{stop};
+    my $strand=$opts{strand};
+    my $feat=$opts{feat};
+    print "Adding location $chr:($start-$stop, $strand)\n" if $DEBUG;
+    my $loc = $coge->resultset('Location')->create(
+						   {
+						    feature_id => $feat->id,
+						    start      => $start,
+						    stop       => $stop,
+						    strand     => $strand,
+						    chromosome => $chr
+						   }
+						  ) if $GO;
+    return $loc;
+  }
+
+sub get_feature
+  {
+    my %opts = @_;
+    my $type = $opts{type};
+    my $name = $opts{name};
+    my $chr = $opts{chr};
+    my $start = $opts{start};
+    my $stop = $opts{stop};
+    print "Working on $name of type $type\n" if $DEBUG;
+    my $di = $opts{di};
+    my $feat;
+    foreach my $fn ($coge->resultset('FeatureName')->search({name=>$name}))
+      {
+	my $f = $fn->feature;
+	next unless $f->type->name eq $type;
+	next unless $di && $f->dataset->id eq $di->id;
+	$feat = $f;
+	print "Found existing feature for $name\n";
+#	return $feat;
+      }
+    my $feat_type = $coge->resultset('FeatureType')->find_or_create( { name => $type } ) if $GO;
+    print "Creating feature of type $type\n" if $DEBUG;
+    $feat = $di->add_to_features ({
+				   feature_type_id => $feat_type->id,
+				   #dataset_id => $di->id,
+				   start=>$start,
+				   stop=>$stop,
+				   chromosome=>$chr,
+				   strand=>1,
+				  }) if (!$feat && $GO);
+    if ($DEBUG)
+      {
+	print "Creating feature_name $name for feat ";
+	print $feat->id if $feat;
+	print "\n";
+      }
+    my $feat_name = $coge->resultset('FeatureName')->create({
+							     name=>$name,
+							     feature_id=>$feat->id,
+							    }) if $GO;
+    return $feat;
+  }
+
+sub process_nt
+  {
+    my %opts = @_;
+    my $file =$opts{file};
+    my $di = $opts{di};
+    my $dir = $opts{dir};
+    my @files;
+    push @files, $file if $file && -r $file;
+    if (-d $dir)
+      {
+	opendir (DIR, $dir);
+	while (my $item = readdir(DIR))
+	  {
+	    next if $item =~ /^\.\.?$/;
+	    push @files, "$dir/$item" if -r "$dir/$item";
+	  }
+	closedir(DIR);
+      }
+    foreach my $file (@files)
+      {
+	process_nt_file (file=>$file, di=>$di);
+      }
+  }
+
+
+sub process_nt_file
+  {
+    my %opts = @_;
+
+    my $file =$opts{file};
+    my $di = $opts{di};
+    return unless $file;
+    $/ = "\n>";
+    open (IN, $file) || die "can't open $file for reading: $!";
+    while (<IN>)
+      {
+	s/>//g;
+	my ($name, $seq) = split /\n/, $_,2;
+	my $chrtmp = $chr;
+	$seq =~ s/\n//g;
+	($chrtmp) = $name=~/(\d+)/ unless $chrtmp;
+	$chrtmp = $name unless $chrtmp;
+	$chrtmp =~ s/chromosome//;
+	$chrtmp =~ s/chr//;
+	$chrtmp =~ s/^0+//;
+	$chrtmp =~ s/^\s//;
+	$chrtmp =~ s/\s$//;
+	load_genomic_sequence(chr=>$chrtmp,
+			      seq=>$seq,
+			      di=>$di,
+			      );
+      }
+    close IN;
+  }
+
+sub load_genomic_sequence
+  {
+    my %opts = @_;
+    my $seq = $opts{seq};
+    my $len = $opts{len};
+    my $chr = $opts{chr};
+    my $di = $opts{di};
+    $len = $GENOMIC_SEQ_LEN unless $len;
+    my $seqlen = length $seq;
+    print "Loading genomic sequence for chr $chr ($seqlen nt)\n" if $DEBUG;
+    $seq =~ s/\s//g;
+    $seq =~ s/\n//g;
+    my $i = 0;
+#    my $gso = $coge->get_genomic_sequence_obj;
+    while ($i < $seqlen)
+      {
+	my $str = substr($seq,$i,$len);
+	my $start = $i+1;
+	my $stop = $i + length $str;
+	$di->add_to_genomic_sequences({start=>$start,
+				       stop=>$stop,
+				       chromosome=>$chr,
+				       sequence_data=>$str,
+				       genomic_sequence_type_id=>$gstype->id,
+				      }) if $GO;
+	#	print "\t$start-$stop\n";
+	$i += $len;
+      }
+    if ($use_contigs_as_features)
+      {
+	my $feat = get_feature(type=>"contig", name=>$chr, di=>$di, chr=>$chr, start=>1, stop=>length($seq));
+	add_location(chr=>$chr, start=>1, stop=>length($seq), feat=>$feat, strand=>1);
+
+      }
+  }
+
+sub generate_di
+  {
+    my %opts = @_;
+    my $org_id = $opts{org_id};
+    my $org_name = $opts{org_name};
+    my $org_desc = $opts{org_desc};
+    my $ds_id = $opts{ds_id};
+    my $ds_name = $opts{ds_name};
+    my $ds_desc = $opts{ds_desc};
+    my $ds_link = $opts{ds_link};
+    my $di_name = $opts{di_name};
+    my $di_desc = $opts{di_desc};
+    my $di_link = $opts{di_link};
+    my $di_version = $opts{di_version};
+    unless ($org_id || $org_name)
+      {
+	warn "no organism id or name specified\n";
+	return;
+      }
+    my $org = $coge->resultset('Organism')->find($org_id) if $org_id;
+    $org = $coge->resultset('Organism')->find_or_create({
+							 name=>$org_name,
+							 description=>$org_desc,
+							})  if $GO && !$org;
+    unless ($ds_id || $ds_name)
+      {
+	warn "no data source id or name specified\n";
+	return;
+      }
+
+    my $ds = $coge->resultset('DataSource')->find($ds_id) if $ds_id;
+    $ds = $coge->resultset('DataSource')->find_or_create({
+							  name=>$ds_name,
+							  description=>$ds_name,
+							  link=>$ds_link,
+							 }) if $GO && !$ds;
+    unless ($di_name)
+      {
+	warn "no dataset name specified\n";
+	return;
+      }
+    my $di = $coge->resultset('Dataset')->find_or_create({
+									   name                => $di_name,
+									   description         => $di_desc,
+									   link                => $di_link, 
+									   organism_id         => $org->id,
+									   data_source_id      => $ds->id(),
+									   version=>$di_version,
+									  })  if $GO;
+    return $di;
+
+  }
+
+sub generate_gstype
+  {
+    my %opts = @_;
+    my $name = $opts{name};
+    my $desc = $opts{desc};
+    my $id = $opts{id};
+    my $gstype;
+    $id = 1 unless ($id || $name);
+    if ($id)
+      {
+	$gstype = $coge->resultset('GenomicSequenceType')->find($id);
+      }
+    else
+      {
+	$gstype = $coge->resultset('GenomicSequenceType')->find_or_create({name=>$name,
+									     description=>$desc,
+									    });
+      }
+    return $gstype;
+  }
+
+__END__
