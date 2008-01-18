f2dc56662d4bac323e817f561c3862f736acbfa9
Eric Lyons <lyonsden@gmail.com>
2008-01-18 19:26:44 +0000

    added percentage to overlap feature stats in results

--- web/GEvo.pl
+++ web/GEvo.pl
@@ -722,7 +722,8 @@ sub run
     $html .= qq{<td class = small>Overlap Feature Stats:};
     foreach my $item (@sets)
       {
-	$html .= "<div class = small>".$item->{obj}->accn.": ".$item->{overlap_count}." / ".$item->{feat_count};
+	$html .= "<div class = small>".$item->{obj}->accn.": ".$item->{overlap_count}." / ".$item->{feat_count}." (".sprintf("%.2f", $item->{overlap_count}/$item->{feat_count}*100)."%)";
+	
       }
     $html .= qq{</table>};
 
