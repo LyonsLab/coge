4495daeabd71449bad55c3cb014528c4083527ec
Eric Lyons <lyonsden@gmail.com>
2008-04-25 17:14:01 +0000

    updated to latest version

--- web/js/Genomic.js
+++ web/js/Genomic.js
@@ -7,9 +7,72 @@
  * 
  * @requires OpenLayers/Layer/Grid1D.js
  */
+
+OpenLayers.Layer.Grid.prototype.initGriddedTiles = function(bounds) {
+    if(this.grid.length == 0){ this.grid.push([]); }
+
+    var viewSize = this.map.getSize();
+    var minCols = Math.ceil(viewSize.w/this.tileSize.w) +
+            Math.max(1, 2 * this.buffer);
+
+    var extent = this.map.getMaxExtent();
+    var resolution = this.map.getResolution();
+
+    var tileLayout = this.calculateGridLayout(bounds, extent, resolution);
+
+    var tileoffsetx   = Math.round(tileLayout.tileoffsetx);
+    var tileoffsetlon = tileLayout.tileoffsetlon;
+    var tilelon       = tileLayout.tilelon;
+
+    this.origin = new OpenLayers.Pixel(tileoffsetx, 0);
+
+    var startX   = tileoffsetx;
+    var startLon = tileoffsetlon;
+
+    var colidx = 0;
+
+
+    var layerContainerDivLeft = parseInt(this.map.layerContainerDiv.style.left);
+    var layerContainerDivTop = parseInt(this.map.layerContainerDiv.style.top);
+
+    // follow the inner loop in the original OpenLayers version.
+    do {
+        var tileBounds = new OpenLayers.Bounds(tileoffsetlon,
+                                                0,
+                                                tileoffsetlon + tilelon,
+                                                1);
+
+        //var x = tileoffsetx;
+        //x -= layerContainerDivLeft;
+        //var y = 0;
+
+        var px = new OpenLayers.Pixel(tileoffsetx - layerContainerDivLeft, 0);
+
+        var tile = this.grid[0][colidx++];
+        if (!tile) {
+            tile = this.addTile(tileBounds, px);
+            this.addTileMonitoringHooks(tile);
+            this.grid[0].push(tile);
+        } else {
+            tile.moveTo(tileBounds, px, false);
+        }
+
+        tileoffsetlon += tilelon;
+        tileoffsetx += this.tileSize.w;
+    } while ((tileoffsetlon <= bounds.right + tilelon * this.buffer)
+        || colidx < minCols)
+
+    this.removeExcessTiles(2, colidx);
+    //now actually draw the tiles
+    //this.linearTileLoad();
+    this.spiralTileLoad();
+};
+OpenLayers.Layer.Grid.prototype.buffer = 1;
+
+
 OpenLayers.Layer.Genomic = OpenLayers.Class.create();
 OpenLayers.Layer.Genomic.prototype = 
-  OpenLayers.Class.inherit( OpenLayers.Layer.Grid1D, {
+  OpenLayers.Class.inherit( OpenLayers.Layer.Grid, {
 
     /** Hashtable of default parameter key/value pairs 
      * @final @type Object */
@@ -29,7 +92,7 @@ OpenLayers.Layer.Genomic.prototype =
         var newArguments = [];
         //uppercase params
         newArguments.push(name, url, params, options);
-        OpenLayers.Layer.Grid1D.prototype.initialize.apply(this, newArguments);
+        OpenLayers.Layer.Grid.prototype.initialize.apply(this, newArguments);
         OpenLayers.Util.applyDefaults(
                        this.params, 
                        this.DEFAULT_PARAMS
@@ -48,7 +111,7 @@ OpenLayers.Layer.Genomic.prototype =
      */
     destroy: function() {
         // for now, nothing special to do here. 
-        OpenLayers.Layer.Grid1D.prototype.destroy.apply(this, arguments);  
+        OpenLayers.Layer.Grid.prototype.destroy.apply(this, arguments);  
     },
 
     
@@ -68,7 +131,7 @@ OpenLayers.Layer.Genomic.prototype =
         }
 
         //get all additions from superclasses
-        obj = OpenLayers.Layer.Grid1D.prototype.clone.apply(this, [obj]);
+        obj = OpenLayers.Layer.Grid.prototype.clone.apply(this, [obj]);
 
         // copy/set any non-init, non-simple values here
 
@@ -88,8 +151,8 @@ OpenLayers.Layer.Genomic.prototype =
     },
     getURL: function (bounds) {
         var zoom = this.getZoom();
-        var res = this.getResolution();        
-        var xmin = Math.ceil(Math.ceil(bounds.left/res) * res);
+        var res = this.getResolution();
+        var xmin = Math.floor(Math.floor(bounds.left/res) * res);
         var xmax = Math.floor(Math.floor(bounds.right/res) * res);
         return this.getFullRequestString(
                      {
@@ -151,7 +214,7 @@ OpenLayers.Layer.Genomic.prototype =
     mergeNewParams:function(newParams) {
         var upperParams = OpenLayers.Util.upperCaseObject(newParams);
         var newArguments = [upperParams];
-        OpenLayers.Layer.Grid1D.prototype.mergeNewParams.apply(this, 
+        OpenLayers.Layer.Grid.prototype.mergeNewParams.apply(this, 
                                                              newArguments);
 
         if (this.map != null) {
@@ -171,7 +234,7 @@ OpenLayers.Layer.Genomic.prototype =
     */
     getFullRequestString:function(newParams) {
 
-        return OpenLayers.Layer.Grid1D.prototype.getFullRequestString.apply(
+        return OpenLayers.Layer.Grid.prototype.getFullRequestString.apply(
                                                     this, arguments);
     },
     
