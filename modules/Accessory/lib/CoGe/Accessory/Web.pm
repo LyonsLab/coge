23a4548a51c367e928742f1edf4bbb2dcb4fdabe
Eric Lyons <lyonsden@gmail.com>
2008-05-07 22:41:30 +0000

    removed lots of old and unused functions 'I hope'

--- modules/Accessory/lib/CoGe/Accessory/Web.pm
+++ modules/Accessory/lib/CoGe/Accessory/Web.pm
@@ -1,7 +1,6 @@
 package CoGe::Accessory::Web;
 
 use strict;
-use CoGe::Genome;
 use CoGeX;
 use Data::Dumper;
 use base 'Class::Accessor';
@@ -13,7 +12,7 @@ use File::Temp;
 
 BEGIN {
     use Exporter ();
-    use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $coge $Q $cogex $TEMPDIR);
+    use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $Q $cogex $TEMPDIR);
     $VERSION     = 0.1;
     $TEMPDIR = "/opt/apache/CoGe/tmp";
     @ISA         = (@ISA, qw (Exporter));
@@ -21,39 +20,11 @@ BEGIN {
     @EXPORT      = qw (login write_log read_log check_taint check_filename_taint save_settings load_settings reset_settings initialize_basefile);
     @EXPORT_OK   = qw ();
     %EXPORT_TAGS = ();
-    $coge = new CoGe::Genome;
     $cogex = CoGeX->dbconnect();
 #    $cogex->storage->debugobj(new DBIxProfiler());
 #    $cogex->storage->debug(1);
     __PACKAGE__->mk_accessors qw(restricted_orgs basefilename basefile logfile sqlitefile);
  }
-
-
-sub feat_name_search
-  {
-    my ($self, $accn, $num, $min_search) = self_or_default(@_);
-    #may want to extend this function to use all the options of feat->power_search.  See FeatView.pl for an example
-    $num = 1 unless defined $num;
-    $min_search = 2 unless defined $min_search;
-    return unless $accn;
-    my $blank = qq{<input type="hidden" id="accn_select$num">};
-#    print STDERR Dumper @_;
-    return $blank unless length($accn) > $min_search;# || $type || $org;
-    my $html;
-    my %seen;
-    my @opts = sort map {"<OPTION>$_</OPTION>"} grep {! $seen{$_}++} map {uc($_)} $coge->get_feature_name_obj->power_search(accn=>$accn."%");
-    if (@opts > 5000)
-      {
-	return $blank."Search results over 1000, please refine your search.\n";
-      }
-    $html .= "<font class=small>Name count: ".scalar @opts."</font>\n<BR>\n";
-    $html .= qq{<SELECT id="accn_select$num" SIZE="5" MULTIPLE onChange="source_search_chain(); " >\n};
-    $html .= join ("\n", @opts);
-    $html .= "\n</SELECT>\n";
-    $html =~ s/<OPTION/<OPTION SELECTED/;
-    return $blank."No results found.\n", $num unless $html =~ /OPTION/;
-    return $html, $num;
-  }
 
 sub dataset_search_for_feat_name
   {
@@ -162,126 +133,6 @@ sub feat_search_for_feat_name
     return $html;
   }
 
-sub type_search_for_feat_name
-  {
-    my ($accn, $dsid, $num) = self_or_default(@_);
-    $num = 1 unless defined $num;
-    my $html;
-    my $blank = qq{<input type="hidden" id="type_name$num">};
-    my %seen;
-    my @opts = sort map {"<OPTION>$_</OPTION>"} grep {! $seen{$_}++} map {$_->type->name} $coge->get_features_by_name_and_dataset_id(name=>$accn, id=>$dsid);
-    $html .= "<font class=small>Type count: ".scalar @opts."</font>\n<BR>\n";
-    $html .= qq{<SELECT id="Type_name" SIZE="5" MULTIPLE onChange="get_anno(['accn_select$num','type_name$num', 'dsid$num'],['anno$num'])" >\n};
-    $html .= join ("\n", @opts);
-    $html .= "\n</SELECT>\n";
-    $html =~ s/OPTION/OPTION SELECTED/;
-    return $blank unless $html =~ /OPTION/;
-    return ($html, 1, $num);
-  }
-
-sub feat_name_search_box
-  {
-    my ($self, $num, $default) = self_or_default(@_);
-    $num = 1 unless defined $num;
-    return qq{
-<input type="text" name="accn$num" id="accn$num" tabindex="1"  size="10" onKeyup ="feat_name_search_chain(0);"  onBlur="feat_name_search_chain(1);" value="$default"/>
-<DIV class="" id="accn_list$num"><input type="hidden" id="accn_select$num" ></DIV>
-<DIV class="" id="ds$num"></DIV>
-<DIV class="" id="feat_type$num"></DIV>
-<DIV class="" id="anno$num"></DIV>
-};
-  }
-
-sub feat_search_box
-  {
-    my ($self, $num, $default) = self_or_default(@_);
-    $num = 1 unless defined $num;
-    return qq{
-<input class="backbox" type="text" name="accn$num" id="accn$num" tabindex="$num"  size="20" onBlur="dataset_search(['accn$num','args__$num'],[feat_search_chain])" value="$default" />
-<DIV id="ds$num"><input type="hidden" id="dsid$num"></DIV>
-<DIV id="feat$num"><input type="hidden" id="featid$num"></DIV>
-};
-  }
-
-sub feat_name_search_chain
-  {
-    my ($self, $lim) = self_or_default(@_);
-    $lim = 4 unless defined $lim;
-    return qq{
-<SCRIPT language="javascript">
-function feat_name_search_chain(val, num) {
- if (!num) {num = 1};
- minlen = $lim;
- if ((val == 1) && (kaj.GS('accn'+num).length > minlen)){
-   return;
- } 
- kaj.GS('accn_list'+num,'fnsc_accn_list'+num);	
- kaj.GS('ds'+num,'fnsc_ds'+num);	
- kaj.GS('feat_type'+num,'fnsc_feat_type'+num);	
-// kaj.GS('anno'+num,'');	
- if (val == 1 || kaj.GS('accn'+num).length > minlen) {
-//   kaj.GS('DS_title','<font class="loading">Searching. . .</font>');// for "'+kaj.GS('accn')+'" type '+''+'. . .</font>');
-   feat_name_search(['accn'+num, 'args__'+num],[ds_search_chain]); 
- }
-}
-</SCRIPT>
-};
-  }
-
-sub ds_search_chain
-  {
-return qq{
-<SCRIPT language="javascript">
-function ds_search_chain (val, num) {
- kaj.GS('ds'+num,'<font class="loading">dssc_ds'+num+'Loading. . .</font>');	
- kaj.GS('feat_type'+num,'dssc_feat_type'+num);
- kaj.GS('anno'+num,'dssc_anno'+num);
- if (val) {kaj.GS('accn_list'+num,val);}
- dataset_search(['accn_select'+num, 'args__'+num], [feat_type_search_chain]);
-}
-</SCRIPT>
-
-};
-
-  }
-
-sub feat_search_chain
-  {
-    return qq{
-<SCRIPT language="javascript">
-function feat_search_chain(val, num) {
- if (val) {
-  kaj.GS('ds'+num, val);
-  feat_search(['accn'+num,'dsid'+num, 'args__'+num],['feat'+num]);
- }
-}
-</SCRIPT>
-};
-  }
-
-sub feat_type_search_chain
-  {
-    return qq{
-<SCRIPT language="javascript">
-function feat_type_search_chain (val1, num) {
-// kaj.GS('FT_title','');
- kaj.GS('feat_type'+num,'<font class="loading">FTC_feat_type Loading. . .</font>');	
- kaj.GS('anno'+num,'FTC_anno');	
- if (val1) {kaj.GS('ds'+num, val1);}
-// if (val2) {kaj.GS('DS_title', "Dataset");}	
- type_search(['accn_select'+num, 'dsid'+num, 'args__'+num],[get_anno_chain]);	
-}
-</SCRIPT>
-};
-}
-
-sub kaj
-  {
-    return qq{
-<SCRIPT language="JavaScript" type="text/javascript" src="./js/kaj.stable.js"></SCRIPT>
-};
-  }
-
 sub self_or_default { #from CGI.pm
     return @_ if defined($_[0]) && (!ref($_[0])) &&($_[0] eq 'CoGe::Accessory::Web');
     unless (defined($_[0]) && 
@@ -321,10 +172,6 @@ sub ajax_func
   {
     return 
       (
-       dataset_search=>\&dataset_search_for_feat_name,
-       feat_search=>\&feat_search_for_feat_name,
-       feat_name_search=>\&feat_name_search,
-       type_search=> \&type_search_for_feat_name,
        login=>\&login,
        read_log=>\&read_log,
        initialize_basefile=>\&initialize_basefile,
