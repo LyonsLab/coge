f374bf854e65f9082aae43779ef122f2deea98d5
Eric Lyons <lyonsden@gmail.com>
2008-10-17 18:58:45 +0000

    updated which datasets are loaded from NCBI

--- modules/Database/scripts/load_all_ncbi_genomes.pl
+++ modules/Database/scripts/load_all_ncbi_genomes.pl
@@ -5,20 +5,48 @@ use LWP::Simple;
 use Data::Dumper;
 use Parallel::ForkManager;
 
+# for understanding accession nomeclature from NCBI see http://www.ncbi.nlm.nih.gov/RefSeq/key.html#accessions
+# genbank genlist.cgi type codes:
+# 0 == Chromosomes and plasmids (NC_)
+# 1 == Chromosomes (NC_)
+# 2 == plasmids (NC_)
+# 3 == Whole shotgun sequences (NZ_)
+# 4 == mito, plastids, plasmids, nucleomorphs
+# 5 == viruses
+# 6 == phages
+# 7 == all plasmids for all proks? (NC_)
+# 8 == Chromosomes (NC_ and NS_)
+# 9 == chromosomes and plasmids (NC_ NS_ NZ_)
 my @pages = qw(
 http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=2157&type=1&name=Archaea%20Complete%20Chromosomes
-http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=2&type=1&name=Bacteria%20Complete%20Chromosomes
+http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=2&type=0
 http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=2759&type=8&name=Eukaryotae%20Complete%20Chromosomes
 http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=10239&type=5&name=Viruses
 http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=10239&type=6&name=Phages
 http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?taxid=12884&type=0&name=Viroids
 );
+my $base_url = "http://www.ncbi.nlm.nih.gov/genomes/genlist.cgi?";
 
+#taxids
+#2157 == archaea
+#2    == bacteria
+#2759 == euks
+
+#taxid type
+my @taxids =(
+	     [2157, 9], #archaea, get WGS
+	     [2,    9], #bacteria, get WGS
+	     [2759, 0], #euks, not ready for WGS -- lots of data with minimal annotations.  Will get there though!
+	     [10239,9], #viruses, phages, 
+	     [12884,9], #viroids
+	    );
 
 my $pm = new Parallel::ForkManager(20);
-foreach my $page (@pages)
+foreach my $item (@taxids)
   {
-    my $content =get($page);
+    my ($id, $type) = @$item;
+    my $url = $base_url."type=$type"."&taxid=$id";
+    my $content =get($url);
     my @tables = split /<\/table>/, $content;
     my @rows = split /<tr>/,$tables[8];
     my $prog = '/home/elyons/projects/CoGeX/scripts/genbank_genome_loader.pl';
@@ -42,8 +70,9 @@ foreach my $page (@pages)
 	$cols[2] =~ s/\s+$//;
 	$cols[2] = undef if $cols[2] && ($cols[2] eq " " || $cols[2] =~ /nbsp/);
 	$run .= " -chr '$cols[2]'" if $cols[2];
-	$run .= " -go -td '/tmp/gb/'";
-	print $run,"\n";
+	$run .= " -td '/tmp/gb/'";
+	$run .= " -go";
+	print "\n",$run,"\n";
 	open (IN, $run." |");
 	while (<IN>)
 	  {
