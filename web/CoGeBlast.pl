2c8a800a2deb4472fc572c410b72967d1c2f87bf
Eric Lyons <lyonsden@gmail.com>
2008-11-14 19:26:40 +0000

    updated call for getting current datasets to org->current_datasets

--- web/CoGeBlast.pl
+++ web/CoGeBlast.pl
@@ -931,7 +931,7 @@ sub get_blast_db
   {
     my $orgid = shift;
     my $org = $coge->resultset('Organism')->find($orgid);
-    my @ds = $coge->get_current_datasets_for_org(org=>$orgid);
+    my @ds = $org->current_datasets();
     @ds = sort {$a->id <=> $b->id }@ds;
     return unless @ds;
     my $org_name = $ds[0]->organism->name;
