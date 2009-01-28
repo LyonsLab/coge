c3c6402ebd506232000318487a4c252661fd4c69
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2009-01-28 17:50:10 +0000

    proper gff3 output, remove dups, do the best job with naming that we can

--- modules/Database/scripts/export_to_gff.pl
+++ modules/Database/scripts/export_to_gff.pl
@@ -12,6 +12,7 @@ if(scalar(@ARGV) < 3){
     exit();
 }
 
+
 my $organism = shift;
 my $name_re = shift;
 my $datasets = \@ARGV;
@@ -32,7 +33,6 @@ foreach my $k (keys %$schrs){
     }
 }
 
-
 sub get_locs {
     my $SEP = "\t";
     my $datasets = shift;
@@ -52,7 +52,10 @@ sub get_locs {
     }
     my %seen = {};
     my %names = {};
+    #my $i = 0;
+    my %fids = {};
     foreach my $chr (@chrs){
+        #if ($i++ > 2){ print STDERR "*" x 1000 . "\nENDING EARLY";  last; }
         
         my $gene_rs = $s->resultset('Feature')->search( {
                   'me.dataset_id' => { 'IN' => $datasets },
@@ -65,18 +68,28 @@ sub get_locs {
 
         #gff: chr  organization feature_type  start stop strand . name
         my %chrs;
-        print STDERR $chr . "\n";
-        print STDERR join(",", @$datasets) . "\n";
+        print STDERR "dataset_ids: " . join(",", @$datasets) . ";  chr: $chr\n";
         while(my $g = $gene_rs->next()){
+            if ($fids{$g->feature_id}){ next; }
+            $fids{$g->feature_id} = 1;
 
-            my @gene_names = grep { $_->name =~ /$name_re/ } $g->feature_names(); 
+            my @gene_names = grep { $_->name =~ /$name_re/i } $g->feature_names(); 
             my $gene_name;
             if (scalar(@gene_names) == 0){
                 $gene_name = $g->feature_names()->next()->name;
+                print STDERR "not from re:" . $gene_name . "\n";
+                print STDERR $name_re . "\n";
             }
             else {
-                $gene_name = $gene_names[0];
+                foreach my $name (@gene_names){
+                    $gene_name = $name->name;
+                    if (! $names{$gene_name} ){ last; }
+                    $gene_name = "";
+                }
+                
+                $gene_name = $gene_names[0]->name;
             }
+            if(!$gene_name || $names{$gene_name} ) { next; }
 
             my $mrna_rs = $s->resultset('Feature')->search( {
                   'me.dataset_id' => { 'IN' => $datasets },
@@ -85,8 +98,8 @@ sub get_locs {
                   'feature_type.name'  =>  'mRNA' 
                 } , { 
                     'join' => 'feature_names',
-                   'prefetch'           => [ 'feature_type', 'feature_names'] 
-                  ,'order_by'           => [ 'me.start']
+                   'prefetch'           => [ 'feature_type', 'locations'] 
+                  ,'order_by'           => [ 'me.start', 'locations.start']
                 });
 
             #my $mrna = $mrna_rs->next();
@@ -94,23 +107,25 @@ sub get_locs {
 
             my $strand = $g->strand == 1 ? '+' : '-';
             my $clean_name = $gene_name;
-            while ($names{$clean_name}){
-                $clean_name .= ".1";
-            }
-            $names{$clean_name} = 1;
+            $names{$gene_name} = 1;
             $clean_name =~ s/\s+/_/g;
             my $attrs = "ID=$clean_name;Name=$clean_name";
+
             my $gstr = join("\t", ($chr, 'ucb', $g->feature_type->name, $g->start, $g->stop, ".", $strand, ".", $attrs));
             if($seen{$gstr}){ next; }
             $seen{$gstr} = 1;
+
             print $gstr . "\n";
+
             my $parent = $clean_name;
             my $has_mrna = 0;
             while(my $f = $mrna_rs->next()){
+                if($fids{$f->feature_id}){ next; }
+                $fids{$f->feature_id} = 1;
                 $attrs = "Parent=$parent;ID=$parent" . ".mRNA";
-                my $has_mrna = 1;
-                my $locs = $f->locations({}, {'order_by' => 'start'});
-                while(my $loc = $locs->next()){
+                $has_mrna = 1;
+                               
+                foreach my $loc ($f->locations()){
                     my $gstr = join("\t", ($f->chr, 'ucb', $f->feature_type->name, $loc->start, $loc->stop, ".", $strand, ".", $attrs));
                     if($seen{$gstr}){ next; }
                     $seen{$gstr} = 1;
@@ -118,11 +133,9 @@ sub get_locs {
             
                 }
             }
+            $attrs = "Parent=$clean_name";
             if($has_mrna){
-                $attrs = "Parent=$clean_name" . "mRNA";
-            }
-            else {
-                $attrs = "Parent=$clean_name";
+                $attrs .=  ".mRNA";
             }
 
             #print join("\t", ($chr, 'ucb', 'mRNA', $mrna->start, $mrna->stop, ".", $strand, ".", $attrs)) . "\n";
@@ -137,10 +150,11 @@ sub get_locs {
                   ,'order_by'           => [ 'me.chromosome', 'me.start']
                 });
             while(my $f = $sub_rs->next()){
-                my $locs = $f->locations({}, {'order_by' => 'start'});
-                while(my $loc = $locs->next()){
+                #my $locs = $f->locations({}, {'order_by' => 'start'});
+                foreach my $loc ($f->locations()){
                     my $gstr = join("\t", ($f->chr, 'ucb', $f->feature_type->name, $loc->start, $loc->stop, ".", $strand, ".", $attrs));
                     if($seen{$gstr}){ next; }
+                    $seen{$gstr} = 1;
                     print $gstr . "\n";
             
                 }
