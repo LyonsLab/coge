ad6390cddd7a4a72c4b355d9e3506c6330d39512
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2009-02-02 17:19:41 +0000

    removing debugging print statements

--- modules/Database/lib/CoGeX/Fasta.pm
+++ modules/Database/lib/CoGeX/Fasta.pm
@@ -1,18 +1,13 @@
 package CoGeX::Fasta;
 
 use strict;
-use Data::Dumper;
 
 sub get_seq {
     my $module = shift;
-    print Dumper $module;
     my $opts = shift;
-    print Dumper $opts;
-    print $opts->{'blastdb'} . "\n";
     my $fastacmd = $opts->{fastacmd} || 'fastacmd';
     my $blastdb = $opts->{blastdb} || $opts->{db};
     my $seqid   = $opts->{seqid} || $opts->{chr} || $opts->{chromosome}; # chr 
-    print $blastdb . "\n";
 
     my $cmd = "$fastacmd -d $blastdb -s $seqid ";
     if($opts->{start} && ($opts->{stop} || $opts->{end})){
@@ -21,7 +16,7 @@ sub get_seq {
     if($opts->{reverse_complement} || $opts->{rc}){
         $cmd .= "-S 2";
     }
-    print $cmd . "\n";
+    #print $cmd . "\n";
     return `$cmd`;
 
 }
