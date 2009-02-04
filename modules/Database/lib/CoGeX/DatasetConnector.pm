e17e37a25c65f9a1a37fb47bc2ce36777fc94e61
Eric Lyons <lyonsden@gmail.com>
2009-02-04 23:58:51 +0000

    initial import

---
+++ modules/Database/lib/CoGeX/DatasetConnector.pm
@@ -0,0 +1,25 @@
+package CoGeX::DatasetConnector;
+
+# Created by DBIx::Class::Schema::Loader v0.03009 @ 2006-12-01 18:13:38
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("PK::Auto", "Core");
+__PACKAGE__->table("dataset_connector");
+__PACKAGE__->add_columns(
+  "dataset_connector_id",
+  { data_type => "INT", default_value => 1, is_nullable => 0, size => 10 },
+  "dataset_id",
+  { data_type => "INT", default_value => "", is_nullable => 0, size => 10 },
+  "dataset_group_id",
+  { data_type => "INT", default_value => "", is_nullable => 0, size => 10 },
+);
+__PACKAGE__->set_primary_key("dataset_connector_id");
+__PACKAGE__->belongs_to("dataset_group" => "CoGeX::DatasetGroup", "dataset_group_id");
+__PACKAGE__->belongs_to("dataset" => "CoGeX::Dataset", "dataset_id");
+
+1;
+
