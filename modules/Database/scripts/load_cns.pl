bdc138158acb240e3a99656c455a45c402ead164
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2009-04-17 02:05:57 +0000

    update loading script for cnss to new db.

--- modules/Database/scripts/load_cns.pl
+++ modules/Database/scripts/load_cns.pl
@@ -5,19 +5,32 @@ use Data::Dumper;
 use CoGeX;
 use Getopt::Long;
 
-my $connstr = 'dbi:mysql:dbname=genomes;host=biocon;port=3306';
+my $connstr = 'dbi:mysql:dbname=coge;host=homer;port=3306';
 my$coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
 
-my $DEBUG = 1;
 my $GO=0;
 my $ds1; my $ds2;
-my $add_gene = 0;
 GetOptions ( 
              "ds1=i"            => \$ds1,
              "ds2=i"            => \$ds2,
 	         "go=s"             => \$GO,
-	         "add_gene_feature" => \$add_gene,
 	   );
+# perl scripts/load_cns.pl -ds1 34465 -ds2 34978 < /tmp/rice_sorghum_for_paper.csv
+
+# to clear out db:
+#DELETE from annotation where annotation.feature_id IN (SELECT f.feature_id  FROM feature f WHERE f.feature_type_id =202 AND f.dataset_id IN (34465, 34978));
+#DELETE from location where location.feature_id IN (SELECT f.feature_id  FROM feature f WHERE f.feature_type_id =202 AND f.dataset_id IN (34465, 34978));
+
+#DELETE from feature_name where feature_name.feature_id IN (SELECT f.feature_id  FROM feature f WHERE f.feature_type_id =202 AND f.dataset_id IN (34465, 34978));
+#DELETE FROM feature WHERE feature.feature_id IN (
+# SELECT fid from (
+#    SELECT f.feature_id as fid
+#    FROM feature f
+#    WHERE f.feature_type_id =202
+#    AND f.dataset_id
+#    IN ( 34465, 34978 )
+# ) as foo
+#);
 
 exit() unless $ds1 && $ds2;
 
@@ -25,6 +38,7 @@ my $dsq = $coge->resultset('Dataset')->find($ds1);
 my $dss = $coge->resultset('Dataset')->find($ds2);
 my ($anno_type) = $coge->resultset('AnnotationType')->search({name=>"note"});
 my $cns_id = 202; #feature type id
+my $anno_id = 16156;
 
 print STDERR $dsq->dataset_id . "\n";
 print STDERR $dss->dataset_id . "\n";
@@ -35,70 +49,64 @@ my %data;
 my %annos;
 my %seen;
 # line like:
-# OS02G04490,SB10G029285, 02, 10, 1986128,1986156,59117428,59117400|1986133,1986163,59117429,59117399|1992129,1992147,59096365,59096347|1984218,1984236,59096368,59096350|1992128,1992143,59096365,59096350|1982831,1982849,59113429,59113411|1995931,1995957,59090974,59090948|1993267,1993287,59103979,59103959|2001316,2001330,59109300,59109286|1982780,1982794,59109486,59109472|1978893,1978907,59110202,59110188|2000840,2000857,59114956,59114939|1979357,1979377,59116046,59116026|1986130,1986153,59117429,59117406
+#qname   qchr    qstart  qstop   qstrand sname   schr    sstart  sstop   sstrand
+my $outfile = "cns_fids_$ds1" . "_" . "$ds2.txt";
+print "printing ids to: " . $outfile . "\n";
+open(SAVEFIDS, ">", $outfile);
+
 while (my $line = <>) {
-    next if $line =~ /^#/;
+    next if $line =~ /^qname/ || $line =~ /^#/;
     chomp $line;
     $line =~ s/,\s/,/g;
-    my ($qname, $sname, $qchr, $schr, @line)= split(/[,|\|]/, $line);
+    #my ($qname, $sname, $qchr, $schr, @line)= split(/[,|\|]/, $line);
+    my ($qname, $qchr, $qstart, $qstop, $qstrand, $sname, $schr, $sstart, $sstop, $sstrand) = split(/\t/, $line);
     $qchr =~ s/^0+//;
     $schr =~ s/^0+//;
 
-    my $tot = 0;
-    for(my $i=0; $i<scalar(@line); $i+=4){
-        my $strand = 1;
-        $tot++;
-        my ($qstart, $qstop, $sstart, $sstop) = @line[$i .. $i + 4];
-        print STDERR "$qname, $sname, $qchr, $schr, $qstart, qstop: $qstop, $sstart, $sstop\n";
-        if ($sstart > $sstop){ 
-            $strand = -1;
-            ($sstart, $sstop) = ($sstop, $sstart);
-        }
+    if ($seen{ $qchr . ", " . $qstart . "," . $qstop . "," . $schr . "," . $sstart . "," . $sstop }){ next; }
+    $seen{ $qstart . "," . $qstop . "," . $sstart . "," . $sstop } = 1;
+    
+  	my $qfeat = $dsq->add_to_features({
+				 feature_type_id => $cns_id,
+				 start=>$qstart, stop=>$qstop,
+				 chromosome=>$qchr, strand=>$qstrand,
+    }) if $GO;
+  	my $sfeat = $dss->add_to_features({
+				 feature_type_id => $cns_id,
+				 start=>$sstart, stop=>$sstop,
+				 chromosome=>$schr, strand=>$sstrand,
+    }) if $GO;
 
-        # dont do repeats.
-        if ($qseen{ $qstart . "," . $qstop . "," . $sstart . "," . $sstop }){ next; }
-        $seen{ $qstart . "," . $qstop . "," . $sstart . "," . $sstop } = 1;
-        
-  	    my $qfeat = $dsq->add_to_features({
-					 feature_type_id => $cns_id,
-					 start=>$qstart, stop=>$qstop,
-					 chromosome=>$qchr, strand=>$strand,
-        }) if $GO;
-  	    my $sfeat = $dss->add_to_features({
-					 feature_type_id => $cns_id,
-					 start=>$sstart, stop=>$sstop,
-					 chromosome=>$schr, strand=>$strand,
-        }) if $GO;
+	my $qloc = $qfeat->add_to_locations( {
+					   start      => $qstart,
+					   stop       => $qstop,
+					   strand     => $qstrand,
+					   chromosome => $qchr
+					  }) if $GO;
+	my $sloc = $sfeat->add_to_locations( {
+					   start      => $sstart,
+					   stop       => $sstop,
+					   strand     => $sstrand,
+					   chromosome => $schr
+					  }) if $GO;
 
-	    my $qloc = $qfeat->add_to_locations( {
-						   start      => $qstart,
-						   stop       => $qstop,
-						   strand     => $strand,
-						   chromosome => $qchr
-						  }) if $GO;
-	    my $sloc = $sfeat->add_to_locations( {
-						   start      => $sstart,
-						   stop       => $sstop,
-						   strand     => $strand,
-						   chromosome => $schr
-						  }) if $GO;
+    my $qfeat_name = $qfeat->add_to_feature_names({
+					     name=>"CNS $qname with $sname",
+					    }) if $GO ;
+    my $sfeat_name = $sfeat->add_to_feature_names({
+					     name=>"CNS $sname with $qname",
+					    }) if $GO ;
 
-        my $qfeat_name = $qfeat->add_to_feature_names({
-						     name=>$qname . "_CNS_" . $i/4,
-						    }) if $GO ;
-        my $sfeat_name = $sfeat->add_to_feature_names({
-						     name=>$sname . "_CNS_" . $i/4,
-						    }) if $GO ;
-
-        my $qannos = $qfeat->add_to_annotations({
-						 annotation=>"CNS for $qname, $sname",
-						 annotation_type_id=>$anno_type->id,
-						    }) if $GO ;
-        my $sannos = $sfeat->add_to_annotations({
-						     annotation=>"CNS for $qname, $sname"
-						    }) if $GO ;
-        
-        print $qfeat->feature_id . "\n" if $qfeat;
-        print $sfeat->feature_id . "\n" if $qfeat;
-      }
+    my $qannos = $qfeat->add_to_annotations({
+					 annotation=>"CNS for $qname, $sname",
+					 annotation_type_id=>$anno_id,
+					    }) if $GO ;
+    my $sannos = $sfeat->add_to_annotations({
+					     annotation=>"CNS for $qname, $sname",
+					     annotation_type_id=>$anno_id,
+					    }) if $GO ;
+    
+    print SAVEFIDS $qfeat->feature_id . "\n" if $qfeat;
+    print SAVEFIDS $sfeat->feature_id . "\n" if $sfeat;
+     
 }
