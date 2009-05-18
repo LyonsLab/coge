e3acbcd55ee21d645dd5986634bb996680bed44e
Eric Lyons <lyonsden@gmail.com>
2009-05-18 23:55:22 +0000

    initial import

---
+++ modules/Database/scripts/add_dataset_to_dataset_group.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+use strict;
+use CoGeX;
+use Getopt::Long;
+
+
+
+my $dsid;
+my $dsgid;
+
+my $connstr = 'dbi:mysql:dbname=coge;host=homer;port=3306';
+my$coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
+
+GetOptions ( "dsid=i"=>\$dsid,
+	     "dsgid=i"=>\$dsgid,
+	     );
+unless ($dsid && $dsgid)
+  {
+    print qq{
+USAGE  $0 -dsid <dataset id> -dsgid <dataset group id>
+
+Purpose:  Add the approprate row to the dataset_connector table so a dataset is in a dataset_group
+};
+    exit;
+  }
+
+my $res = $coge->resultset('DatasetConnector')->find_or_create({dataset_id=>$dsid, dataset_group_id=>$dsgid});
+
+if ($res)
+  {
+    print "Created dataset connector with database id: ".$res->id,"\n";
+  }
+else
+  {
+    print "Failed to create dataset connector\n";
+  }
