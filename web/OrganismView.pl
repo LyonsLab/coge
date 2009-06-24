80e32ca4f22e6b2d8fd95aac1aee5840c5bc0873
Eric Lyons <lyonsden@gmail.com>
2009-06-24 22:22:11 +0000

    added page title

--- web/OrganismView.pl
+++ web/OrganismView.pl
@@ -73,6 +73,7 @@ sub gen_html
 	my ($body, $seq_names, $seqs) = gen_body();
 	my $template = HTML::Template->new(filename=>'/opt/apache/CoGe/tmpl/generic_page.tmpl');
 	$template->param(TITLE=>'Organism Overview');
+	$template->param(PAGE_TITLE=>'OrgView');
 	$template->param(HEAD=>qq{});
 	my $name = $USER->user_name;
         $name = $USER->first_name if $USER->first_name;
