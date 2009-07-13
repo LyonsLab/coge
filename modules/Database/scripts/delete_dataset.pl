bb5f6c38c8fd64048ae3b9293f7d55a9d5a14bf1
Eric Lyons <lyonsden@gmail.com>
2009-07-13 21:57:16 +0000

    changed db to biocon

--- modules/Database/scripts/delete_dataset.pl
+++ modules/Database/scripts/delete_dataset.pl
@@ -14,7 +14,7 @@ This program will remove the specified dataset and all related data from the CoG
     exit;
   }
 
-my $connstr = 'dbi:mysql:dbname=coge;host=homer;port=3306';
+my $connstr = 'dbi:mysql:dbname=coge;host=biocon.berkeley.edu;port=3306';
 my $coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
 my ($ds) = $coge->resultset('Dataset')->resolve($dsid);
 unless ($ds)
