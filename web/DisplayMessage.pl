d728220f062570b61304059119ad86057a21e4ae
Eric Lyons <lyonsden@gmail.com>
2009-08-07 15:23:03 +0000

    initial import

---
+++ web/DisplayMessage.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use strict;
+use CGI;
+
+$ENV{PATH} = "/opt/apache2/CoGe/";
+
+my $FORM = new CGI;
+
+my $text = $FORM->param('text');
+$text = qq{<div style="
+  float: left;
+  width: 300px;
+  height: 182px;
+  background-image: url(/CoGe/picts/animations/DNA_orbit_animated_small-side.gif);
+">
+<span style="
+  font-style: oblique;
+  color: #990000;
+  font-size: large;
+  text-decoration: blink;
+">
+Loading. . .
+</span></div>} unless $text;
+
+print $FORM->header;
+print $text;
