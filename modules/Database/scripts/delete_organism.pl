0ba112fb22feb391a475735347ecaeb9d626c8c8
Eric Lyons <lyonsden@gmail.com>
2009-08-20 21:18:35 +0000

    updated delete sequence

--- modules/Database/scripts/delete_organism.pl
+++ modules/Database/scripts/delete_organism.pl
@@ -22,7 +22,23 @@ print "Do you want to delete:  \n\t".$org->name.": ".$org->description."?\n";
 my $var = <STDIN>;
 if ($var =~ /y/i)
   {
-    print "Deleteing. . .";
+    print "Initiating delete protocol.\n";
+    foreach my $dsg ($org->dataset_groups)
+      {
+	foreach my $ds ($dsg->datasets)
+	  {
+	    print "Deleting dataset: ".$ds->name,"\n";
+	    $ds->delete;
+	  }
+	my $path = $dsg->file_path;
+	$path =~ s/[^\/]*$//;
+	my $cmd = "rm -rf $path";
+	print "Deleting genomic sequence: $cmd\n";
+	`$cmd`;
+	print "Deleteing dataset group\n";
+	$dsg->delete;
+      }
+    print "Deleting organism\n";
     $org->delete;
     print "completed!\n";
   }
