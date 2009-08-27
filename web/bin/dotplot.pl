09a2e4040c1d895e47ee249251ffe14e36786692
Eric Lyons <lyonsden@gmail.com>
2009-08-27 17:03:09 +0000

    darkened yellow color

--- web/bin/dotplot.pl
+++ web/bin/dotplot.pl
@@ -662,7 +662,7 @@ sub get_color
     my @colors = (
 		  [255,0,0], #red
 		  [255,126,0], #orange
-		  [255,255,0], #yellow
+		  [200,200,0], #yellow
 		  [0,255,0], # green
 		  [0,255,255], # cyan
 		  [0,0,255], # blue
