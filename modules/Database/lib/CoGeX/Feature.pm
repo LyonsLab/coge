86cffb17f2339457cba132d9425bd1bd6e9b6e4a
Eric Lyons <lyonsden@gmail.com>
2009-09-04 16:55:06 +0000

    updated outout of fasta to use fid

--- modules/Database/lib/CoGeX/Feature.pm
+++ modules/Database/lib/CoGeX/Feature.pm
@@ -1527,13 +1527,16 @@ sub fasta
     my ($pri_name) = $self->primary_name;
     my $head = ">";
     if ($name_only)
-      {$head .= $pri_name;}
+      {
+	$head = "fid:".$self->id." ".$head if $add_fid;
+	$head .= $pri_name;
+      }
     else
       {
 	$head .= $self->dataset->organism->name."(v".$self->version;
 	$head .= ", ".$gst->name if $gst;
 	$head .= ")".", Name: ".(join (", ", $self->names)).", Type: ".$self->type->name.", Feature Location: (Chr: ".$self->chromosome.", ".$self->genbank_location_string.")";
-	$head = "fid:".$self->id." ".$head if $add_fid;
+	$head .= "fid:".$self->id." ".$head if $add_fid;
 	$head .= " +up: $upstream" if $upstream;
 	$head .= " +down: $downstream" if $downstream;
 	$head .= " (reverse complement)" if $rc;
