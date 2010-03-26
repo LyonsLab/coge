6fed7b70e7e0c5c79baad4202508c013811e0129
Eric Lyons <lyonsden@gmail.com>
2010-03-26 11:21:52 +0000

    added routine to specifically handle blast reports from genomic squence.  Blast name for them need to be converted to include position iformation, otherwise the local finder matches everything to the contigs

--- web/bin/SynMap/blast2bed.pl
+++ web/bin/SynMap/blast2bed.pl
@@ -13,8 +13,13 @@ GetOptions (
 	    "outfile2=s"    => \$outfile2,
 	   );
 
+
+convert_blast_genomic_names($infile) if ($infile =~ /genomic/);
+
 open (OUT1, ">$outfile1") || die "Can't open $outfile1 for writing: $!";
 open (OUT2, ">$outfile2") || die "Can't open $outfile2 for writing: $!";
+
+
 open (IN, $infile)  || die "Can't open $infile for reading: $!";
 my %seen1 = ();
 my %seen2 = ();
@@ -30,11 +35,52 @@ while (<IN>)
     $item1[2] = $line[7] unless defined $item1[2];
     $item2[1] = $line[8] unless defined $item2[1];
     $item2[2] = $line[9] unless defined $item2[2];
-    print OUT1 join ("\t", $item1[0], $item1[1], $item1[2], $line[0]),"\n" unless $seen1{$line[0]};
-    print OUT2 join ("\t", $item2[0], $item2[1], $item2[2], $line[1]),"\n" unless $seen2{$line[1]};
-    $seen1{$line[0]} =1;
-    $seen2{$line[1]} =1;
+
+    #genomic comparisons need a different name as the default one won't distinguish between blast hits
+    my $name1 = $item1[3] ? $line[0] : join ("_", @item1);
+    my $name2 = $item2[3] ? $line[1] : join ("_", @item2);
+
+    print OUT1 join ("\t", $item1[0], $item1[1], $item1[2], $line[0]),"\n" unless $seen1{$name1};
+    print OUT2 join ("\t", $item2[0], $item2[1], $item2[2], $line[1]),"\n" unless $seen2{$name2};
+    $seen1{$name1} =1;
+    $seen2{$name2} =1;
   }
 close IN;
 close OUT1;
 close OUT2;
+
+sub convert_blast_genomic_names
+  {
+    my $infile = shift;
+    open (OUT, ">$infile.tmp") || die "Can't open $infile.tmp for writing: $!";
+    open (IN, $infile)  || die "Can't open $infile for reading: $!";
+    my $count=1;
+    while (<IN>)
+      {
+	chomp;
+	next unless $_;
+	my @line = split/\t/;
+	my @item1 = split/\|\|/, $line[0];
+	my @item2 = split/\|\|/, $line[1];
+	#add start and stop positions to items if not present (e.g. genomci sequence hits)  
+	unless (defined $item1[1] && defined $item1[2])
+	  {
+	    $item1[1] = $line[6] unless defined $item1[1];
+	    $item1[2] = $line[7] unless defined $item1[2];
+	    ($item1[1], $item1[2]) = ($item1[2], $item1[1]) if $item1[1]>$item1[2];
+	  }
+	unless (defined $item2[1] && defined $item2[2])
+	  {
+	    $item2[1] = $line[8] unless defined $item2[1];
+	    $item2[2] = $line[9] unless defined $item2[2];
+	    ($item2[1], $item2[2]) = ($item2[2], $item2[1]) if $item2[1]>$item2[2];
+	  }
+	my $name1 = $item1[3] ? $line[0] : join ("||", @item1);
+	my $name2 = $item2[3] ? $line[1] : join ("||", @item2);
+	$line[0] = $name1;
+	$line[1] = $name2;
+	print OUT join "\t", @line,"\n";
+		$count++;
+      }
+    `mv $infile.tmp $infile`;
+  }
