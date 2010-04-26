6591051c9a7f8bfadeee655f18c0205cd197054a
bpederse <bpederse@b0c93bf3-8215-4e60-8b05-a344dd04285b>
2010-04-26 22:37:03 +0000

    add organism to db

--- modules/Database/scripts/simle2sqlite.py
+++ modules/Database/scripts/simle2sqlite.py
@@ -2,27 +2,30 @@ import sys
 import sqlite3
 
 f = sys.argv[1]
-db = sqlite3.connect(f + ".sqlite")
+org = sys.argv[2]
+fdb = sys.argv[3]
+db = sqlite3.connect(fdb)
 c = db.cursor()
 c.execute("""\
 CREATE TABLE simple (
    id INTEGER PRIMARY KEY,
+   organism TEXT,
    seqid TEXT,
    name TEXT,
    start INTEGER,
    end INTEGER,
    strand TEXT,
    type TEXT,
-   UNIQUE(seqid, start, end, type)
+   UNIQUE(organism, seqid, start, end, type)
 )""")
 
-def geninserts(f):
+def geninserts(f, org):
     for line in open(f):
         seqid, ftype, start, end, strand, name = line.rstrip().split(",")
         start, end = map(int, (start, end))
-        yield seqid, name, start, end, strand, ftype
+        yield org, seqid, name, start, end, strand, ftype
 
-c.executemany("INSERT INTO simple(seqid, name, start, end, strand, type) VALUES"
-              "(?, ?, ?, ?, ?, ?)", geninserts(f))
+c.executemany("INSERT INTO simple(organism, seqid, name, start, end, strand, type) VALUES"
+              "(?, ?, ?, ?, ?, ?, ?)", geninserts(f, org))
 db.commit()
 db.close()
