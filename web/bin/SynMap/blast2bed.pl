9092c670ccb7fa68cd8b16de9dce46105c4670c3
Eric Lyons <lyonsden@gmail.com>
2010-05-03 15:36:56 +0000

    Added code to add correct strand information of HIT to genomic reports.  Takes into account orientation of the query gene if available

--- web/bin/SynMap/blast2bed.pl
+++ web/bin/SynMap/blast2bed.pl
@@ -63,20 +63,48 @@ sub convert_blast_genomic_names
 	my @item1 = split/\|\|/, $line[0];
 	my @item2 = split/\|\|/, $line[1];
 	#add start and stop positions to items if not present (e.g. genomci sequence hits)  
-	unless (defined $item1[1] && defined $item1[2])
+	my ($ori1, $ori2);
+	$ori1 = $item1[4];
+	$ori2 = $item2[4];
+	unless (defined $item1[1] && defined $item1[2] && defined $item1[4])
 	  {
 	    $item1[1] = $line[6] unless defined $item1[1];
 	    $item1[2] = $line[7] unless defined $item1[2];
 	    ($item1[1], $item1[2]) = ($item1[2], $item1[1]) if $item1[1]>$item1[2];
+	    #determine orientation of the hit relative to the gene (if present)
+	    $ori1 = $line[6] > $line[7] ? -1 : 1;
 	  }
-	unless (defined $item2[1] && defined $item2[2])
+#	$ori1 = $item1[4] unless $ori1;
+	unless (defined $item2[1] && defined $item2[2] && defined $item2[4])
 	  {
 	    $item2[1] = $line[8] unless defined $item2[1];
 	    $item2[2] = $line[9] unless defined $item2[2];
 	    ($item2[1], $item2[2]) = ($item2[2], $item2[1]) if $item2[1]>$item2[2];
+	    $ori2 = $line[8] > $line[9] ? -1 : 1;
+#	    print STDERR $ori2,"!\n";
 	  }
-	my $name1 = $item1[3] ? $line[0] : join ("||", @item1);
-	my $name2 = $item2[3] ? $line[1] : join ("||", @item2);
+	#working here on determining orientation of genomic blast hits to add into file.  Needs to set equal (1,1) or (-1,-1) if in the same orientation
+#	 /opt/apache/CoGe//bin/SynMap/blast2bed.pl -infile /opt/apache/CoGe//diags/Zea_mays_maize_corn/Zea_mays_maize_corn/8082_9106.CDS-genomic.blastn.blast -outfile1 /opt/apache/CoGe//diags/Zea_mays_maize_corn/Zea_mays_maize_corn/8082_9106.CDS-genomic.blastn.blast.q.bed -outfile2 /opt/apache/CoGe//diags/Zea_mays_maize_corn/Zea_mays_maize_corn/8082_9106.CDS-genomic.blastn.blast.s.bed
+#	$ori2 = $item2[4] unless $ori2;
+#	print STDERR $ori1, "\t", $ori2,"\n";
+	my $ori = $ori1 eq $ori2 ? 1 : -1;
+#	print $ori,"\n";
+	$item1[3] = "genomic_hit" unless $item1[3];
+	$item2[3] = "genomic_hit" unless $item2[3];
+	unless ($item1[4])
+	  {
+	    $ori1 = $ori eq 1 ? $ori2 : $ori2*-1;
+	  }
+	unless ($item2[4])
+	  {
+	    $ori2 = $ori eq 1 ? $ori1 : $ori1*-1;
+	  }
+	$item1[4] = $ori1 unless $item1[4];
+	$item2[4] = $ori2 unless $item2[4];
+	my $name1 = $item1[5] ? $line[0] : join ("||", @item1);
+	my $name2 = $item2[5] ? $line[1] : join ("||", @item2);
+#	print $name1,"\n";
+#	print $name2,"\n";
 	$line[0] = $name1;
 	$line[1] = $name2;
 	print OUT join "\t", @line,"\n";
