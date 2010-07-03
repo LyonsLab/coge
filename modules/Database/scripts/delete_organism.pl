a9ef70a031a21a9807c0d70f704256c881f756c5
Eric Lyons <lyonsden@gmail.com>
2010-07-03 18:00:14 +0000

    updated db

--- modules/Database/scripts/delete_organism.pl
+++ modules/Database/scripts/delete_organism.pl
@@ -14,7 +14,7 @@ This program will remove the specified organism and all related data from the Co
     exit;
   }
 
-my $connstr = 'dbi:mysql:dbname=coge;host=biocon.berkeley.edu;port=3306';
+my $connstr = 'dbi:mysql:dbname=coge;host=toxic.berkeley.edu;port=3306';
 my $coge = CoGeX->connect($connstr, 'cnssys', 'CnS' );
 my $org = $coge->resultset('Organism')->resolve($orgid);
 
