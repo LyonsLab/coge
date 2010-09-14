b56a667a5b33bb19022ba8e653042b11a2202889
Eric Lyons <lyonsden@gmail.com>
2010-09-14 22:04:09 +0000

    added more color schemes

--- web/bin/ks_histogram.pl
+++ web/bin/ks_histogram.pl
@@ -178,7 +178,7 @@ sub get_color
     my %opts = @_;
     my $val = $opts{val};
     return [0,0,0] unless defined $val;
-    my @colors = (
+    my @colors_orig = (
 		  [255,255,0], #yellow
 		  [200,200,0], # green
 		  [0,200,0], # green
@@ -192,6 +192,22 @@ sub get_color
 		  [200,100,0], #orange
 		  [255,126,0], #orange
 		 );
+     my @rainbow = (
+                  [255,0,0], #red
+                  [255,255,0], #yellow
+                  [0,255,0], # green
+                  [0,255,255], # cyan
+		  [220,0,220], #magenta
+                  [0,0,255], # blue
+                 );
+    my @red_yellow_blue = (
+                  [0,0,150], # blue
+                  [220,220,20], #yellow
+                  [255,0,0], #red
+                 );
+#    my @colors = @colors_orig;
+    my @colors = @rainbow;
+#    my @colors = @red_yellow_blue;
     @colors = reverse @colors;
     my ($index1, $index2) = ((floor((scalar(@colors)-1)*$val)), ceil((scalar(@colors)-1)*$val));
 
