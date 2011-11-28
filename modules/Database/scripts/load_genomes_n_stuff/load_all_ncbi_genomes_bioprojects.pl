34ff0e13519d1950cc4619d5135411651b8f4fbd
Eric Lyons <lyonsden@gmail.com>
2011-11-28 00:04:56 +0000

    updated to be parallel processing,dumpin output to a log file set a max number of sub entries in a wgs file to 10,000 before it is skipped by the genome loading program

--- modules/Database/scripts/load_genomes_n_stuff/load_all_ncbi_genomes_bioprojects.pl
+++ modules/Database/scripts/load_genomes_n_stuff/load_all_ncbi_genomes_bioprojects.pl
@@ -4,25 +4,43 @@ use strict;
 use LWP::Simple;
 use Data::Dumper;
 use XML::Simple;
+use Parallel::ForkManager;
 
 
 my $bpids = get_NCBI_bioprj();
 
-foreach my $bpid (@$bpids)
-  {
-    next unless check_bioprj_genome($bpid);
-    my $ntids = get_bioprj_nt($bpid);
-    my @accns;
-    foreach my $ntid (@$ntids)
-      {
-	my $accn = get_nt_accn($ntid);
-	push @accns, $accn;
-      }
-    next unless @accns;
-    my $processed_accns = process_accns(\@accns);
-    run_load_genome($processed_accns);
-  }
+my $pm = new Parallel::ForkManager(10);
+my $out = "Load_all_NCBI.".sprintf( "%04d-%02d-%02d-%02d:%02d:%02d",
+                 sub { ($_[5]+1900, $_[4]+1, $_[3]),$_[2],$_[1],$_[0] }->(localtime)).".log";
 
+
+foreach my $bpid (@$bpids)
+  { 
+    my $pid = $pm->start and next;
+    my $output;
+    $output.= "BioProject $bpid Started\n";
+    if (check_bioprj_genome($bpid))
+      {
+	my $ntids = get_bioprj_nt($bpid);
+	my @accns;
+	foreach my $ntid (@$ntids)
+	  {
+	    my $accn = get_nt_accn($ntid);
+	    push @accns, $accn;
+	  }
+	if (@accns)
+	  {
+	    my $processed_accns = process_accns(\@accns, \$output);
+	    run_load_genome($processed_accns, \$output);
+	  }
+      }
+    $output .= "BioProject $bpid Finished\n";
+    open (OUT, ">>$out");
+    print OUT $output,"\n\n";;
+    close OUT;
+    $pm->finish;
+  }
+$pm->wait_all_children;
 
 sub get_NCBI_bioprj
   {
@@ -77,7 +95,8 @@ sub get_nt_accn
 sub process_accns
       {
 	my $accns = shift;
-	print join ("\t", @$accns),"!\n";
+	my $output = shift;
+	$$output .= join ("\t", @$accns)."!\n";
 	my $found=0;
 	foreach my $accn (@$accns)
 	  {
@@ -85,9 +104,10 @@ sub process_accns
 	  }
 	if ($found)
 	  {
-	    print "#"x20,"\n";
-	    print $found,"\n";
-	    print "#"x20,"\n";
+	    
+	    $$output .= ("#"x20)."\n";
+	    $$output .= $found."\n";
+	    $$output .= ("#"x20)."\n";
 	    return [$found];
 	  }
 	return $accns;
@@ -96,20 +116,22 @@ sub process_accns
 sub run_load_genome
   {
     my $accns = shift;
+    my $output = shift;
     my $prog = '/home/elyons/projects/CoGeX/scripts/load_genomes_n_stuff/genbank_genome_loader.pl';
 #    $prog .= " -autoupdate";
     $prog .= " -td '/tmp/gb/'";
     $prog .= " -go";
     $prog .= " -autoskip";
     $prog .= " -delete_src_file";
+    $prog .= " -max_entries 10000";
     my $run = $prog." -accn ".join (" -accn ", @$accns);
-    print $run,"\n";
+    $$output .=  $run."\n";
     open (IN, $run." |");
     while (<IN>)
       {
 #	$previously_loaded =1 if /previously loaded/;
 #	$skipped=1 if /skipping/;
-	print $_;
+	$$output .= $_;
       }
     close IN;
   }
