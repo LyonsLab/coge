e426792c9fcf2086a83a73e76741eb8cfd3cad35
Eric Lyons <lyonsden@gmail.com>
2011-12-05 20:25:53 +0000

    fixed errors in migrating to QuantitationExperiment

--- modules/Database/lib/CoGeX/Result/QuantitationExperiment.pm
+++ modules/Database/lib/CoGeX/Result/QuantitationExperiment.pm
@@ -1,4 +1,4 @@
-package CoGeX::Result::QuantitationType;
+package CoGeX::Result::QuantitationExperiment;
 
 # Created by DBIx::Class::Schema::Loader v0.03009 @ 2006-12-01 18:13:38
 
@@ -9,16 +9,16 @@ use base 'DBIx::Class::Core';
 
 =head1 NAME
 
-CoGeX::QuantitationType
+CoGeX::QuantitationExperiment
 
 =head1 SYNOPSIS
 
-This object uses the DBIx::Class to define an interface to the C<quantitation_type> table in the CoGe database.
+This object uses the DBIx::Class to define an interface to the C<quantitation_experiment> table in the CoGe database.
 
 =head1 DESCRIPTION
 
 Has columns:
-C<quantitation_type_id> B<Primary Key>
+C<quantitation_experiment_id> B<Primary Key>
 Type:INT, Default: undef, Nullable: no, Size: 11
 
 C<name>
@@ -26,13 +26,11 @@ Type: VARCHAR, Default: "", Nullable: no, Size: 100
 
 C<description>
 Type: VARCHAR, Default: undef, Nullable: yes, Size: 255
-
-C<quantitation_type_group_id>
 Type: INT, Default: undef, Nullable: yes, Size: 11
 
 
-Belongs to CCoGeX::Result::QuantitationTypeGroup> via C<quantitation_type_group_id>
-Has many CCoGeX::Result::Quantitation> via C<quantitation_type_id>
+
+Has many CCoGeX::Result::Quantitation> via C<quantitation_experiment_id>
 
 =head1 USAGE
 
@@ -42,9 +40,9 @@ Has many CCoGeX::Result::Quantitation> via C<quantitation_type_id>
 
 =cut
 
-__PACKAGE__->table("quantitation_type");
+__PACKAGE__->table("quantitation_experiment");
 __PACKAGE__->add_columns(
-  "quantitation_type_id",
+  "quantitation_experiment_id",
   { data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
   "name",
   { data_type => "VARCHAR", default_value => "", is_nullable => 0, size => 1024 },
@@ -54,38 +52,12 @@ __PACKAGE__->add_columns(
     default_value => undef,
     is_nullable => 1,
   },
-  "quantitation_type_group_id",
-  { data_type => "INT", default_value => undef, is_nullable => 1, size => 11 },
 );
-__PACKAGE__->set_primary_key("quantitation_type_id");
+__PACKAGE__->set_primary_key("quantitation_experiment_id");
 
-__PACKAGE__->has_many("quantitations" => "CoGeX::Result::Quantitation", 'quantitation_type_id');
-
-__PACKAGE__->belongs_to("quantitation_type_group" => "CoGeX::Result::QuantitationTypeGroup", 'quantitation_type_group_id');
+__PACKAGE__->has_many("quantitations" => "CoGeX::Result::Quantitation", 'quantitation_experiment_id');
 
 
-
-################################################ subroutine header begin ##
-
-=head2 group
-
- Usage     : $QuantitationType_obj->group->QuantitationTypeGroup_object_method_or_value
- Purpose   : Returns the QuantitationTypeGroup object associated with this QuantitationType object.
- Returns   : QuantitationTypeGroup object
- Argument  : 
- Throws    : None
- Comments  : 
-
-See Also   : 
-
-=cut
-
-################################################## subroutine header end ##
-
-sub group
-  {
-    shift->quantitation_type_group(@_);
-  }
 
 sub desc
     {
