3f3520591bd2fb126341e764d46bec89390dc4f9
Eric Lyons <lyonsden@gmail.com>
2011-12-20 23:23:57 +0000

    initial import

---
+++ modules/Graphics/lib/CoGe/Graphics/Feature/Quant.pm
@@ -0,0 +1,178 @@
+package CoGe::Graphics::Feature::Quant;
+use strict;
+use base qw(CoGe::Graphics::Feature);
+
+=head1 AUTHOR
+
+	Eric Lyons
+	elyons@nature.berkeley.edu
+
+=head1 COPYRIGHT
+
+Permission to use, copy, modify, and distribute this software and its documentation for educational, research, and not-for-profit purposes, without fee and without a signed licensing agreement, is hereby granted, provided that the above copyright notice, this paragraph and the following two paragraphs appear in all copies, modifications, and distributions. Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, for commercial licensing opportunities.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+
+
+BEGIN {
+    use vars qw($VERSION $HEIGHT $WIDTH);
+    $VERSION     = '0.1';
+    $HEIGHT = 5;
+    $WIDTH = 5;
+    __PACKAGE__->mk_accessors(
+"val",
+"segments",
+);
+}
+
+sub start
+  {
+    my $self = shift;
+    my $val = shift;
+    if ($val) {$self->segments->[0][0] = $val;}
+    return unless $self->segments;
+    return $self->segments->[0][0];
+  }
+
+sub stop
+  {
+    my $self = shift;
+    my $val = shift;
+    if ($val) {$self->segments->[-1][-1] = $val;}
+    return unless $self->segments;
+    return $self->segments->[-1][-1];
+  }
+
+sub add_segment
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $start = $opts{start} || $opts{begin} || $opts{START} || $opts{BEGIN};
+    my $stop = $opts{stop} || $opts{end} || $opts{STOP} || $opts{END};
+#    use Data::Dumper;
+#    print STDERR Dumper \%opts;
+#    print STDERR "$start - $stop\n";
+    if ($start > $stop)
+      {
+        my $tmp = $start;
+        $start = $stop;
+        $stop = $tmp;
+      }
+#    $start = 1 if $start < 1;
+    return unless $start && $stop;
+    my @segs;
+    push @segs,  @{$self->segments} if $self->segments;
+    push @segs, [$start, $stop];
+    $self->segments([sort {$a->[0]<=>$b->[0]} @segs]);
+  }
+
+sub _initialize
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $h = $HEIGHT; #total image height 
+    my $w = $WIDTH;
+    $self->image_width($w);
+    $self->image_height($h);
+    $self->bgcolor([255,255,255]) unless $self->bgcolor;
+    $self->fill(1);
+    $self->order(1);
+#    $self->stop($self->start + length $self->nt-1) unless $self->stop;
+    $self->skip_overlap_search(1); #make sure to skip searching for overlap for these guys.  Search can be slow
+    my $ga = 0;
+
+
+#    $self->color(\@color);
+#    $self->label($self->nt) if $self->nt && $self->show_label;
+    $self->type('quant');
+  }
+
+sub _post_initialize
+  {
+    my $self = shift;
+    my %opts = @_;
+    my $gd = $self->gd;
+    $gd->fill(0,0, $self->get_color($self->color));
+#    $gd->transparent($gd->colorResolve(255,255,255));
+  }
+
+#################### subroutine header begin ####################
+
+=head2 sample_function
+
+ Usage     : How to use this function/method
+ Purpose   : What it does
+ Returns   : What it returns
+ Argument  : What it wants to know
+ Throws    : Exceptions and other anomolies
+ Comment   : This is a sample subroutine header.
+           : It is polite to include more pod and fewer comments.
+
+See Also   : 
+
+=cut
+
+#################### subroutine header end ####################
+
+
+
+
+#################### main pod documentation begin ###################
+## Below is the stub of documentation for your module. 
+## You better edit it!
+
+
+=head1 NAME
+
+CoGe::Graphics::Feature::Base
+
+=head1 SYNOPSIS
+
+  use CoGe::Graphics::Feature::Base
+
+
+=head1 DESCRIPTION
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+	Eric Lyons
+	elyons@nature.berkeley.edu
+
+=head1 COPYRIGHT
+
+This program is free software licensed under the...
+
+	The Artistic License
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
