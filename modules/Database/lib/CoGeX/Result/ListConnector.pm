7cf027a5a370d846751b6c316547999d49b43fff
Eric Lyons <lyonsden@gmail.com>
2012-01-09 20:36:33 +0000

    initial import

---
+++ modules/Database/lib/CoGeX/Result/ListConnector.pm
@@ -0,0 +1,103 @@
+package CoGeX::Result::ListConnector;
+
+# Created by DBIx::Class::Schema::Loader v0.03009 @ 2006-12-01 18:13:38
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 NAME
+
+CoGeX::FeatureListConnector
+
+=head1 SYNOPSIS
+
+This object uses the DBIx::Class to define an interface to the C<feature_list_connector> table in the CoGe database.
+
+=head1 DESCRIPTION
+
+Has columns:
+C<feature_list_connector_id> (Primary Key)
+Type: INT, Default: undef, Nullable: no, Size: 10
+
+C<feature_list_id>
+Type: INT, Default: "", Nullable: no, Size: 10
+
+C<feature_id>
+Type: INT, Default: "", Nullable: no, Size: 10
+
+C<preferred_name>
+Type: VARCHAR, Default: undef, Nullable: yes, Size: 50
+
+C<description>
+Type: VARCHAR, Default: undef, Nullable: yes, Size: 1024
+
+=head1 USAGE
+
+  use CoGeX;
+
+=head1 METHODS
+
+=cut
+
+__PACKAGE__->table("feature_list_connector");
+__PACKAGE__->add_columns(
+  "feature_list_connector_id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 10 },
+  "feature_list_id",
+  { data_type => "INT", default_value => "", is_nullable => 0, size => 10 },
+  "feature_id",
+  { data_type => "INT", default_value => "", is_nullable => 0, size => 10 },
+  "preferred_name",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 50,
+  },
+  "description",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 1024,
+  },
+);
+__PACKAGE__->set_primary_key("feature_list_connector_id");
+__PACKAGE__->belongs_to("list" => "CoGe::Result::List", 'list_id');
+__PACKAGE__->belongs_to("feature" => "CoGe::Result::Feature", 'feature_id');
+__PACKAGE__->belongs_to("dataset_group" => "CoGe::Result::DatasetGroup", 'dataset_group_id');
+
+sub genome
+  {
+    return shift->dataset_group(@_);
+  }
+1;
+
+
+
+=head1 BUGS
+
+
+=head1 SUPPORT
+
+
+=head1 AUTHORS
+
+ Eric Lyons
+ Brent Pedersen
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+=cut
+
