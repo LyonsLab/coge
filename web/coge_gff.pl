379184eaa43bf39610353890c2ee7bfc740c2372
Eric Lyons <lyonsden@gmail.com>
2012-03-19 17:57:26 +0000

    updated with new options for which information is generated

--- web/coge_gff.pl
+++ web/coge_gff.pl
@@ -18,8 +18,13 @@ my $coge = CoGeX->connect($connstr, $DBUSER, $DBPASS );
 my $FORM = new CGI;
 my $dsgid = $FORM->param('dsgid');
 my $dsid = $FORM->param('dsid');
-my $annos = 0;
-$annos = $FORM->param('annos');
+my $annos = 0; #flag for printing annotations
+$annos = $FORM->param('annos') if $FORM->param('annos');
+my $cds = 0; #flag for printing only genes, mRNA, and CDSs
+$cds = $FORM->param('cds') if $FORM->param('cds');
+my $name_unique = 0;
+$name_unique = $FORM->param('nu') if $FORM->param('nu');
+
 my ($USER) = CoGe::Accessory::LogUser->get_user();
 
 my $item;
@@ -54,4 +59,4 @@ $header .= $org;
 $header .= $id;
 $header .=".gff\n\n";
 print $header;
-$item->gff(print=>1, annos=>$annos);
+$item->gff(print=>1, annos=>$annos, cds=>$cds, name_unique=>$name_unique);
