b2796ebbe71307799ea877f57fb80e493703f33e
Eric Lyons <lyonsden@gmail.com>
2012-06-12 15:40:13 +0000

    added a space to the organism title

--- web/GEvo.pl
+++ web/GEvo.pl
@@ -2362,7 +2362,7 @@ sub get_obj_from_genome_db
     my $gst = $coge->resultset('GenomicSequenceType')->find($gstid);
     my $org_name = $ds->organism->name();
     $org_name .= ": ".$dsg->description if $dsg->description;
-    $org_name .= "(".$ds->data_source->name." v".$ds->version.", ".$gst->name.")";
+    $org_name .= " (".$ds->data_source->name." v".$ds->version.", ".$gst->name.")";
     my $obj= new CoGe::Accessory::GenBank({
 					   accn=>$accn,
 					   locus=>$accn,
