7a3265b1c0363fdfb0901622291b683ab9827546
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-07-16 18:51:32 +0000

    Initial import

---
+++ modules/Database/lib/CoGeX/Result/ExperimentType.pm
@@ -0,0 +1,99 @@
+package CoGeX::Result::ExperimentType;
+
+# Created by DBIx::Class::Schema::Loader
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+
+=head1 NAME
+
+My::Schema::Result::ExperimentType
+
+=cut
+
+__PACKAGE__->table("experiment_type");
+
+=head1 ACCESSORS
+
+=head2 experiment_type_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  size: 11
+
+=head2 name
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 description
+
+  data_type: 'text'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+			 "experiment_type_id",
+			 { data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
+			 "name",
+			 { data_type => "VARCHAR", is_nullable => 0, size => 255 },
+			 "description",
+			 { data_type => "TEXT", is_nullable => 1 },
+			);
+__PACKAGE__->set_primary_key("experiment_type_id");
+__PACKAGE__->has_many('experiment_type_connector'=>"CoGeX::Result::ExperimentTypeConnector","experiment_type_id");
+
+
+################################################ subroutine header begin ##
+
+=head2 desc
+
+ Usage     : 
+ Purpose   : alias for $self->description
+ Returns   : 
+ Argument  : 
+ Throws    : 
+ Comments  : 
+
+See Also   : 
+
+=cut
+
+################################################## subroutine header end ##
+
+
+sub desc
+  {
+    return shift->description(@_);
+  }
+
+################################################ subroutine header begin ##
+
+=head2 experiments
+
+ Usage     : $self->experiments
+ Purpose   : pass through experiment_type_connector to fake a many-to-many connection with experiment
+ Returns   : 
+ Argument  : 
+ Throws    : 
+ Comments  : 
+
+See Also   : 
+
+=cut
+
+################################################## subroutine header end ##
+
+sub experiments
+ {
+   map {$_->experiment(@_)} shift->experiment_type_connectors();
+ }
+
+
+1;
