b3c2ddcb609d4a97469bdfd8913f161c439bc877
Eric Lyons <lyonsden@gmail.com>
2012-07-16 17:11:37 +0000

    initial import

---
+++ modules/Database/lib/CoGeX/ResultSet/List.pm
@@ -0,0 +1,33 @@
+  package CoGeX::ResultSet::List;
+
+  use strict;
+  use warnings;
+  use base 'DBIx::Class::ResultSet';
+
+################################################ subroutine header begin ##
+
+=head2 public_lists
+
+ Usage     : 
+ Purpose   : 
+ Returns   : 
+ Argument  : 
+ Throws    : 
+ Comments  : 
+           : 
+
+See Also   : 
+
+=cut
+
+################################################## subroutine header end ##
+
+
+sub public_lists
+  {
+    my $self = shift;
+    my %opts = @_;
+    return $self->search({public=>1});
+  }
+
+1;
