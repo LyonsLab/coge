8171afbbb1926dcc21a7851f9b795d261b69f813
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-08-27 19:22:57 +0000

    Non-functional changes

--- modules/Accessory/lib/CoGe/Accessory/LogUser.pm
+++ modules/Accessory/lib/CoGe/Accessory/LogUser.pm
@@ -5,45 +5,38 @@ use CGI::Cookie;
 use Data::Dumper;
 use CoGeX::Result::User;
 
-sub get_user
-  {
-    my $self = shift;
-    my %opts = @_;
-    my $cookie_name = $opts{cookie_name};
-    my $coge = $opts{coge};
-    my %cookies = fetch CGI::Cookie;
-    my ($user, $uid, $session);# = "Public";
-    if ($cookie_name && ref $cookies{$cookie_name})
-      {
-	my %session = $cookies{$cookie_name}->value;
-	$session = $session{session};
-	my ($user_session) = $coge->resultset("UserSession")->find({session=>$session});
-	$user = $user_session->user if $user_session;
-      }
-    unless ($user)
-      {
-	$user = new CoGeX::Result::User;
-	$user->user_name("public");
-      }
-    return ($user);
-  }
+sub get_user {
+	my $self        = shift;
+	my %opts        = @_;
+	my $cookie_name = $opts{cookie_name};
+	my $coge        = $opts{coge};
+	my %cookies     = fetch CGI::Cookie;
+	my ( $user, $uid, $session );    # = "Public";
+	if ( $cookie_name && ref $cookies{$cookie_name} ) {
+		my %session = $cookies{$cookie_name}->value;
+		$session = $session{session};
+		my ($user_session) = $coge->resultset("UserSession")->find( { session => $session } );
+		$user = $user_session->user if $user_session;
+	}
+	unless ($user) {
+		$user = new CoGeX::Result::User;
+		$user->user_name("public");
+	}
+	return ($user);
+}
 
-sub gen_cookie
-  {
-    my $self = shift;
-    my %opts = @_;
-    my $session = $opts{session} || 0;
-    my $exp = $opts{exp} || "+1D";
-    my $cookie_name = $opts{cookie_name};
-    my %params = (-name=>$cookie_name,
-		 -path=>"/");
+sub gen_cookie {
+	my $self        = shift;
+	my %opts        = @_;
+	my $session     = $opts{session} || 0;
+	my $exp         = $opts{exp} || "+1D";
+	my $cookie_name = $opts{cookie_name};
+	my %params      = ( -name => $cookie_name, -path => "/" );
 
-    $params{-expires} = $exp if $exp;
-    $params{-values}={session=>$session} if $session;
-    my $c = new CGI::Cookie(%params);
-    return $c;
-  }
-
-
+	$params{-expires} = $exp if $exp;
+	$params{ -values } = { session => $session } if $session;
+	my $c = new CGI::Cookie(%params);
+	return $c;
+}
 
 1;
