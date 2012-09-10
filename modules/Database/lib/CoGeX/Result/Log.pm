b3cf224cc584396af95473d43c40c783a58ca635
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-09-10 16:15:42 +0000

    Add feature to log certain user actions in the Log table.

---
+++ modules/Database/lib/CoGeX/Result/Log.pm
@@ -0,0 +1,37 @@
+package CoGeX::Result::Log;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+
+=head1 NAME
+
+My::Schema::Result::Log
+
+=cut
+
+__PACKAGE__->table("log");
+
+=head1 ACCESSORS
+
+=cut
+
+__PACKAGE__->add_columns(
+	"time",
+	{ data_type => "TIMESTAMP", default_value => undef, is_nullable => 0 },
+	"user_id",
+	{ data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
+#   "target", # table name
+#	{ data_type => "VARCHAR", default_value => undef, is_nullable => 1, size => 30 },
+	"page",
+	{ data_type => "VARCHAR", default_value => undef, is_nullable => 0, size => 255 },
+	"description",
+	{ data_type => "VARCHAR", default_value => undef, is_nullable => 1, size => 255 },
+);
+__PACKAGE__->belongs_to('user' => "CoGeX::Result::User", 'user_id');
+
+
+
+1;
