ca306a7afb278c00c641536d0e8474dc356c043c
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-09-25 19:21:55 +0000

    Fix to v5.

--- web/Groups.pl
+++ web/Groups.pl
@@ -107,10 +107,6 @@ sub gen_body {
 	my $groups = get_groups_for_user();
 	$template->param( MAIN_TABLE => $groups );
 	$template->param( ADMIN_AREA => 1 ) if $USER->is_admin;
-	my $ugid = $FORM->param('ugid');
-	my $box_open = $ugid ? 'true' : 'false';
-	$template->param( EDIT_BOX_OPEN    => $box_open );
-	$template->param( ADDITIONAL_STUFF => qq{edit_group({ugid: $ugid});} ) if $ugid;
 	
 	return $template->output;
 }
