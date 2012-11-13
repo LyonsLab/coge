fc041f9528baf05562d48f0865ade533106a8eb1
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-11-13 19:03:45 +0000

    Integrated SlickGrid.

--- web/History.pl
+++ web/History.pl
@@ -12,19 +12,21 @@ use Digest::MD5 qw(md5_base64);
 use URI::Escape;
 use Data::Dumper;
 use File::Path;
+use DBIx::Class::ResultClass::HashRefInflator;
 
 no warnings 'redefine';
 
-use vars qw($P $DBNAME $DBHOST $DBPORT $DBUSER $DBPASS $connstr $PAGE_NAME 
+use vars qw($P $DBNAME $DBHOST $DBPORT $DBUSER $DBPASS $connstr $PAGE_TITLE 
 			$TEMPDIR $USER $DATE $BASEFILE $coge $cogeweb %FUNCTION $COOKIE_NAME 
-			$FORM $URL $COGEDIR $TEMPDIR $TEMPURL);
+			$FORM $URL $COGEDIR $TEMPDIR $TEMPURL $MAX_RESULTS);
 $P = CoGe::Accessory::Web::get_defaults( $ENV{HOME} . 'coge.conf' );
-
 $DATE = sprintf(
 	"%04d-%02d-%02d %02d:%02d:%02d",
 	sub { ( $_[5] + 1900, $_[4] + 1, $_[3] ), $_[2], $_[1], $_[0] }->(localtime)
 );
-$PAGE_NAME = 'Groups.pl';
+$PAGE_TITLE = 'History';
+
+$MAX_RESULTS = 100;
 
 $FORM = new CGI;
 
@@ -39,9 +41,9 @@ $coge = CoGeX->connect( $connstr, $DBUSER, $DBPASS );
 $COOKIE_NAME = $P->{COOKIE_NAME};
 $URL         = $P->{URL};
 $COGEDIR     = $P->{COGEDIR};
-$TEMPDIR     = $P->{TEMPDIR} . "History/";
+$TEMPDIR     = $P->{TEMPDIR} . "$PAGE_TITLE/";
 mkpath( $TEMPDIR, 0, 0777 ) unless -d $TEMPDIR;
-$TEMPURL = $P->{TEMPURL} . "History/";
+$TEMPURL = $P->{TEMPURL} . "$PAGE_TITLE/";
 
 my ($cas_ticket) = $FORM->param('ticket');
 $USER = undef;
@@ -49,10 +51,10 @@ $USER = undef;
 ($USER) = CoGe::Accessory::LogUser->get_user( cookie_name => $COOKIE_NAME, coge => $coge ) unless $USER;
 
 %FUNCTION = (
-	gen_html => \&gen_html,
-	get_history_for_user => \&get_history_for_user,
-	toggle_star => \&toggle_star,
-	update_comment => \&update_comment,
+	gen_html 				=> \&gen_html,
+	get_history_for_user	=> \&get_history_for_user,
+	toggle_star				=> \&toggle_star,
+	update_comment			=> \&update_comment,
 );
 
 dispatch();
@@ -79,14 +81,14 @@ sub dispatch {
 sub gen_html {
 	my $html;
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . 'generic_page.tmpl' );
-	$template->param( HELP => '/wiki/index.php?title=History' );
+	$template->param( HELP => "/wiki/index.php?title=$PAGE_TITLE" );
 	my $name = $USER->user_name;
 	$name = $USER->first_name if $USER->first_name;
 	$name .= " " . $USER->last_name if $USER->first_name && $USER->last_name;
 	$template->param( USER       => $name );
 	$template->param( TITLE      => qq{User History} );
-	$template->param( PAGE_TITLE => qq{History} );
-	$template->param( LOGO_PNG   => "History-logo.png" );
+	$template->param( PAGE_TITLE => $PAGE_TITLE );
+	$template->param( LOGO_PNG   => "$PAGE_TITLE-logo.png" );
 	$template->param( LOGON      => 1 ) unless $USER->user_name eq "public";
 	$template->param( DATE       => $DATE );
 	$template->param( BODY       => gen_body() );
@@ -96,11 +98,12 @@ sub gen_html {
 }
 
 sub gen_body {
-	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . 'History.tmpl' );
-	$template->param( PAGE_NAME => $FORM->url );
-	$template->param( MAIN      => 1 );
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . $PAGE_TITLE . '.tmpl' );
+	$template->param( PAGE_NAME  => $PAGE_TITLE . '.pl' );
+	$template->param( MAIN       => 1 );
 	$template->param( ADMIN_AREA => 1 ) if $USER->is_admin;
-	$template->param( HISTORY_CONTENTS => get_history_for_user() );
+	$template->param( USER_NAME  => $USER->name );
+#	$template->param( HISTORY_CONTENTS => get_history_for_user(html_only=>1) );
 	
 	return $template->output;
 }
@@ -110,73 +113,39 @@ sub get_history_for_user
 	my %opts = @_;
 	my $time_range = $opts{time_range}; # in hours
 	$time_range = 24 if (not defined $time_range or $time_range !~ /[-\d]/);
+	my $last_id = $opts{last_id};
+#	print STDERR "get_history_for_user: $time_range $last_id\n";
 	
+	my %users = map { $_->user_id => $_->name } $coge->resultset('User')->all;
 	my @entries;
 	if ( $USER->is_admin ) {
 		if ($time_range == 0) {
-			@entries = $coge->resultset('Log')->all;
-		}
-		elsif ($time_range == -1) { # Starred entries
-			@entries = $coge->resultset('Log')->search( { status => 1 } );
-		}
-		elsif ($time_range == -2) { # Entries with comments
-			@entries = $coge->resultset('Log')->search( { comment => { '!=', '' }} );
-		}
-		elsif ($time_range == -3) { # My entries (default for non-admins)
-			@entries = $coge->resultset('Log')->search( { user_id => $USER->id } );
-		}
-		else {
-			@entries = $coge->resultset('Log')->search_literal( 'time >= DATE_SUB(NOW(), INTERVAL ? HOUR)', ($time_range) );
+			#$rs = $coge->resultset('Log')->search( undef, { result_class => 'DBIx::Class::ResultClass::HashRefInflator', order_by => { -desc => 'time' } });
+			@entries = $coge->resultset('Log')->search( undef, { order_by => { -desc => 'time' } });
 		}
 	}
 	else {
 		if ($time_range == 0 or $time_range == -3) {
-			@entries = $coge->resultset('Log')->search( { user_id => $USER->id } );
-		}
-		elsif ($time_range == -1) { # Starred entries
-			@entries = $coge->resultset('Log')->search( { user_id => $USER->id, status => 1 } );
-		}
-		elsif ($time_range == -2) { # Entries with comments
-			@entries = $coge->resultset('Log')->search( { user_id => $USER->id, comment => { '!=', '' }} );
-		}
-		else {
-			@entries = $coge->resultset('Log')->search_literal( 'user_id = ? AND time >= DATE_SUB(NOW(), INTERVAL ? HOUR)', ($USER->id, $time_range) );
+			@entries = $coge->resultset('Log')->search( { user_id => $USER->id }, { order_by => { -desc => 'time' } } );
 		}
 	}
 
-	my $dup = 1;
-	my @rows;	
-	for (my $i = 0;  $i < @entries;  $i++) {
-		my $entry = $entries[$i]; 
-		my $next_entry = $entries[$i+1]; 
-		if ($next_entry and $entry->link and $next_entry->link and $entry->link eq $next_entry->link) {
-			$dup++;
-			next;
-		}
-		
-		my %row;
-		$row{LOG_ID} = $entry->id;
-		$row{STAR_ICON} = $entry->status == 0 ? 'picts/star-hollow.png' : 'picts/star-full.png';
-		$row{TIME} = $entry->time;
-		$row{USER} = ($entry->user ? $entry->user->name : '');
-		$row{PAGE} = $entry->page;
-		$row{DESCRIPTION} = $entry->description;
-		$row{LINK} = '<a href="' . $entry->link . '" target="_blank">' . $entry->link . '</a>' if ($entry->link);
-		
-		if ($dup > 1) {
-			$row{LINK} = '(' . $dup . ') ' . $row{LINK};
-			$dup = 1;
-		}
-		
-		$row{COMMENT} = $entry->comment;
-		push @rows, \%row;
+	my @items;
+	foreach (@entries) {
+		push @items, 
+			{ id => $_->id,
+			  starred => ($_->status != 0),
+			  date_time => $_->time, 
+			  user => ($_->user_id ? $users{$_->user_id} : 'public'),
+			  page => $_->page,
+			  description => $_->description,
+			  link => ($_->link ? $_->link : ''),
+			  comment => $_->comment
+			};
 	}
-	
-	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . 'History.tmpl' );
-	$template->param( HISTORY => 1 );
-	$template->param( HISTORY_LOOP => [ reverse @rows ] );
 
-	return $template->output;
+	print STDERR "items: " . @items . "\n";
+	return encode_json(\@items);
 }
 
 sub toggle_star 
@@ -199,7 +168,7 @@ sub update_comment
 	my %opts = @_;
 	my $log_id = $opts{log_id};
 	my $comment = $opts{comment};
-#	print STDERR "$log_id $comment\n";
+	# print STDERR "udpate_comment: $log_id $comment\n";
 
 	my $entry = $coge->resultset('Log')->find($log_id);
 	return unless $entry;
