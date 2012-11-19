d46d888c3c35bae2d95e4653860fbd0aa1c774f8
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-11-19 21:07:13 +0000

    Added Experiments and Genomes pages.

---
+++ web/Experiments.pl
@@ -0,0 +1,178 @@
+#! /usr/bin/perl -w
+
+use strict;
+use CGI;
+use CoGeX;
+use DBI;
+
+use Data::Dumper;
+use CoGe::Accessory::LogUser;
+use CoGe::Accessory::Web;
+use HTML::Template;
+use JSON::XS;
+use URI::Escape;
+use Spreadsheet::WriteExcel;
+use Digest::MD5 qw(md5_base64);
+use DBIxProfiler;
+use File::Path;
+use Sort::Versions;
+use Data::UUID;
+no warnings 'redefine';
+
+use vars qw(
+	$P $DBNAME $DBHOST $DBPORT $DBUSER $DBPASS $connstr $PAGE_TITLE 
+	$TEMPDIR $USER $DATE $COGEDIR $coge $FORM $URL $TEMPURL $COOKIE_NAME 
+	$IRODS_ENV_FILE %FUNCTION
+);
+
+$P         = CoGe::Accessory::Web::get_defaults( $ENV{HOME} . 'coge.conf' );
+$ENV{PATH} = $P->{COGEDIR};
+$COGEDIR   = $P->{COGEDIR};
+$URL       = $P->{URL};
+$DATE = sprintf(
+	"%04d-%02d-%02d %02d:%02d:%02d",
+	sub { ( $_[5] + 1900, $_[4] + 1, $_[3] ), $_[2], $_[1], $_[0] }->(localtime)
+);
+
+$PAGE_TITLE = 'Experiments';
+
+$TEMPDIR = $P->{TEMPDIR} . $PAGE_TITLE . '/';
+mkpath( $TEMPDIR, 0, 0777 ) unless -d $TEMPDIR;
+$TEMPURL = $P->{TEMPURL} . $PAGE_TITLE . '/';
+
+$IRODS_ENV_FILE = '/opt/apache/CoGe-Dev/irodsEnv'; # FIXME load from config
+if (not -e $IRODS_ENV_FILE) {
+	print STDERR "fatal error: irods env file missing!\n";
+	return;	
+}
+
+$FORM = new CGI;
+
+$DBNAME      = $P->{DBNAME};
+$DBHOST      = $P->{DBHOST};
+$DBPORT      = $P->{DBPORT};
+$DBUSER      = $P->{DBUSER};
+$DBPASS      = $P->{DBPASS};
+$connstr     = "dbi:mysql:dbname=" . $DBNAME . ";host=" . $DBHOST . ";port=" . $DBPORT;
+$coge        = CoGeX->connect( $connstr, $DBUSER, $DBPASS );
+$COOKIE_NAME = $P->{COOKIE_NAME};
+
+my ($cas_ticket) = $FORM->param('ticket');
+$USER = undef;
+($USER) = CoGe::Accessory::Web->login_cas( ticket => $cas_ticket, coge => $coge, this_url => $FORM->url() ) if ($cas_ticket);
+($USER) = CoGe::Accessory::LogUser->get_user( cookie_name => $COOKIE_NAME, coge => $coge ) unless $USER;
+
+#$SIG{'__WARN__'} = sub { };    # silence warnings
+
+%FUNCTION = (
+	generate_html				=> \&generate_html,
+	delete_experiment			=> \&delete_experiment,
+	get_experiments_for_user	=> \&get_experiments_for_user,
+);
+
+if ( $FORM->param('jquery_ajax') ) {
+	my %args  = $FORM->Vars;
+	my $fname = $args{'fname'};
+	if ($fname) {
+		if ( $args{args} ) {
+			my @args_list = split( /,/, $args{args} );
+			print $FORM->header, $FUNCTION{$fname}->(@args_list);
+		}
+		else {
+			print $FORM->header, $FUNCTION{$fname}->(%args);
+		}
+	}
+}
+else {
+	print $FORM->header, "\n", generate_html();
+}
+
+sub delete_experiment {
+	my %opts = @_;
+	my $eid = $opts{eid};
+	return "Must have valid experiment id\n" unless ($eid);
+	
+	# Security check
+	if (not $USER->is_owner(experiment => $eid)) {
+		return 0;
+	}
+
+	# Delete the experiment and associated connectors & annotations
+	#FIXME add some error checking/logging here
+	my $experiment = $coge->resultset('Experiment')->find($eid);
+	return 0 unless ($experiment);
+	$experiment->delete;
+	
+	CoGe::Accessory::Web::log_history( db => $coge, user_id => $USER->id, page => "$PAGE_TITLE.pl", description => 'delete experiment id' . $experiment->id );
+
+	return 1;
+}
+
+sub get_experiments_for_user {
+	#my %opts = @_;
+
+	my @experiments;
+#	if ( $USER->is_admin ) {
+#		@experiments = $coge->resultset('Experiment')->all();
+#	}
+#	else {
+		@experiments = $USER->owner_list->experiments;
+#	}
+
+	my @rows;
+	foreach my $e (@experiments) {#( sort experimentcmp @experiments ) {
+		push @rows, 
+		  { NAME  => qq{<span class="link" onclick='window.open("ExperimentView.pl?eid=} . $e->id . qq{")'>} . $e->info . "</span>",
+			VERSION  => $e->version,
+			DATE =>  $e->date,
+			EDIT_BUTTON => 0 ?
+				"<span class='link ui-icon ui-icon-locked' onclick=\"alert('This list is locked and cannot be edited.')\"></span>" :
+				"<span class='link ui-icon ui-icon-gear' onclick=\"window.open('ExperimentView.pl?eid=" . $e->id . "')\"></span>",
+			DELETE_BUTTON => 0 ?
+				"<span class='link ui-icon ui-icon-locked' onclick=\"alert('This list is locked and cannot be deleted.')\"></span>" :
+				"<span class='link ui-icon ui-icon-trash' onclick=\"dialog_delete_experiment({eid: '" . $e->id . "'});\"></span>"
+		  };
+	}
+
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . $PAGE_TITLE . '.tmpl' );
+	$template->param( DO_EXPERIMENT_TABLE => 1 );
+	$template->param( EXPERIMENT_LOOP  => \@rows );
+
+	return $template->output;
+}
+
+sub generate_html {
+	my $html;
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . 'generic_page.tmpl' );
+	$template->param( PAGE_TITLE => $PAGE_TITLE );
+	$template->param( HELP       => '/wiki/index.php?title=' . $PAGE_TITLE . '.pl' );
+	my $name = $USER->user_name;
+	$name = $USER->first_name if $USER->first_name;
+	$name .= ' ' . $USER->last_name if ( $USER->first_name && $USER->last_name );
+	$template->param( USER     => $name );
+	$template->param( LOGO_PNG => $PAGE_TITLE . "-logo.png" );
+	$template->param( LOGON    => 1 ) unless $USER->user_name eq "public";
+	$template->param( DATE     => $DATE );
+	my $link = "http://" . $ENV{SERVER_NAME} . $ENV{REQUEST_URI};
+	$link = CoGe::Accessory::Web::get_tiny_link( url => $link );
+
+	$template->param( BODY => generate_body() );
+	$template->param( ADJUST_BOX => 1 );
+
+	$html .= $template->output;
+}
+
+sub generate_body {
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . $PAGE_TITLE . '.tmpl' );
+	$template->param( MAIN => 1 );
+	$template->param( PAGE_NAME => $PAGE_TITLE );
+	$template->param( EXPERIMENT_TABLE  => get_experiments_for_user() );
+	
+	return $template->output;
+}
+
+# FIXME this comparison routine is duplicated elsewhere
+sub experimentcmp {
+	no warnings 'uninitialized'; # disable warnings for undef values in sort
+	versioncmp($b->version, $a->version) || $a->name cmp $b->name || $b->id cmp $a->id
+}
---
+++ web/Genomes.pl
@@ -0,0 +1,176 @@
+#! /usr/bin/perl -w
+
+use strict;
+use CGI;
+use CoGeX;
+use DBI;
+
+use Data::Dumper;
+use CoGe::Accessory::LogUser;
+use CoGe::Accessory::Web;
+use HTML::Template;
+use JSON::XS;
+use URI::Escape;
+use Spreadsheet::WriteExcel;
+use Digest::MD5 qw(md5_base64);
+use DBIxProfiler;
+use File::Path;
+use Sort::Versions;
+use Data::UUID;
+no warnings 'redefine';
+
+use vars qw(
+	$P $DBNAME $DBHOST $DBPORT $DBUSER $DBPASS $connstr $PAGE_TITLE 
+	$TEMPDIR $USER $DATE $COGEDIR $coge $FORM $URL $TEMPURL $COOKIE_NAME 
+	$IRODS_ENV_FILE %FUNCTION
+);
+
+$P         = CoGe::Accessory::Web::get_defaults( $ENV{HOME} . 'coge.conf' );
+$ENV{PATH} = $P->{COGEDIR};
+$COGEDIR   = $P->{COGEDIR};
+$URL       = $P->{URL};
+$DATE = sprintf(
+	"%04d-%02d-%02d %02d:%02d:%02d",
+	sub { ( $_[5] + 1900, $_[4] + 1, $_[3] ), $_[2], $_[1], $_[0] }->(localtime)
+);
+
+$PAGE_TITLE = 'Genomes';
+
+$TEMPDIR = $P->{TEMPDIR} . $PAGE_TITLE . '/';
+mkpath( $TEMPDIR, 0, 0777 ) unless -d $TEMPDIR;
+$TEMPURL = $P->{TEMPURL} . $PAGE_TITLE . '/';
+
+$IRODS_ENV_FILE = '/opt/apache/CoGe-Dev/irodsEnv'; # FIXME load from config
+if (not -e $IRODS_ENV_FILE) {
+	print STDERR "fatal error: irods env file missing!\n";
+	return;	
+}
+
+$FORM = new CGI;
+
+$DBNAME      = $P->{DBNAME};
+$DBHOST      = $P->{DBHOST};
+$DBPORT      = $P->{DBPORT};
+$DBUSER      = $P->{DBUSER};
+$DBPASS      = $P->{DBPASS};
+$connstr     = "dbi:mysql:dbname=" . $DBNAME . ";host=" . $DBHOST . ";port=" . $DBPORT;
+$coge        = CoGeX->connect( $connstr, $DBUSER, $DBPASS );
+$COOKIE_NAME = $P->{COOKIE_NAME};
+
+my ($cas_ticket) = $FORM->param('ticket');
+$USER = undef;
+($USER) = CoGe::Accessory::Web->login_cas( ticket => $cas_ticket, coge => $coge, this_url => $FORM->url() ) if ($cas_ticket);
+($USER) = CoGe::Accessory::LogUser->get_user( cookie_name => $COOKIE_NAME, coge => $coge ) unless $USER;
+
+#$SIG{'__WARN__'} = sub { };    # silence warnings
+
+%FUNCTION = (
+	generate_html			=> \&generate_html,
+	create_genome			=> \&create_genome,
+	delete_genome			=> \&delete_genome,
+	get_genomes_for_user	=> \&get_genomes_for_user,
+);
+
+if ( $FORM->param('jquery_ajax') ) {
+	my %args  = $FORM->Vars;
+	my $fname = $args{'fname'};
+	if ($fname) {
+		if ( $args{args} ) {
+			my @args_list = split( /,/, $args{args} );
+			print $FORM->header, $FUNCTION{$fname}->(@args_list);
+		}
+		else {
+			print $FORM->header, $FUNCTION{$fname}->(%args);
+		}
+	}
+}
+else {
+	print $FORM->header, "\n", generate_html();
+}
+
+sub delete_genome {
+	my %opts = @_;
+	my $gid = $opts{gid};
+	return "Must have valid genome id\n" unless ($gid);
+	
+	# Security check
+	if (not $USER->is_owner(dsg => $gid)) {
+		return 0;
+	}
+
+	# Delete the genome and associated connectors & datasets
+	#FIXME add some error checking/logging here
+	my $genome = $coge->resultset('Genome')->find($gid);
+	return 0 unless ($genome);
+	$genome->delete;
+	
+	CoGe::Accessory::Web::log_history( db => $coge, user_id => $USER->id, page => "$PAGE_TITLE.pl", description => 'delete genome id' . $genome->id );
+
+	return 1;
+}
+
+sub get_genomes_for_user {
+	#my %opts = @_;
+
+	my @genomes;
+#	if ( $USER->is_admin ) {
+#		@genomes = $coge->resultset('Genome')->all();
+#	}
+#	else {
+		@genomes = $USER->owner_list->genomes;
+#	}
+
+	my @genome_info;
+	foreach my $g (@genomes) {#( sort genomecmp @genomes ) {
+		push @genome_info, 
+		  { NAME  => qq{<span class="link" onclick='window.open("OrganismView.pl?lid=} . $g->id . qq{")'>} . $g->info . "</span>",
+			VERSION  => $g->version,
+			DATE =>  $g->date,
+			EDIT_BUTTON => "<span class='link ui-icon ui-icon-gear' onclick=\"window.open('OrganismView.pl?gid=" . $g->id . "')\"></span>",
+			DELETE_BUTTON => "<span class='link ui-icon ui-icon-trash' onclick=\"dialog_delete_genome({gid: '" . $g->id . "'});\"></span>"
+		  };
+	}
+
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . $PAGE_TITLE . '.tmpl' );
+	$template->param( DO_GENOME_TABLE => 1 );
+	$template->param( GENOME_LOOP  => \@genome_info );
+#	$template->param( TYPE_LOOP => get_list_types() );
+
+	return $template->output;
+}
+
+sub generate_html {
+	my $html;
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . 'generic_page.tmpl' );
+	$template->param( PAGE_TITLE => $PAGE_TITLE );
+	$template->param( HELP       => '/wiki/index.php?title=' . $PAGE_TITLE . '.pl' );
+	my $name = $USER->user_name;
+	$name = $USER->first_name if $USER->first_name;
+	$name .= ' ' . $USER->last_name if ( $USER->first_name && $USER->last_name );
+	$template->param( USER     => $name );
+	$template->param( LOGO_PNG => $PAGE_TITLE . "-logo.png" );
+	$template->param( LOGON    => 1 ) unless $USER->user_name eq "public";
+	$template->param( DATE     => $DATE );
+	my $link = "http://" . $ENV{SERVER_NAME} . $ENV{REQUEST_URI};
+	$link = CoGe::Accessory::Web::get_tiny_link( url => $link );
+
+	$template->param( BODY => generate_body() );
+	$template->param( ADJUST_BOX => 1 );
+
+	$html .= $template->output;
+}
+
+sub generate_body {
+	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . $PAGE_TITLE . '.tmpl' );
+	$template->param( MAIN => 1 );
+	$template->param( PAGE_NAME => $PAGE_TITLE );
+	$template->param( GENOME_TABLE  => get_genomes_for_user() );
+	
+	return $template->output;
+}
+
+# FIXME these comparison routines are duplicated elsewhere
+sub genomecmp {
+	no warnings 'uninitialized'; # disable warnings for undef values in sort
+	$a->organism->name cmp $b->organism->name || versioncmp($b->version, $a->version) || $a->type->id <=> $b->type->id || $a->name cmp $b->name || $b->id cmp $a->id
+}
