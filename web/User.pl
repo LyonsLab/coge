9a6f0b6a356c6b9e6f100a457b5143f47750be4e
Matt Bombhoff <matthew.bomhoff@gmail.com>
2012-11-21 16:23:01 +0000

    Minor fixes.

--- web/User.pl
+++ web/User.pl
@@ -112,11 +112,13 @@ sub gen_html {
 }
 
 sub gen_body {
-	return "Access Denied" if ($USER->user_name eq "public");
+	return 'Access Denied' if ($USER->user_name eq 'public');
 
+	# Other user specified as param, only allow access if collaborator
 	my $uid = $FORM->param('uid');
 	my $user = $USER;
 	if ($uid) {
+		return '' if (not $USER->has_collaborator($uid));
 		$user = $coge->resultset('User')->find($uid);
 	}
 
@@ -162,6 +164,7 @@ sub get_groups {
 
 		push @rows, \%row;
 	}
+	push @rows, {GROUP_NAME => '<span style="font-style:italic;color:gray;">None</span>'} unless (@rows);
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( GROUP_TABLE => 1 );
@@ -182,6 +185,7 @@ sub get_collaborators {
 		
 		push @rows, \%row;
 	}
+	push @rows, {COLLABORATOR_INFO => '<span style="font-style:italic;color:gray;">None</span>'} unless (@rows);
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( COLLABORATOR_TABLE => 1 );
@@ -191,17 +195,21 @@ sub get_collaborators {
 
 sub get_lists {
 	my $user = shift;
+	my $is_user = ($user->id == $USER->id);
 	my @lists = $user->lists;
-	
+
 	my @rows;
 	foreach my $list (sort listcmp @lists) {
+		next if ($list->restricted && !$is_user && !$user->has_access({list => $list}));
+
 		my $id = $list->id;
-				
 		my %row;
 		$row{LIST_INFO} = qq{<span class="link" onclick='window.open("ListView.pl?lid=$id")'>} . $list->info . "</span>";
 		
 		push @rows, \%row;
 	}
+	push @rows, {LIST_INFO => '<span style="font-style:italic;color:gray;">None</span>'} unless (@rows);
+
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( LIST_TABLE => 1 );
@@ -211,17 +219,20 @@ sub get_lists {
 
 sub get_genomes {
 	my $user = shift;
+	my $is_user = ($user->id == $USER->id);
 	my @genomes = $user->owner_group->owner_list->genomes;
-	
+
 	my @rows;
 	foreach my $genome (sort genomecmp @genomes) {
+		next if ($genome->restricted && !$is_user && !$user->has_access({dsg => $genome}));
+
 		my $id = $genome->id;
-				
 		my %row;
 		$row{GENOME_INFO} = qq{<span class="link" onclick='window.open("OrganismView.pl?dsgid=$id")'>} . $genome->info . "</span>";
 		
 		push @rows, \%row;
 	}
+	push @rows, {GENOME_INFO => '<span style="font-style:italic;color:gray;">None</span>'} unless (@rows);
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( GENOME_TABLE => 1 );
@@ -231,17 +242,20 @@ sub get_genomes {
 
 sub get_experiments {
 	my $user = shift;
+	my $is_user = ($user->id == $USER->id);
 	my @experiments = $user->owner_group->owner_list->experiments;
-	
+
 	my @rows;
 	foreach my $exp (sort experimentcmp @experiments) {
+		next if ($exp->restricted && !$is_user && !$user->has_access({experiment => $exp}));
+
 		my $id = $exp->id;
-				
 		my %row;
 		$row{EXPERIMENT_INFO} = qq{<span class="link" onclick='window.open("ExperimentView.pl?eid=$id")'>} . $exp->info . "</span>";
 		
 		push @rows, \%row;
 	}
+	push @rows, {EXPERIMENT_INFO => '<span style="font-style:italic;color:gray;">None</span>'} unless (@rows);
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( EXPERIMENT_TABLE => 1 );
@@ -250,11 +264,12 @@ sub get_experiments {
 }
 
 sub get_logs {
+	return if ($USER->user_name eq "public");
+
 	my %opts = @_;
 	my $type = $opts{type};
 
 	my @logs;
-
 	if (!$type or $type eq 'recent') {
 		@logs = $coge->resultset('Log')->search( { user_id => $USER->id }, { order_by => { -desc => 'time' } } ); # $user->logs;
 		#my @logs = reverse $coge->resultset('Log')->search_literal( 'user_id = ' . $user->id . ' AND time >= DATE_SUB(NOW(), INTERVAL 1 HOUR)' );
@@ -271,6 +286,7 @@ sub get_logs {
 					  LOG_LINK => $_->link
 					};
 	}
+	return if (not @rows);
 
 	my $template = HTML::Template->new( filename => $P->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
 	$template->param( LOG_TABLE => 1 );
@@ -285,6 +301,8 @@ sub upload_image_file {
 	my $image_filename = '' . $FORM->param('input_upload_file');
 	my $fh = $FORM->upload('input_upload_file');
 	return if (-s $fh > 2*1024*1024); # limit to 2MB
+
+	#TODO delete old image
 
 	# Create the image
 	my $image;
