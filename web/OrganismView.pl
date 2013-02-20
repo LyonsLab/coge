0edf129995ff2771b0d8eea830b10ea668c26fc1
Eric Lyons <lyonsden@gmail.com>
2013-02-20 18:11:33 +0000

    added in a check such that deleted experiments are not shown

--- web/OrganismView.pl
+++ web/OrganismView.pl
@@ -637,7 +637,7 @@ sub get_genome_info
 #    $html .= qq{&nbsp|&nbsp};
 #    $html .= qq{<span id=irods class='link' onclick="gen_data(['args__loading...'],['irods']);add_to_irods(['args__dsgid','args__$dsgid'],['irods']);">Send To iPlant Data Store</span>};  
     $html .= "</td></tr>";
-    if (my $exp_count = $dsg->experiments->count )
+    if (my $exp_count = $dsg->experiments->count({deleted=>0}) )
     {
     	$html .= "<tr><td>Experiment count:</td>";
 		$html .= "<td><span class=link onclick=window.open('ExperimentList.pl?dsgid=".$dsg->id."')>".$exp_count."</span></td></tr>";
