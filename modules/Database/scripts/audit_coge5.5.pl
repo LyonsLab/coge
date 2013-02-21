38fd6113854ccf9df2cb95369d8da5303efe8aa1
Matt Bombhoff <matthew.bomhoff@gmail.com>
2013-02-21 17:29:10 +0000

    Added checks

--- modules/Database/scripts/audit_coge5.5.pl
+++ modules/Database/scripts/audit_coge5.5.pl
@@ -51,39 +51,86 @@ my $dbh = $coge->storage->dbh;
 # Audit 
 #-------------------------------------------------------------------------------
 
+# Verify groups
+print STDERR "Verifying groups ---------------------------------------------\n";
 foreach my $group ($coge->resultset('UserGroup')->all) {
+	my $group_desc = $group->id." ('" . $group->name . "')";
+	
+	# Find orphan groups
+	unless ($group->user_connectors) {
+		print STDERR "Group $group_desc is orphaned\n";
+		next;
+	}
+
 	# Check that each group has a creator
 	unless ($group->creator) {
-		print STDERR "Group ".$group->id." ('" . $group->name . "') is missing creator user\n";	
+		print STDERR "Group $group_desc is missing creator user\n";	
 	}	
 	
 	# Check that each group has an owner user connection
 	unless ($group->owner) {
-		print STDERR "Group ".$group->id." ('" . $group->name . "') is missing owner user\n";	
+		print STDERR "Group $group_desc is missing owner user\n";	
 	}
 	
 	# Check that non-owner users' roles match group role
-#	foreach my $user ($group->users) {
-#		print STDERR "User ".$user->id."\n";
-#	}
+	foreach my $conn ($group->user_connectors) {
+		next if ($conn->role_id == 2); # skip owner
+		my $user = $conn->parent;
+		if ($conn->role_id != $group->role_id) {
+			print STDERR "Group $group_desc has role mismatch with user " . $user->id . "\n";
+		}
+	}
 }
 
+print STDERR "Verifying lists ----------------------------------------------\n";
+# Find orphan lists
+foreach my $list ($coge->resultset('List')->all) {
+	unless ($list->user_connectors) {
+		print STDERR "List ".$list->id." is orphaned\n";
+	}
+}
+
+# Find orphan list connectors -- FIXME: THIS IS SLOW
 my (%missing_parent, %missing_child);
 foreach my $conn ($coge->resultset('ListConnector')->all) {
 	# Check that each list connector has nodes
 	unless ($conn->parent_list) {
 		push @{$missing_parent{$conn->parent_id}}, $conn->id;
+		next;
 	}
 	unless ($conn->child) {
 		push @{$missing_child{$conn->child_id}}, $conn->id;
 	}
 }
-
 foreach my $lid (keys %missing_parent) {
 	print STDERR "Parent list " . $lid . " is missing for list connectors: " . join(', ', @{$missing_parent{$lid}}) . "\n";	
 }
 foreach my $child_id (keys %missing_child) {
 	print STDERR "Child  " . $child_id . " is missing for list connectors: " . join(', ', @{$missing_child{$child_id}}) . "\n";	
+}
+
+print STDERR "Verifying images ---------------------------------------------\n";
+# Find orphan images
+foreach my $image ($coge->resultset('Image')->all) {
+	unless ($image->user or $image->list_annotation) {
+		print STDERR "Image ".$image->id." is orphaned\n";
+	}
+}
+
+print STDERR "Verifying genomes --------------------------------------------\n";
+foreach my $genome ($coge->resultset('Genome')->all) {
+	unless ($genome->list_connectors or $genome->user_connectors or $genome->group_connectors) {
+		print STDERR "Genome ".$genome->id." is orphaned\n";
+		next;
+	}
+}
+
+print STDERR "Verifying experiments ----------------------------------------\n";
+foreach my $experiment ($coge->resultset('Experiment')->all) {
+	unless ($experiment->list_connectors or $experiment->user_connectors or $experiment->group_connectors) {
+		print STDERR "Experiment ".$experiment->id." is orphaned\n";
+		next;
+	}
 }
 
 #-------------------------------------------------------------------------------
