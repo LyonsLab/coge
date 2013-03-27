53cc7fca206a80a268209a8e9d646c8e3f9a3dc0
Matt Bombhoff <matthew.bomhoff@gmail.com>
2013-03-27 19:36:42 +0000

    Fix for issue 61

--- web/bin/fastbit_query.pl
+++ web/bin/fastbit_query.pl
@@ -10,19 +10,27 @@ use CoGeX;
 use CoGe::Accessory::Web;
 use CGI;
 use Benchmark;
+use Cwd 'abs_path';
+
+my $NUM_COL = 6;
+
+my $t1 = new Benchmark;
 
 my ($exp_id, $coge_conf, $chr, $start, $stop);
-my $t1    = new Benchmark;
-
 GetOptions ( # FIXME for testing, remove someday
-	    "exp_id=i" 			=> \$exp_id,
-			"chr=s" 				=> \$chr,
-			"start=i" 			=> \$start,
-			"stop=i" 				=> \$stop,
-			"coge_conf=s" 	=> \$coge_conf   
-	   );
+	"exp_id=i" 		=> \$exp_id,
+	"chr=s" 		=> \$chr,
+	"start=i" 		=> \$start,
+	"stop=i" 		=> \$stop,
+	"coge_conf=s" 	=> \$coge_conf   
+);
 
 # Load config file
+unless ($coge_conf) { # mdb added 3/26/13, tempfix for issue 61
+	$coge_conf = abs_path($0);
+	$coge_conf =~ s/bin\/fastbit_query\.pl//;
+	$coge_conf .= 'coge.conf';
+}
 my $P = CoGe::Accessory::Web::get_defaults($coge_conf);
 my $DBNAME = $P->{DBNAME};
 my $DBHOST = $P->{DBHOST};
@@ -34,66 +42,66 @@ my $CMDPATH = $P->{FASTBIT_QUERY};
 # Parse URL
 my $FORM = new CGI;
 $exp_id = $FORM->param('exp_id')	unless defined $exp_id;
-$chr 		= $FORM->param('chr') 		unless defined $chr;
+$chr 	= $FORM->param('chr') 		unless defined $chr;
 $start 	= $FORM->param('start') 	unless defined $start;
 $stop 	= $FORM->param('stop') 		unless defined $stop;
+
 # Connect to the database
 my $connstr = "dbi:mysql:dbname=$DBNAME;host=$DBHOST;port=$DBPORT";
 my $coge = CoGeX->connect($connstr, $DBUSER, $DBPASS);
 #$coge->storage->debugobj(new DBIxProfiler());
 #$coge->storage->debug(1);
-my $t2    = new Benchmark;
+my $t2 = new Benchmark;
+
 # Get experiment's storage path
 my ($exp) = $coge->resultset("Experiment")->find($exp_id);
-# FIXME handle experiment not found case
+unless ($exp) {
+	print STDERR "fastbit_query: experiment $exp_id not found\n"; 
+	exit;
+}
+
 my $exp_storage_path = $exp->storage_path;
-my $t3    = new Benchmark;
+my $t3 = new Benchmark;
+
 # Call FastBit to do query
-my $cmd = "$CMDPATH -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where chr=$chr and start > $start and stop < $stop\" 2>&1";
-print STDERR $cmd;
+# Issue 61: query string must contain a "." for fastbit to use consistent
+# format (see jira thread for explanation) so "0.0=0.0" was added along with
+# -v 2 option.  Output parsing was modified accordingly for new output format.
+#my $cmd = "$CMDPATH -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where chr='$chr' and start > $start and stop < $stop\" 2>&1"; # mdb removed 3/27/13 issue 61
+my $cmd = "$CMDPATH -v 2 -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where 0.0=0.0 and chr='$chr' and start > $start and stop < $stop\" 2>&1"; # mdb added 3/27/13 issue 61
+#print STDERR $cmd;
 my $cmdOut = qx{$cmd};
+#print STDERR $cmdOut;
 my $cmdStatus = $?;
 die "Error executing command $CMDPATH ($cmdStatus)" if ($cmdStatus != 0);
-my $t4    = new Benchmark;
+my $t4 = new Benchmark;
+
 # Convert FastBit output into JSON
-my ($numHits, $queryStr, $header, $results);
+my $results;
 
 my @lines = split("\n", $cmdOut);
-foreach (@lines) 
-	{
-		if (/(.+) \(with counts\)/) { # first line
-			$header = $1;
-			$header =~ s/\"?([\w\.\+\-]+)\"?/\"$1\"/g;
+foreach (@lines)  { # mdb rewritten 3/27/13 issue 61, could be optimized or perhaps there is a better way to grab ouput (such as -o file option)
+	if (my @fields = /\"?([\w\.\+\-\_]*)\"?\,/g) { # result line
+		s/"//g;
+		s/, /,/g;
+		my @items = split(/,/);
+		next if (@items != $NUM_COL || $items[0] !~ /$chr/);
+		for (my $i =0; $i<@items; $i++) {
+			$items[$i] = 1 if $items[$i] !~ /\w/;
 		}
-		elsif (/^doQuery:: evaluate\((.+)\) produced (\d+) hit/) { # last line
-			$queryStr = $1;
-			$numHits = $2;
-		}
-		elsif (my @fields = /\"?([\w\.\+\-]*)\"?\,/g) { # result line
-		  s/"//g;
-		  s/, /,/g;
-		  my @items = split/,/;
-		  for (my $i =0; $i<@items; $i++)
-		    {
-		      $items[$i] = 1 if $items[$i] !~ /\w/;
-		    }
-		  $results .= ',' if ($results);
-		  $results .= '[' . join(',', map {"\"$_\""} @items) . ']';
-		}
+		$results .= ',' if ($results);
+		$results .= '[' . join(',', map {"\"$_\""} @items) . ']';
 	}
-my $t5    = new Benchmark;
-print "Content-type: text/json\n\n";
-if (defined $numHits and defined $queryStr) 
-	{
-		$header = '' unless ($header);
-		$results = '' unless ($results);
-		print qq{{"query" : "$queryStr","result_count" : "$numHits","header" : [$header],"results" : [$results]}};
-	}
-print STDERR "Initialize CoGe: ". timestr( timediff( $t2, $t1 ) ),"\n";
-print STDERR "Get experiment info from CoGe: ".timestr( timediff( $t3, $t2 ) ),"\n";
-print STDERR "Get data from fastbit: ".timestr( timediff( $t4, $t3 ) ),"\n";
-print STDERR "Assemble results: ". timestr( timediff( $t5, $t4 ) ),"\n";
+}
 
+my $t5 = new Benchmark;
+print "Content-type: text/json\n\n";
+print qq{{"results" : [$results]}};
+	
+#print STDERR "Initialize CoGe: ". timestr( timediff( $t2, $t1 ) ),"\n",
+#			 "Get experiment info from CoGe: ".timestr( timediff( $t3, $t2 ) ),"\n",
+#			 "Get data from fastbit: ".timestr( timediff( $t4, $t3 ) ),"\n",
+#			 "Assemble results: ". timestr( timediff( $t5, $t4 ) ),"\n";
 
 exit;
 
