bad51ea35cb15fc237d34cf2a32485ea1c902d7a
Matt Bombhoff <matthew.bomhoff@gmail.com>
2013-05-28 17:15:20 +0000

    Use conf file command path

--- modules/Services/lib/CoGe/Services/JBrowse/Experiment.pm
+++ modules/Services/lib/CoGe/Services/JBrowse/Experiment.pm
@@ -42,12 +42,14 @@ sub features {
 	return unless (($eid or $nid) and $chr and $start and $end);
 	
 	# Load config file
+	#print STDERR "conf = $coge_conf\n";
 	my $P = CoGe::Accessory::Web::get_defaults($coge_conf);
 	my $DBNAME = $P->{DBNAME};
 	my $DBHOST = $P->{DBHOST};
 	my $DBPORT = $P->{DBPORT};
 	my $DBUSER = $P->{DBUSER};
 	my $DBPASS = $P->{DBPASS};
+	my $CMDPATH = $P->{FASTBIT_QUERY};
 
 	# Connect to the database
 	my $connstr = "dbi:mysql:dbname=$DBNAME;host=$DBHOST;port=$DBPORT";
@@ -76,7 +78,7 @@ sub features {
 		# format (see jira thread for explanation) so "0.0=0.0" was added along with
 		# -v option.  Output parsing was modified accordingly for new output format.
 		#my $cmd = "$CMDPATH -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where chr='$chr' and start > $start and stop < $stop\" 2>&1"; # mdb removed 3/27/13 issue 61
-		my $cmd = "/usr/local/bin/ibis -v 1 -d $storage_path -q \"select chr,start,stop,strand,value1,value2 where 0.0=0.0 and chr='$chr' and start > $start and stop < $end order by start limit 999999999\" 2>&1"; # mdb added 3/27/13 issue 61
+		my $cmd = "$CMDPATH -v 1 -d $storage_path -q \"select chr,start,stop,strand,value1,value2 where 0.0=0.0 and chr='$chr' and start > $start and stop < $end order by start limit 999999999\" 2>&1"; # mdb added 3/27/13 issue 61
 		#print STDERR "$cmd\n";
 		my @cmdOut = qx{$cmd};
 		#print STDERR @cmdOut;
@@ -107,51 +109,5 @@ sub features {
 #	print STDERR "{ 'features' : [ $results ] }\n";
 	return qq{{ "features" : [ $results ] }};
 }
-
-#sub features {
-#	my $self = shift;
-#	my $eid = $self->param('exp');
-#	my $chr = $self->param('chr');
-#	my $start = $self->query->param('start');
-#	my $end = $self->query->param('end');
-#	print STDERR "experiment features eid=" . ($eid ? $eid : '') . " $chr:$start:$end\n";
-#	
-#	my $exp_storage_path = '/storage/coge/data/experiments/0/0/0/'.$eid; #FIXME hardcoded path
-#	
-#	# Call FastBit to do query
-#	# Issue 61: query string must contain a "." for fastbit to use consistent
-#	# format (see jira thread for explanation) so "0.0=0.0" was added along with
-#	# -v option.  Output parsing was modified accordingly for new output format.
-#	#my $cmd = "$CMDPATH -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where chr='$chr' and start > $start and stop < $stop\" 2>&1"; # mdb removed 3/27/13 issue 61
-#	my $cmd = "/usr/local/bin/ibis -v 1 -d $exp_storage_path -q \"select chr,start,stop,strand,value1,value2 where 0.0=0.0 and chr='$chr' and start > $start and stop < $end order by start limit 999999999\" 2>&1"; # mdb added 3/27/13 issue 61
-##	print STDERR "$cmd\n";
-#	my @cmdOut = qx{$cmd};
-##	print STDERR @cmdOut;
-#	my $cmdStatus = $?;
-#	die "Error executing command $CMDPATH ($cmdStatus)" if ($cmdStatus != 0);
-#	
-#	# Convert FastBit output into JSON
-#	my $results = '';
-#	foreach (@cmdOut) { # mdb rewritten 3/27/13 issue 61
-#		chomp;
-#		if (/^\"/) { #if (/^\"$chr\"/) { # potential result line
-#			s/"//g;
-#			my @items = split(/,\s*/);
-#			next if (@items != $NUM_COL);# || $items[0] !~ /^\"?$chr/); # make sure it's a row output line
-#			for (my $i =0; $i<@items; $i++) {
-#				$items[$i] = 1 if $items[$i] !~ /\w/; # what's this for?
-#			}
-#			#$results .= '[' . join(',', map {"\"$_\""} @items) . ']';
-#			
-#			my ($chr, $start, $end, $strand, $value1, $value2) = @items;
-#			$end = $start+1 if ($end == $start); #FIXME revisit this
-#			$value1 = $strand*$value1;
-#			$results .= ($results ? ',' : '') . qq{{ "start": $start, "end": $end, "score": $value1 }};
-#		}
-#	}
-#	
-#	#print STDERR "{ 'features' : [ $results ] }\n";
-#	return qq{{ "features" : [ $results ] }};
-#}
 
 1;
