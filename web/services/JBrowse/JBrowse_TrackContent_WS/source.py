b2ec406a6ba97df6e1bfdefb7ad626eb61daf8d4
Roey Chasman <rchasman@gmail.com>
2013-06-07 23:41:45 +0000

    Removed primary_name = 1 in where clause of annotation track because it was filtering important datum

--- web/services/JBrowse/JBrowse_TrackContent_WS/source.py
+++ web/services/JBrowse/JBrowse_TrackContent_WS/source.py
@@ -149,8 +149,7 @@ def an_features(environ, start_response):
                 JOIN feature_name fn ON f.feature_id = fn.feature_id \
                 JOIN feature_type ft ON f.feature_type_id = ft.feature_type_id \
                 WHERE f.dataset_id = {0} AND f.chromosome = '{1}' \
-                AND f.stop > {2} AND f.start <= {3} \
-                AND fn.primary_name = 1;"
+                AND f.stop > {2} AND f.start <= {3};"
                     .format(dataset_id, chr_id, start, end))
 
         results = cur.fetchall()
