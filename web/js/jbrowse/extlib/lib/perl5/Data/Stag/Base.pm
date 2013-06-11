e55dec5ac4f69be75a140dd2804f62c6c6a5354e
Matt Bombhoff <matthew.bomhoff@gmail.com>
2013-06-11 22:08:28 +0000

    Clean up jbrowse invocation

--- web/js/jbrowse/extlib/lib/perl5/Data/Stag/Base.pm
+++
@@ -1,9 +0,0 @@
-package Data::Stag::Base;
-use base qw(Data::Stag::BaseHandler);
-
-use vars qw($VERSION);
-$VERSION="0.11";
-
-# DEPRECATED MODULE - synonym for BaseHandler
-
-1;
--- web/js/jbrowse/extlib/lib/perl5/Data/Stag/StagI.pm
+++
@@ -1,6 +0,0 @@
-package Data::Stag::StagI;
-
-sub z {
-}
-
-1;
