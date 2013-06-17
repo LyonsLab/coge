04c1b10885cc1136d52aa3995f6b6f3becc387d5
Evan Briones <erbriones@gmail.com>
2013-06-17 19:34:51 +0000

    Implemented base functionality of JEX module.

--- modules/Accessory/lib/CoGe/Accessory/Jex.pm
+++ modules/Accessory/lib/CoGe/Accessory/Jex.pm
@@ -1,112 +1,148 @@
+package CoGe::Accessory::Jex;
 use strict;
 use warnings;
-use v5.10.0;
+use 5.10.0;
 
+use Moose;
 use JSON::XS;
-use ZMQ::LibZMQ3;
+use ZMQ::LibZMQ2;
 use ZMQ::Constants qw(ZMQ_REQ);
+use CoGe::Accessory::Workflow;
 
-BEGIN {
-    our($VERSION, %INTERFACES);
-    
-    $VERSION = 0.1;
-    %INTERFACES = (
-        FILE => \&_send_local,
-        HTTP => \&_send_remote,
-    );
+
+# Attributes
+has 'host' => (
+    is => 'ro',
+    isa  => 'Str',
+    required => 1,
+);
+
+has 'local' => (
+    is => 'ro',
+    isa  => 'Bool',
+);
+
+has '_context' => (
+    is => 'ro',
+    lazy => 1,
+    init_arg => undef,
+    default => sub { zmq_init(); },
+);
+
+# Public functions
+sub create_workflow {
+    my ($self, $name) = @_;
+    my $workflow = CoGe::Accessory::Workflow->new(name => $name);
+
+    return $workflow;
 }
 
-package CoGe::Accessory::Jex;
-{
-    use Moose;
 
-    # Attributes
-    has 'uri' => (
-        is => 'ro',
-        isa  => 'Str',
-        required => 1,
-        handles => { uri => 'host' },
-    );
+sub submit_workflow {
+    my ($self, $workflow) = @_;
+    my ($socket, $msg, $jobs);
+    $jobs = $workflow->jobs();
 
-    has 'local' => (
-        is => 'ro',
-        isa  => 'Bool',
-    );
+    my $request = encode_json({
+        request => 'schedule',
+        data => {
+                    'name' => $workflow->name,
+                    'jobs' => $jobs,
+                },
+    });
 
-    has 'workflow' => (
-        is => 'ro',
-        isa => 'ArrayRef',
-        writer => '_set_workflow',
-    );
+    say $request;
+    $socket = zmq_socket($self->_context, ZMQ_REQ);
+    zmq_connect($socket, $self->host);
+    zmq_send($socket, $request);
+    $msg = zmq_recv($socket);
 
-    has '_zmq_context' => (
-        is => 'ro',
-        lazy => 1,
-        builder => '_build_zmq_context',
-        init_arg => undef,
-        handle => 'context',
-    );
+    return zmq_msg_data($msg);
+}
 
-    sub create_workflow {
-        my ($self, $name) = @_;
-        my $workflow = Workflow->new(name => $name);
+sub wait_for_completion {
+    my ($self, $id) = @_;
+    my ($status, $wait);
+    $wait = 1;
+    
+    while (1) {
+        $status = get_status($self, $id);
 
-        return $workflow;
+        say $status;
+        if ($status eq "TERMINATED") {
+            return -1;
+        }
+
+        if ($status eq "COMPLETED") {
+            return 1;
+        }
+        
+        if ($status eq "CANCELLED") {
+            return -1;
+        }
+        
+        if ($status eq "NOT_FOUND") {
+            return -1;
+        }
+
+        sleep $wait;
+        $wait = $wait * 2;
     }
+}
 
-    sub submit_workflow {
-    }
+sub terminate {
+    my ($self, $id) = @_;
+    my ($socket, $resp, $cmd, $msg);
 
-    sub wait_for_compeletion {
-    }
+    $cmd = encode_json { cmd => 'TERM', id => $id};
+    $socket = zmq_socket($self->_context, ZMQ_REQ);
 
-    sub terminate {
-    }
+    zmq_connect($socket, $self->host);
+    zmq_send($socket, $cmd);
 
-    sub get_status {
-    }
+    $resp = zmq_recv($socket);
+    $msg = decode_json(zmq_msg_data($resp));
+
+    return $msg;
+}
+
+sub get_status {
+    my ($self, $id) = @_;
+    my ($socket, $reply, $msg);
+
+    $socket = zmq_socket($self->_context, ZMQ_REQ);
+    zmq_connect($socket, $self->host);
+
+
+    my $request = encode_json({
+        request => 'get_status',
+        data => $id,
+    });
+    
+    $msg = zmq_send($socket, $request);
+    $msg = zmq_recv($socket);
+
+    my $data = zmq_msg_data($msg);
+    my $res = decode_json($data);
+
+    return ${$res}{'status'};
+}
 
 # Private functions
-    sub _build_zmq_context {
-        return zmq_init(); 
-    }
-
-    sub _send_local {
-    }
-
-
-    sub _send_remote {
-
-    }
-}
-package CoGe::Accessory::Workflow
-{
-    use Moose;
-
-    # Attributes
-    has 'workflow_id' => (
-        is => 'ro',
-        isa  => 'Int',
-        handle => 'id',
-    );
-    
-    has 'name' => (
-        is => 'ro',
-        isa  => 'Str',
-        handle => 'id',
-    );
-
-    has 'jobs' => (
-        is => 'rw',
-        isa  => 'ArrayRef',
-        default => sub { [] },
-    );
-
-    # Public functions
-    sub add_job {
-    }
+sub _build_zmq_context {
+    return zmq_init();
 }
 
-__PACKAGE__->meta->make_immutable();
+sub _send_local {
+}
+
+
+sub _send_remote {
+
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
 __END__
+
