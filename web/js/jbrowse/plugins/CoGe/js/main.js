e02f2f780baa72c9e2674e9cd0df4f5d94c7682b
Matt Bombhoff <matthew.bomhoff@gmail.com>
2013-06-19 22:04:40 +0000

    Upgrade to jbrowse 1.9.6

---
+++ web/js/jbrowse/plugins/CoGe/js/main.js
@@ -0,0 +1,19 @@
+define([
+           'dojo/_base/declare',
+           'JBrowse/Plugin'
+       ],
+       function(
+           declare,
+           JBrowsePlugin
+       ) {
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        var browser = args.browser;
+
+        // do anything you need to initialize your plugin here
+        console.log( "CoGe plugin starting" );
+
+    }
+});
+});
