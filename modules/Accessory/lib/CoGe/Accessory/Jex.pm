13cbcd8828451fab8565ecd29d82e30f8376c020
Evan Briones <erbriones@gmail.com>
2013-07-02 01:46:58 +0000

    Implemented base functionality of JEX module.

--- modules/Accessory/lib/CoGe/Accessory/Jex.pm
+++ modules/Accessory/lib/CoGe/Accessory/Jex.pm
@@ -9,131 +9,136 @@ use ZMQ::LibZMQ2;
 use ZMQ::Constants qw(ZMQ_REQ);
 use CoGe::Accessory::Workflow;
 
+
 # Attributes
 has 'host' => (
-    is       => 'ro',
-    isa      => 'Str',
-    required => 1,);
-
-has 'port' => (
-    is       => 'ro',
-    isa      => 'Int',
-    default  => 5151,
-    required => 0,);
+    is => 'ro',
+    isa  => 'Str',
+    required => 1,
+);
 
 has 'local' => (
-    is  => 'ro',
-    isa => 'Bool',);
+    is => 'ro',
+    isa  => 'Bool',
+);
 
 has '_context' => (
-    is       => 'ro',
-    lazy     => 1,
+    is => 'ro',
+    lazy => 1,
     init_arg => undef,
-    default  => sub {zmq_init();},);
+    default => sub { zmq_init(); },
+);
 
 # Public functions
-sub create_workflow
-{
-    my ($self, %opts) = @_;
-
-    my $workflow = CoGe::Accessory::Workflow->new(
-        name    => $opts{name},
-        logfile => $opts{logfile});
+sub create_workflow {
+    my ($self, $name) = @_;
+    my $workflow = CoGe::Accessory::Workflow->new(name => $name);
 
     return $workflow;
 }
 
-sub submit_workflow
-{
+
+sub submit_workflow {
     my ($self, $workflow) = @_;
     my ($socket, $msg, $jobs);
     $jobs = $workflow->jobs();
 
     my $request = encode_json({
-            request => 'schedule',
-            data    => {
-                'name'     => $workflow->name,
-                'logfile'  => $workflow->logfile,
-                'jobs'     => $jobs,},});
+        request => 'schedule',
+        data => {
+                    'name' => $workflow->name,
+                    'jobs' => $jobs,
+                },
+    });
 
+    say $request;
     $socket = zmq_socket($self->_context, ZMQ_REQ);
-    zmq_connect($socket, _connection_string($self->host, $self->port));
+    zmq_connect($socket, $self->host);
     zmq_send($socket, $request);
     $msg = zmq_recv($socket);
 
     return zmq_msg_data($msg);
 }
 
-sub wait_for_completion
-{
+sub wait_for_completion {
     my ($self, $id) = @_;
     my ($status, $wait);
-    $wait = 0;
-
+    $wait = 1;
+    
     while (1) {
         $status = get_status($self, $id);
 
-        given ($status) {
-            when (-1) {return -1;}
-            when (2)  {return 2;}
-            when (3)  {return 3;}
-            when (4)  {return 4;}
-            default {
-                sleep $wait;
-                $wait = $wait + 0.25;
-            }
+        say $status;
+        if ($status eq "TERMINATED") {
+            return -1;
         }
+
+        if ($status eq "COMPLETED") {
+            return 1;
+        }
+        
+        if ($status eq "CANCELLED") {
+            return -1;
+        }
+        
+        if ($status eq "NOT_FOUND") {
+            return -1;
+        }
+
+        sleep $wait;
+        $wait = $wait * 2;
     }
 }
 
-sub terminate
-{
+sub terminate {
     my ($self, $id) = @_;
     my ($socket, $resp, $cmd, $msg);
 
-    $cmd = encode_json {cmd => 'TERM', id => $id};
+    $cmd = encode_json { cmd => 'TERM', id => $id};
     $socket = zmq_socket($self->_context, ZMQ_REQ);
 
-    zmq_connect($socket, _connection_string($self->host, $self->port));
+    zmq_connect($socket, $self->host);
     zmq_send($socket, $cmd);
 
     $resp = zmq_recv($socket);
-    $msg  = decode_json(zmq_msg_data($resp));
+    $msg = decode_json(zmq_msg_data($resp));
 
     return $msg;
 }
 
-sub get_status
-{
+sub get_status {
     my ($self, $id) = @_;
     my ($socket, $reply, $msg);
 
     $socket = zmq_socket($self->_context, ZMQ_REQ);
-    zmq_connect($socket, _connection_string($self->host, $self->port));
+    zmq_connect($socket, $self->host);
+
 
     my $request = encode_json({
-            request => 'get_status',
-            data    => $id,});
-
+        request => 'get_status',
+        data => $id,
+    });
+    
     $msg = zmq_send($socket, $request);
     $msg = zmq_recv($socket);
 
     my $data = zmq_msg_data($msg);
-    my $res  = decode_json($data);
+    my $res = decode_json($data);
 
     return ${$res}{'status'};
 }
 
 # Private functions
-sub _build_zmq_context
-{
+sub _build_zmq_context {
     return zmq_init();
 }
 
-sub _connection_string
-{
-    my ($host, $port) = @_;
-    return "tcp://$host:$port";
+sub _send_local {
+}
+
+
+sub _send_remote {
+
 }
 
 __PACKAGE__->meta->make_immutable;
