283067051082b13dfc6c805b756b37d25afa952f
Evan Briones <erbriones@gmail.com>
2014-01-05 13:03:38 -0700

    added wikifeed
    The wikifeed script parses the wiki and generates a wiki feed that
    lists the most recent posts.

---
+++ web/js/coge/wikifeed.js
@@ -0,0 +1,98 @@
+window.wikifeed = function(url, element, size) {
+    var posts = [],
+        numberOfPosts = size || 5,
+        slice = Array.prototype.slice;
+
+    function getIndex(children) {
+        return function(value, index, list) {
+            return children.index(value);
+        };
+    }
+
+    function getRange(value, index, list) {
+        return [value, list[index + 1]];
+    };
+
+    function getData(children) {
+        return function(value, index, list) {
+            //offset by 1 to get anchor tag
+            var offset = [value[0] - 1, value[1] - 1];
+            return slice.apply(children, offset);
+        };
+    }
+
+    function formatPost(value, index, list) {
+        var id = $(value[0]).attr("id");
+
+        var link = $("<a></a>");
+        link.attr("href", url + "#" + id)
+            .attr("target", "_blank")
+            .addClass("ui-icon ui-icon-extlink")
+            .css("float", "right");
+
+        var content = $("<div></div>");
+
+        var items = value.splice(3, value.length);
+        items.unshift(link);
+
+        var title = $(value[1]);
+        var text = title.text().replace(/\[edit\]/gmi, "");
+        var date = $("<span></span>").html($(value[2]).text())
+            .addClass("date");
+
+        return {
+            title: title.text(text).append(date),
+            content: content.html(items)
+        };
+    }
+
+    function parsePosts(html) {
+        var feed = $(html).find("#bodyContent");
+        var children = feed.children();
+
+        var headers = feed.find("h2").toArray();
+
+        if (!headers.length) return console.warn("Could not fetch headers");
+
+        var indexes = headers.map(getIndex(children));
+        indexes.push(children.length);
+        var ranges = indexes.map(getRange);
+        var items = ranges.map(getData(children.toArray()));
+
+        posts = items.map(formatPost).slice(0, -1);
+        console.log(posts);
+        render();
+    }
+
+    function render() {
+        var feed = $("<div></div>");
+
+        for(var i = 0; i < numberOfPosts; i++) {
+            feed.append(posts[i].title);
+            feed.append(posts[i].content);
+        }
+
+        feed.accordion({
+            autoHeight: false,
+            icons: false
+        });
+
+        var footer = $("<a>...more...<a>").attr("href", url)
+            .attr("target", "_blank")
+            .css("text-align", "center")
+            .css("display", "block")
+            .css("margin", "3px");
+
+        element.append(feed);
+        $(".thumbcaption").remove();
+        feed.append(footer);
+    }
+
+    $.ajax({
+        url: url,
+        dataType: "html",
+        success: parsePosts
+    });
+
+    return this;
+};
