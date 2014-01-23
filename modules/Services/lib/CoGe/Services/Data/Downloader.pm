fa967aaab32df8c21865eab6c683b82a2b11d3e7
Evan Briones <erbriones@gmail.com>
2014-01-22 21:34:20 -0700

    add support for experiment downloads

--- modules/Services/lib/CoGe/Services/Data/Downloader.pm
+++ modules/Services/lib/CoGe/Services/Data/Downloader.pm
@@ -19,21 +19,35 @@ sub get {
     my $page = $self->param('page');
     my $file = $self->query->param('file');
     my $gid = $self->query->param('gid');
+    my $eid = $self->query->param('eid');
 
     # Connect to the database
     my ( $db, $user, $conf ) = CoGe::Accessory::Web->init();
-    my $genome = $db->resultset('Genome')->find($gid);
 
-    if ( $genome->restricted
-        and ( not defined $user or not $user->has_access_to_genome($genome) ) )
-    {
-        print STDERR "Data::Sequence::get access denied to genome $gid\n";
-        return;
+    if ($gid) {
+        my $genome = $db->resultset('Genome')->find($gid);
+
+        if ( $genome->restricted
+            and ( not defined $user or not $user->has_access_to_genome($genome) ) )
+        {
+            print STDERR "Data::Sequence::get access denied to genome $gid\n";
+            return;
+        }
+
+        @path = ($conf->{SECTEMPDIR}, $page, "downloads", $eid, $file);
+    } elsif ($eid) {
+        my $exp = $db->resultset('Experiment')->find($eid);
+        if ($exp->restricted and
+            (not defined $user or not $user->has_access_to_experiment($exp))) {
+            print STDERR "Data::Sequence::get access denied to experiment $eid\n";
+            return;
+        }
+
+        @path = ($conf->{SECTEMPDIR}, $page, "downloads", $eid, $file);
     }
 
     $self->header_add( -attachment => $file );
 
-    my @path = ($conf->{SECTEMPDIR}, $page, "downloads", $gid, $file);
     my $file_path = File::Spec->catdir(@path);
     my $content;
 
