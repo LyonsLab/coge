e44b9cc104b2520e87ed7c8e9b9f8cec9f405df3
Evan Briones <erbriones@gmail.com>
2014-02-18 16:23:02 -0700

    Returns a new fasta file with updated headers
    The script will go through and strip extra data from the fasta headers
    and generate a new fasta file with the additional header information
    removed.

---
+++ scripts/fasta_reheader.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+#-------------------------------------------------------------------------------
+# Purpose:  Strip "gi|" and "lcl|" off of input filename.  Used by qTeller pipeline.
+# Author:   mdb
+# Created:  2/10/14
+#------------------------------------------------------------------------------
+my @filter = ('gi\|', 'lcl\|');
+#-------------------------------------------------------------------------------
+
+use strict;
+
+die "Usage:  fasta_reheader.pl <input_file> <output_file>\n" if ($#ARGV+1 < 2);
+
+my $INPUT_FILE = shift @ARGV;
+my $OUTPUT_FILE = shift @ARGV;
+open(my $inf, $INPUT_FILE) or
+    die("Error: cannot open file '$INPUT_FILE'\n");
+open(my $outf, ">$OUTPUT_FILE") or
+    die("Error: cannot open file '$OUTPUT_FILE'\n");
+
+while (<$inf>) {
+    if (/^\>\s*(\S+)/) {
+        my $name = $1;
+        foreach (@filter) {
+            $name =~ s/^$_//;
+        }
+        print $outf '>', $name, "\n";
+    }
+    else {
+        print $outf $_;
+    }
+}
+
+close($inf);
+close($outf);
+exit;
