73782e10a5098b4806cb2f015b30e9126ce09a45
mbomhoff <mbomhoff@email.arizona.edu>
2014-02-26 08:07:39 -0700

    Convert to intervals instead of per position

--- scripts/pileup_to_bed.pl
+++ scripts/pileup_to_bed.pl
@@ -5,23 +5,53 @@
 # Author:   mdb
 # Created:  2/24/14
 #-------------------------------------------------------------------------------
-my $MIN_DEPTH = 5;
-my $FIX_CHR_NAME = 1;
-#-------------------------------------------------------------------------------
 
 use strict;
 
+# Simple per position conversion
+#while (<>) {
+#    my ($chr, $pos, undef, $depth) = split("\t", $_);
+#    if ($depth >= $MIN_DEPTH) {
+#        if ($FIX_CHR_NAME) {
+#            $chr =~ s/^lcl\|//;
+#            $chr =~ s/^gi\|//;
+#        }
+#        print join("\t", $chr, $pos, $pos, '.', $depth, '+'), "\n";
+#    }
+#}
+
+# Create intervals
+my ($start, $stop);
+my ($prevChr, $prevDepth);
 while (<>) {
     my ($chr, $pos, undef, $depth) = split("\t", $_);
-    if ($depth >= $MIN_DEPTH) {
-        if ($FIX_CHR_NAME) {
-            $chr =~ s/^lcl\|//;
-            $chr =~ s/^gi\|//;
+    
+    if (!defined $start || ($pos-$stop > 1) || $depth != $prevDepth || $chr ne $prevChr) {
+        # Print interval
+        if (defined $start) {
+            $prevChr =~ s/^lcl\||gi\|//;
+            print join("\t", $prevChr, $start, $stop+1, '.', $prevDepth, '+'), "\n";
         }
-        print join("\t", $chr, $pos, $pos, '.', $depth, '+'), "\n";
+        
+        # Reset interval
+        $start = $pos;
+        $prevDepth = $depth;
+        $prevChr = $chr;
     }
+    
+    $stop = $pos;
+}
+
+# Print last interval
+if (defined $start and $start != $stop) {
+    $prevChr =~ s/^lcl\||gi\|//;
+    print join("\t", $prevChr, $start, $stop+1, '.', $prevDepth, '+'), "\n";
 }
 
 exit;
 
 #-------------------------------------------------------------------------------
+sub fix_chr_name {
+    $prevChr =~ s/^lcl\|//;
+    $prevChr =~ s/^gi\|//;
+}
