b7a1501b50d3d856d1baaecc3600b734c9ff5cdd
Evan Briones <erbriones@gmail.com>
2014-03-13 13:39:54 -0700

    fix typo to remove text from status dialog
    	modified:   js/synfind.js

--- web/js/synfind.js
+++ web/js/synfind.js
@@ -62,7 +62,7 @@ function launch(dialog, results, options) {
                 // reset dialog
                 status_dialog.find(".dialog-error,.dialog-complete").hide();
                 status_dialog.find(".dialog-running").show();
-                status_dialog.find(".dialog-text").hmtl("");
+                status_dialog.find(".dialog-text").html("");
                 status_dialog.find(".dialog-link").html("");
                 status_dialog.find(".dialog-log").html("");
             });
