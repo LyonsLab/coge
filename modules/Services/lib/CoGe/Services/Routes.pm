0c2354abddeee3d1f8ad1f545b83400bb474233f
Evan Briones <erbriones@gmail.com>
2014-04-02 15:44:59 -0700

    add initial web service support
    Create basic Mojolicious application with routes and stub controllers.
    Adds basic endpoints for Organism search and fetch as an example
    implementation as a guide for stub end points.
    
    * Add startup script for Mojolicious application
    * Add stub controllers in CoGe::Services::Data
    * Add basic routes in Routes.pm
    
    	modified:   modules/MANIFEST
    	modified:   modules/Services/MANIFEST
    	new file:   modules/Services/lib/CoGe/Services/Data/Experiment.pm
    	new file:   modules/Services/lib/CoGe/Services/Data/Genome2.pm
    	new file:   modules/Services/lib/CoGe/Services/Data/Group.pm
    	new file:   modules/Services/lib/CoGe/Services/Data/Organism.pm
    	new file:   modules/Services/lib/CoGe/Services/Data/User.pm
    	new file:   modules/Services/lib/CoGe/Services/Routes.pm
    	new file:   web/services/coge_mojolicious

---
+++ modules/Services/lib/CoGe/Services/Routes.pm
@@ -0,0 +1,61 @@
+package CoGe::Services::Routes;
+use Mojo::Base "Mojolicious";
+
+sub startup {
+    my $self = shift;
+
+    my $r = $self->routes->namespaces(["CoGe::Services::Data"]);
+
+    # Organism routes
+    $r->get("/organisms/search/#term")
+        ->name("organisms-search")
+        ->to("organism#search", term => undef);
+
+    $r->get("/organisms/fetch/:id" => [id => qr/\d+/])
+        ->name("organisms-fetch")
+        ->to("organism#fetch", id => undef);
+
+    $r->post("/organisms")
+        ->name("organisms-add")
+        ->to("organism#add");
+
+    # Genome routes
+    $r->get("/genomes/search/#term")
+        ->name("genomes-search")
+        ->to("genome2#search", term => undef);
+
+    $r->get("/genomes/fetch/:id" => [id => qr/\d+/])
+        ->name("genomes-fetch")
+        ->to("genome2#fetch", id => undef);
+
+    # Experiment routes
+    $r->get("/experiments/search/#term")
+        ->name("experiments-search")
+        ->to("experiment#search", term => undef);
+
+    $r->get("/experiments/fetch/:id" => [id => qr/\d+/])
+        ->name("experiments-fetch")
+        ->to("experiment#fetch", id => undef);
+
+    # User routes
+    $r->get("/users/search/#term")
+        ->name("users-search")
+        ->to("user#search", term => undef);
+
+    $r->get("/users/fetch/:id" => [id => qr/\d+/])
+        ->name("users-fetch")
+        ->to("user#fetch", id => undef);
+
+    # User group routes
+    $r->get("/groups/search/#term")
+        ->name("groups-search")
+        ->to("group#search", term => undef);
+
+    $r->get("/groups/fetch/:id" => [id => qr/\d+/])
+        ->name("groups-fetch")
+        ->to("group#fetch", id => undef);
+
+    # Job routes
+}
+
+1;
