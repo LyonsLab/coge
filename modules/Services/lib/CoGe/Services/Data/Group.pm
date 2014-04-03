27d62f434f6d857ec7030d1d6ba0ddc0c5f75b70
Evan Briones <erbriones@gmail.com>
2014-04-03 13:16:03 -0700

    add items action to Group Controller
    A user can fetch all items shared with the group, if they are a member
    of the group. When the user is not a member of the group an Auth error
    is returned. A public user will always recieve an Auth error when
    trying to access items.
    
    	modified:   lib/CoGe/Services/Data/Group.pm

--- modules/Services/lib/CoGe/Services/Data/Group.pm
+++ modules/Services/lib/CoGe/Services/Data/Group.pm
@@ -62,7 +62,7 @@ sub fetch {
     # Connect to the database
     my ( $db, $user, $conf ) = CoGe::Accessory::Web->init(ticket => $key);
 
-    # Deny a public user access to any user
+    # Deny a public user access to any group
     if ($user->is_public) {
         $self->render(json => {
             error => { Auth => "Access denied"}
@@ -93,8 +93,63 @@ sub fetch {
 
 sub items {
     my $self = shift;
+    my $id = int($self->stash('id'));
+    my $key = $self->param("apiKey");
+
+    # Connect to the database
+    my ( $db, $user, $conf ) = CoGe::Accessory::Web->init(ticket => $key);
+
+    # Deny a public user access to any group
+    if ($user->is_public) {
+        $self->render(json => {
+            error => { Auth => "Access denied"}
+        }, status => 401);
+        return;
+    }
+
+    my $group = $db->resultset("UserGroup")->find($id);
+
+    unless (defined $group) {
+        $self->render(json => {
+            error => { Error => "Item not found"}
+        });
+        return;
+    }
+
+    # Restrict access to users in the group
+    unless ($group->has_member($user)) {
+        $self->render(json => {
+            error => { Auth => "Access denied"}
+        }, status => 401);
+        return;
+    }
+
+    # Format experiments
+    my @experiments = map {
+        {
+            id => int($_->id),
+            type => "experiment",
+        }
+    } $group->experiments;
+
+    # Format Genomes
+    my @genomes = map {
+        {
+            id => int($_->id),
+            type => "genome",
+        }
+    } $group->genomes;
+
+    # Format Lists
+    my @notebooks = map {
+        {
+            id => int($_->id),
+            type => "notebook"
+        }
+    } $group->lists;
+
     $self->render(json => {
-        items => []
+        items => [@experiments, @genomes, @notebooks]
     });
 }
 
