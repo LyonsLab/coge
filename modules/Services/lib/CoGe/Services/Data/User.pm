8e8d0ee044cf073ae915253a97026e832c148e29
Evan Briones <erbriones@gmail.com>
2014-04-03 10:38:24 -0700

    add search action for User
    Searches for all users that match the search term specified. Returns
    the users id, username, first name, last name and description. This
    subset of user information is available to all authenticated users. A
    public user is denied access to user information.
    
    	modified:   lib/CoGe/Services/Data/User.pm

--- modules/Services/lib/CoGe/Services/Data/User.pm
+++ modules/Services/lib/CoGe/Services/Data/User.pm
@@ -7,7 +7,45 @@ use CoGe::Accessory::Web;
 
 sub search {
     my $self = shift;
-    $self->render(json => { success => Mojo::JSON->true });
+    my $search_term = $self->stash('term');
+    my $key = $self->param("apiKey");
+
+    # Connect to the database
+    my ( $db, $user, $conf ) = CoGe::Accessory::Web->init(ticket => $key);
+
+    # Deny a public user access to users
+    if ($user->is_public) {
+        $self->render(json => {
+            error => { Auth => "Access denied"}
+        }, status => 401);
+        return;
+    }
+
+    # Search users
+    my $search_term2 = '%' . $search_term . '%';
+    my @users = $db->resultset("User")->search(
+        \[
+            'user_id= ? OR user_name LIKE ? OR first_name LIKE ?
+                OR last_name LIKE ?',
+            [ 'user_id', $search_term ],
+            [ 'user_name', $search_term2 ],
+            [ 'first_name', $search_term2 ],
+            [ 'last_name', $search_term2 ]
+        ]
+    );
+
+    # Format response
+    my @result = map {
+      {
+        id => int($_->id),
+        user_name => $_->user_name,
+        first_name => $_->first_name,
+        last_name => $_->last_name,
+        description => $_->description
+      }
+    } @users;
+
+    $self->render(json => { users => \@result });
 }
 
 sub fetch {
