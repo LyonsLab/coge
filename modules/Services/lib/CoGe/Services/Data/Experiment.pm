c06766815dd6b88e586983d0d7738afdd66f3708
Evan Briones <erbriones@gmail.com>
2014-04-03 09:47:52 -0700

    implement search action for Experiment
    A user can search for experiments that they have access to. The user
    authenticates using a cookie from a browser or providing an api key
    as the query parameter apiKey.
    
    	modified:   lib/CoGe/Services/Data/Experiment.pm

--- modules/Services/lib/CoGe/Services/Data/Experiment.pm
+++ modules/Services/lib/CoGe/Services/Data/Experiment.pm
@@ -7,7 +7,44 @@ use CoGe::Accessory::Web;
 
 sub search {
     my $self = shift;
-    $self->render(json => { success => Mojo::JSON->true});
+    my $search_term = $self->stash('term');
+    my $key = $self->param("apiKey");
+
+    # Validate input
+    if (!$search_term or length($search_term) < 3) {
+        $self->render(json => { status => Mojo::JSON->false, error => 'Too many search results'});
+        return;
+    }
+
+    # Connect to the database
+    my ( $db, $user, $conf ) = CoGe::Accessory::Web->init(ticket => $key);
+
+    # Search genomes
+    my $search_term2 = '%' . $search_term . '%';
+    my @experiments = $db->resultset("Experiment")->search(
+        \[
+            'experiment_id= ? OR name LIKE ? OR description LIKE ?',
+            [ 'experiment_id', $search_term  ],
+            [ 'name',        $search_term2 ],
+            [ 'description', $search_term2 ]
+        ]
+    );
+
+    # Filter response
+    my @filtered = grep {
+        $user->has_access_to_experiment($_);
+    } @experiments;
+
+    # Format response
+    my @result = map {
+      {
+        id => int($_->id),
+        name => $_->name,
+        description => $_->description,
+      }
+    } @filtered;
+
+    $self->render(json => { experiments => \@result });
 }
 
 sub fetch {
