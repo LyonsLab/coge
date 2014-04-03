9d82a1a1a89317a76e49ea6bdbb2a071784f4b30
Evan Briones <erbriones@gmail.com>
2014-04-03 11:30:25 -0700

    add search action to Group Controller
    A user can search for any group. If a public user searches for a group
    the user will recieve an Auth error.
    
    	modified:   lib/CoGe/Services/Data/Group.pm

--- modules/Services/lib/CoGe/Services/Data/Group.pm
+++ modules/Services/lib/CoGe/Services/Data/Group.pm
@@ -7,7 +7,51 @@ use CoGe::Accessory::Web;
 
 sub search {
     my $self = shift;
-    $self->render(json => { success => Mojo::JSON->true });
+    my $search_term = $self->stash('term');
+    my $key = $self->param("apiKey");
+
+    # Connect to the database
+    my ( $db, $user, $conf ) = CoGe::Accessory::Web->init(ticket => $key);
+
+    # Deny a public group access to groups
+    if ($user->is_public) {
+        $self->render(json => {
+            error => { Auth => "Access denied"}
+        }, status => 401);
+        return;
+    }
+
+    # Validate input
+    if (!$search_term or length($search_term) < 3) {
+        $self->render(json => { status => Mojo::JSON->false, error => 'Too many search results'});
+        return;
+    }
+
+    # Search groups
+    my $search_term2 = '%' . $search_term . '%';
+    my @groups = $db->resultset("UserGroup")->search(
+        \[
+            'user_group_id= ? OR name LIKE ? OR description LIKE ?',
+            [ 'user_group_id', $search_term ],
+            [ 'name', $search_term2 ],
+            [ 'description', $search_term2 ],
+        ]
+    );
+
+    # Format response
+    my @result = map {
+      {
+        id => int($_->id),
+        name => $_->name,
+        description => $_->description,
+        role => {
+            name => $_->role->name,
+            description => $_->role->description
+        }
+      }
+    } @groups;
+
+    $self->render(json => { groups => \@result });
 }
 
 sub fetch {
