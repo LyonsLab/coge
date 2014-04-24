3a0ae66422f0ac6708abbc5a7db7fcd679c1eff5
mbomhoff <mbomhoff@email.arizona.edu>
2014-04-23 17:06:46 -0700

    Correct typo

--- web/GenomeInfo.pl
+++ web/GenomeInfo.pl
@@ -1594,7 +1594,7 @@ sub get_annotations {
         $html .= '</tbody></table>';
     }
     elsif ($user_can_edit) {
-        $html .= '<table class="ui-widget-content ui-corner-all small padded note"><tr><td>There a no additional metadata items for this genome.</tr></td></table>';
+        $html .= '<table class="ui-widget-content ui-corner-all small padded note"><tr><td>There are no additional metadata items for this genome.</tr></td></table>';
     }
 
     if ($user_can_edit) {
