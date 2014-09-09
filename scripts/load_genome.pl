f2560496a658bf02f496ade78e0ea8af092b7bbe
Evan Briones <erbriones@gmail.com>
2014-09-09 16:17:34 -0700

    add creator connector support to load genome

--- scripts/load_genome.pl
+++ scripts/load_genome.pl
@@ -267,6 +267,8 @@ unless ($user) {
     exit(-1);
 }
 my $node_types = CoGeX::node_types();
+
+# Add owner connector
 my $conn       = $coge->resultset('UserConnector')->create(
     {
         parent_id   => $user->id,
@@ -280,6 +282,24 @@ unless ($conn) {
     print STDOUT "log: error creating user connector\n";
     exit(-1);
 }
+
+
+# Add creator connector
+my $creator_conn       = $coge->resultset('UserConnector')->create(
+    {
+        parent_id   => $user->id,
+        parent_type => $node_types->{user},
+        child_id    => $genome->id,
+        child_type  => $node_types->{genome},
+        role_id     => 5                        # FIXME hardcoded
+    }
+);
+
+unless ($conn) {
+    print STDOUT "log: error creating user connector\n";
+    exit(-1);
+}
+
 
 # Create datasets
 my %datasets;
