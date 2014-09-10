5be2ddcca6931b73087ab408696445f023dfba87
Evan Briones <erbriones@gmail.com>
2014-09-10 13:12:55 -0700

    partially cleanup formatting

--- web/js/pages/organism-view.js
+++ web/js/pages/organism-view.js
@@ -4,135 +4,156 @@
 // Initialize jQuery AJAX
 //
 $.ajaxSetup({
-    type: "GET",
-    url: "OrganismView.pl",
-    dataType: "html",
-    cache: false,
+        type: "GET",
+        url: "OrganismView.pl",
+        dataType: "html",
+        cache: false,
 });
 
+
+function comparator(a, b) {
+    return (a > b) ? 1 : (a < b) ? -1 : 0;
+}
+
+/*
+ * JQuery extensions
+ */
 $.fn.sortSelect = function(){
-  this.each(
-          function()
-          {
-                if(this.nodeName.toLowerCase() !== "select"){
-                  return;}
-                var opts = this.options;
-                var sortArray = [];
-                for(var i=0;i<opts.length;i++)
-                {
-                        sortArray[i] = {
-                                        v: opts[i].value,
-                                        t: opts[i].text,
-                                        d: opts[i].id,
-                                        }
-                }
-                sortArray.sort(
-                                function(obj1,obj2)
-                                {
-                                        obj1t = obj1.t.toLowerCase(),
-                                        obj2t = obj2.t.toLowerCase();
-                                        if(obj1t == obj2t){
-                                          return 0;}
-                                        return obj1t < obj2t ? -1 : 1;
-                                }
-                );
-                for(var i=0;i<opts.length;i++)
-                {
-                        opts[i].id = sortArray[i].d;
-                        opts[i].text = sortArray[i].t;
-                        opts[i].value = sortArray[i].v;
-                }
-          }
-        );
-        return this;
+    this.each(function() {
+        var i,
+            opts = this.options,
+            sortArray = [];
+
+        if(this.nodeName.toLowerCase() !== "select") {
+            return;
+        }
+
+        for(i = 0; i < opts.length; i++) {
+            sortArray[i] = {
+                v: opts[i].value,
+                t: opts[i].text,
+                d: opts[i].id,
+            };
+        }
+
+        sortArray.sort(function(obj1, obj2) {
+            var obj1t = obj1.t.toLowerCase(),
+                obj2t = obj2.t.toLowerCase();
+
+            return comparator(obj1t, obj2t);
+        });
+
+        for(i = 0; i < opts.length; i++) {
+            opts[i].id = sortArray[i].d;
+            opts[i].text = sortArray[i].t;
+            opts[i].value = sortArray[i].v;
+        }
+    });
+    return this;
+};
+
+$.fn.getLength = function(val){
+    var opt_length;
+    var blastable;
+
+    this.each(function() {
+        var i,
+            opts = this.options;
+
+        opt_length = opts.length;
+
+        if (opt_length === 0) {
+            return opt_length;
+        }
+
+        blastable = opts[0].id;
+
+        if (val) {
+            for(i = 1; i <opts.length; i++) {
+                blastable += ","+opts[i].id;
+                //need to chopoff last comma
+            }
+        }
+    });
+
+    if (val) {
+        return blastable;
+    }
+
+    return opt_length;
 };
 
 function ajax_wait (val){
-if (ajax.length)
-   {
-    setTimeout("ajax_wait("+'"'+val+'"'+")",100);
-    return;
-   }
-  eval(val);
+    if (ajax.length) {
+        setTimeout("ajax_wait("+'"'+val+'"'+")",100);
+        return;
+    }
+    eval(val);
 }
-$.fn.getLength = function(val){
-  var opt_length;
-  var blastable;
-  this.each(
-        function()
-        {
-                var opts = this.options;
-                opt_length = opts.length;
-                if (opt_length == 0) {return opt_length;}
-                blastable = opts[0].id;
-                if (val){
-                  for(var i=1;i<opts.length;i++)
-                  {
-                        blastable += ","+opts[i].id;
-                        //need to chopoff last comma
-                  }
-                }
-        }
-  );
-  if(val) return blastable;
-  else return opt_length;
-};
 
 function send_to_GenoList(){
-  var check = $('#genomelist_choice').getLength();
-  if(($('#blank').val())||(check==0)){
-    alert('You have not selected any Genomes to examine. You must select at least one.');
-    return;
-  }
-  var genolist = $('#genomelist_choice').getLength(1);
-  parse_for_GenoList(['args__'+genolist],[send_to_url]);
+    var check = $('#genomelist_choice').getLength();
+    if(($('#blank').val())||(check==0)){
+        alert('You have not selected any Genomes to examine. You must select at least one.');
+        return;
+    }
+    var genolist = $('#genomelist_choice').getLength(1);
+    parse_for_GenoList(['args__'+genolist],[send_to_url]);
 }
 
 function add_all_genomes(){
     $('#dsg_id option').each(function(){
-        add_to_genomelist_nowarn($(this).text(),$(this).attr("value"));
+            add_to_genomelist_nowarn($(this).text(),$(this).attr("value"));
     });
     $('#geno_list').dialog('option', 'width', 500).dialog('open');
 }
 
+function counting() {
+    var count=0;
+    if($('#blank').val()){ count =0;}
+    else { count = $('#genomelist_choice')[0].length;}
+        if(count==0) { $('#genomelist_choice').html('<option id="blank" value=null>No genome selected</option>') ; }
+    $('#count').html('Genome Count:'+count);
+}
+
 function remove_selected_genomes(){
-  $('#genomelist_choice option:selected').each(function() {
-          //$('#'+$(this).val()).remove();
-      $(this).remove();
-  });
-  counting();
+    $('#genomelist_choice option:selected').each(function() {
+            //$('#'+$(this).val()).remove();
+            $(this).remove();
+    });
+    counting();
 }
 
 function send_to_url(url){
-  window.open(url);
+    window.open(url);
 }
 
 function add_to_genomelist_nowarn(genome_name,gstid)
 {
     var check = $('#'+gstid).val();
-      if (check){
+    if (check){
 
-       return;
-      }else{
-      var html = '<option id='+gstid+' value='+gstid+ ' >'+genome_name+'</option>';
-      //alert(html);
-      $('#blank').remove();
-      $('#genomelist_choice').append(html);
-      counting();
+        return;
+    }else{
+        var html = '<option id='+gstid+' value='+gstid+ ' >'+genome_name+'</option>';
+        //alert(html);
+        $('#blank').remove();
+        $('#genomelist_choice').append(html);
+        counting();
     }
 }
 
 function add_to_genomelist(genome_name,gstid) {
     var check = $('#'+gstid).val();
-      if (check){
-       alert('You have already added '+genome_name+'.');
-       return;
-      }
-      var html = '<option id='+gstid+' value='+gstid+ ' >'+genome_name+'</option>';
-      //alert(html);
-      $('#blank').remove();
-      $('#genomelist_choice').append(html);
-      counting();
+    if (check){
+        alert('You have already added '+genome_name+'.');
+        return;
+    }
+    var html = '<option id='+gstid+' value='+gstid+ ' >'+genome_name+'</option>';
+    //alert(html);
+    $('#blank').remove();
+    $('#genomelist_choice').append(html);
+    counting();
 
 }
 
@@ -150,17 +171,9 @@ function clear_genome_list(){
     counting();
 }
 
-function counting(){
-    var count=0;
-    if($('#blank').val()){ count =0;}
-    else { count = $('#genomelist_choice')[0].length;}
-    if(count==0) { $('#genomelist_choice').html('<option id="blank" value=null>No genome selected</option>') ; }
-    $('#count').html('Genome Count:'+count);
-}
-
 function add_all_org(){
     $("#org_id option").each(function(){
-         get_genome_list_for_org(['args__oid','args__'+$(this).attr('value')],[add_all_genomes_from_org]);
+            get_genome_list_for_org(['args__oid','args__'+$(this).attr('value')],[add_all_genomes_from_org]);
     });
 }
 
@@ -175,14 +188,6 @@ function add_all_genomes_from_org(val){
             }
         }
     }
-}
-
-function get_organism_name_chain(val) {
-    get_orgs(['args__name','args__'+val, 'args__dsgid','dsg_id'], [get_org_info_chain]);
-}
-
-function get_organism_desc_chain(val) {
-    get_orgs(['args__desc','args__'+val, 'args__dsgid','dsg_id'], [get_org_info_chain]);
 }
 
 function get_organism_chain(val) {
@@ -288,45 +293,47 @@ function timing(val){
 }
 
 function get_feat_gc(opts){
- if (!opts){opts={};}
- chr = opts.chr;
- typeid = opts.typeid;
- min = $('#feat_gc_min').val();
- max = $('#feat_gc_max').val();
- hist_type = $('#feat_hist_type').val();
- dsid = opts.dsid;
- dsgid = $('#dsg_id').val();
- $('#gc_histogram').html('loading...');
+    opts = opts || {};
+    chr = opts.chr;
+    typeid = opts.typeid;
+    min = $('#feat_gc_min').val();
+    max = $('#feat_gc_max').val();
+    hist_type = $('#feat_hist_type').val();
+    dsid = opts.dsid;
+    dsgid = $('#dsg_id').val();
+    $('#gc_histogram').html('loading...');
 
- $.ajax({
-   data: {
-      dsgid: dsgid[0],
-      jquery_ajax: 1,
-          fname: 'get_gc_for_feature_type',
-      dsid: dsid,
-      typeid: typeid,
-      chr: chr,
-      min: min,
-      max: max,
-      hist_type: hist_type,
-         },
-   success: function(data) {$('#gc_histogram').html(data);}
- });
+    $.ajax({
+        data: {
+            dsgid: dsgid[0],
+            jquery_ajax: 1,
+            fname: 'get_gc_for_feature_type',
+            dsid: dsid,
+            typeid: typeid,
+            chr: chr,
+            min: min,
+            max: max,
+            hist_type: hist_type,
+        },
+        success: function(data) {
+            $('#gc_histogram').html(data);
+        }
+    });
 }
 
 function edit_genome_info (dsgid) {
- $.ajax({
+    $.ajax({
         data: {
-           jquery_ajax: 1,
-       fname: 'edit_genome_info',
-       dsgid: dsgid,
-           },
-        success : function(val) {
-          $('#edit_genome_info').html(val);
-          $('#edit_genome_info').dialog('open');
-          get_genome_info(['args__dsgid','dsg_id'],[dataset_chain]);
-    },
-      });
+            jquery_ajax: 1,
+            fname: 'edit_genome_info',
+            dsgid: dsgid,
+        },
+            success : function(val) {
+                $('#edit_genome_info').html(val);
+                $('#edit_genome_info').dialog('open');
+                get_genome_info(['args__dsgid','dsg_id'],[dataset_chain]);
+            },
+    });
 }
 
 function update_genome_info (dsgid) {
@@ -336,22 +343,22 @@ function update_genome_info (dsgid) {
     dsg_message = $('#dsg_message').val();
     dsg_link = $('#dsg_link').val();
     $.ajax({
-        data: {
-            jquery_ajax: 1,
-            fname: 'update_genome_info',
-            dsgid: dsgid,
-            name: dsg_name,
-            desc: dsg_desc,
-            ver: dsg_ver,
-            message: dsg_message,
-            link: dsg_link
-        },
-        success : function(val) {
-            if (val != 1) {alert(val);}
-            $('#edit_genome_info').html('');
-            $('#edit_genome_info').dialog('close');
-            get_genome_info(['args__dsgid','dsg_id'], [dataset_chain]);
-        },
+            data: {
+                jquery_ajax: 1,
+                fname: 'update_genome_info',
+                dsgid: dsgid,
+                name: dsg_name,
+                desc: dsg_desc,
+                ver: dsg_ver,
+                message: dsg_message,
+                link: dsg_link
+            },
+            success : function(val) {
+                if (val != 1) {alert(val);}
+                $('#edit_genome_info').html('');
+                $('#edit_genome_info').dialog('close');
+                get_genome_info(['args__dsgid','dsg_id'], [dataset_chain]);
+            },
     });
 }
 
