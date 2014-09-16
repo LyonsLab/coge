de800589dc674b9a8ae7ce5e4117ca630e86de36
Evan Briones <erbriones@gmail.com>
2014-09-16 09:06:57 -0700

    retab LogUser

--- modules/Accessory/lib/CoGe/Accessory/LogUser.pm
+++ modules/Accessory/lib/CoGe/Accessory/LogUser.pm
@@ -6,41 +6,41 @@ use Data::Dumper;
 use CoGeX::Result::User;
 
 sub get_user {
-	my $self        = shift;
-	my %opts        = @_;
-	my $cookie_name = $opts{cookie_name};
-	my $coge        = $opts{coge};
-	my %cookies     = fetch CGI::Cookie;
-	my ( $user, $uid, $session );    # = "Public";
+    my $self        = shift;
+    my %opts        = @_;
+    my $cookie_name = $opts{cookie_name};
+    my $coge        = $opts{coge};
+    my %cookies     = fetch CGI::Cookie;
+    my ( $user, $uid, $session );    # = "Public";
 
-	#print STDERR "LogUser::get_user cookie=$cookie_name " . (defined $cookies{$cookie_name} ? 'exists' : '!exists') . "\n";
-	if ( $cookie_name && ref $cookies{$cookie_name} ) {
-		my %session = $cookies{$cookie_name}->value;
-		$session = $session{session};
-		my ($user_session) = $coge->resultset("UserSession")->find( { session => $session } );
-		$user = $user_session->user if $user_session;
-	}
-	unless ($user) {
-		$user = new CoGeX::Result::User;
-		$user->user_name("public");
-		$user->admin(0);
-	}
-	return ($user);
+    #print STDERR "LogUser::get_user cookie=$cookie_name " . (defined $cookies{$cookie_name} ? 'exists' : '!exists') . "\n";
+    if ( $cookie_name && ref $cookies{$cookie_name} ) {
+        my %session = $cookies{$cookie_name}->value;
+        $session = $session{session};
+        my ($user_session) = $coge->resultset("UserSession")->find( { session => $session } );
+        $user = $user_session->user if $user_session;
+    }
+    unless ($user) {
+        $user = new CoGeX::Result::User;
+        $user->user_name("public");
+        $user->admin(0);
+    }
+    return ($user);
 }
 
 sub gen_cookie {
-	my $self        = shift;
-	my %opts        = @_;
-	my $session     = $opts{session} || 0;
-	my $exp         = $opts{exp} || '+7d'; # issue 48, this field must have lowercase-only
-	my $cookie_name = $opts{cookie_name};
-	my %params      = ( -name => $cookie_name, -path => "/" );
-#	print STDERR "LogUser::gen_cookie session=$session cookie_name=$cookie_name\n";
+    my $self        = shift;
+    my %opts        = @_;
+    my $session     = $opts{session} || 0;
+    my $exp         = $opts{exp} || '+7d'; # issue 48, this field must have lowercase-only
+    my $cookie_name = $opts{cookie_name};
+    my %params      = ( -name => $cookie_name, -path => "/" );
+#   print STDERR "LogUser::gen_cookie session=$session cookie_name=$cookie_name\n";
 
-	$params{-expires} = $exp if $exp;
-	$params{ -values } = { session => $session } if $session;
-	my $c = new CGI::Cookie(%params);
-	return $c;
+    $params{-expires} = $exp if $exp;
+    $params{ -values } = { session => $session } if $session;
+    my $c = new CGI::Cookie(%params);
+    return $c;
 }
 
 1;
