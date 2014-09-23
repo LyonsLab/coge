eaa24690452b5d9df8576353f87855591e10cd54
Evan Briones <erbriones@gmail.com>
2014-09-23 14:23:58 -0700

    add stubs for tasks and sub commands

--- modules/Pipelines/lib/CoGe/Pipelines/SNP/Samtools.pm
+++ modules/Pipelines/lib/CoGe/Pipelines/SNP/Samtools.pm
@@ -13,3 +13,47 @@ our @EXPORT_OK = qw(build);
 sub build {
     carp "Not Implemented";
 }
+
+sub find_snps {
+    my $options = shift;
+
+    my @subcommands =  (
+        pileup($options),
+        bcftools($options),
+    );
+
+    # Pipe commands together
+    my $command = join "|", @subcommands;
+
+    return {
+        command => $command,
+    };
+}
+
+sub filter_snps {
+    my $options = shift;
+
+    my @subcommands =  (
+        bcftools($options),
+        vcfutils($options),
+    );
+
+    # Pipe commands together
+    my $command = join "|", @subcommands;
+
+    return {
+        command => $command,
+    };
+}
+
+sub samtools {
+    return "";
+}
+
+sub bcftools {
+    return "";
+}
+
+sub vcfutils {
+    return "";
+}
