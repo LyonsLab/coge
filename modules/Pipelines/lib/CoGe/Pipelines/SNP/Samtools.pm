d27858ede3c415186738f6ab7848e2cea121d438
Evan Briones <erbriones@gmail.com>
2014-09-23 14:03:04 -0700

    add module for Samtools pipeline

---
+++ modules/Pipelines/lib/CoGe/Pipelines/SNP/Samtools.pm
@@ -0,0 +1,15 @@
+package CoGe::Pipelines::SNP::Samtools;
+
+use v5.14;
+use warnings;
+use strict;
+
+use Carp;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(build);
+
+sub build {
+    carp "Not Implemented";
+}
