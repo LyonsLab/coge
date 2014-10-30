0f6177d4a8e5071cda2dd36f81f057b5ae9d3c5c
Evan Briones <erbriones@gmail.com>
2014-10-30 10:55:26 -0700

    fix filename by excluding query parameter from the url

--- web/LoadAnnotation.pl
+++ web/LoadAnnotation.pl
@@ -244,7 +244,7 @@ sub load_from_ftp {
             }
         }
         else {                                              # file
-            my ($filename) = $url =~ /([^\/]+)\s*$/;
+            my ($filename) = $url =~ /([^\/]+?)(?:\?|$)/;
             push @files, { name => $filename, url => $url };
         }
     }
