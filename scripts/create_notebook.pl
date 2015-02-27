18208c77cf0e77632c754726a01e4e2a9c3d3e80
mbomhoff <mbomhoff@email.arizona.edu>
2015-02-26 17:22:49 -0700

    Use CoGeX to connect to database

--- scripts/create_notebook.pl
+++ scripts/create_notebook.pl
@@ -11,12 +11,13 @@ use Getopt::Long qw(GetOptions);
 use JSON qw(decode_json);
 use URI::Escape::JavaScript qw(unescape);
 
+use CoGe::Accessory::Web qw(get_defaults);
 use CoGe::Accessory::Utils qw(to_pathname);
 use CoGe::Core::Notebook qw(create_notebook);
 use CoGe::Core::Metadata qw(create_annotations);
 use CoGeX;
 
-our ($LOG, $DEBUG, $PAGE, $P, $db, $host, $port, $user, $pass, $config,
+our ($LOG, $DEBUG, $PAGE, $P, $db, $host, $port, $user, $pass, $config_file,
      $name, $description, $version, $type, $userid, $restricted, $result_file,
      $annotations, @ITEMS);
 
@@ -25,8 +26,8 @@ GetOptions(
 
     # General configuration options
     "log=s"             => \$LOG,
-    "config|cfg=s"      => \$config,
-    "result_file=s"      => \$result_file, # results file
+    "config|cfg=s"      => \$config_file,
+    "result_file=s"     => \$result_file, # results file
 
     # Notebook options
     "page=s"            => \$PAGE, # The reference page
@@ -38,29 +39,15 @@ GetOptions(
 );
 
 $| = 1;
+print STDOUT "Starting $0 (pid $$)\n", qx/ps -o args $$/;
 
 # Check required parameters
-die "ERROR: user id not specified use userid" unless $userid;
+die "ERROR: user id not specified, use userid argument" unless $userid;
 die "ERROR: no items found" unless @ARGV;
 die "ERROR: notebook name not specified" unless $name;
 
 # Set default parameters
-$restricted  = '0' unless (defined $restricted && lc($restricted) eq 'true');
-
-sub setup {
-    $P    = CoGe::Accessory::Web::get_defaults($config);
-    $db   = $P->{DBNAME};
-    $host = $P->{DBHOST};
-    $port = $P->{DBPORT};
-    $user = $P->{DBUSER};
-    $pass = $P->{DBPASS};
-
-    # Items being added to the notebook
-    @ITEMS = @ARGV;
-
-    my $connstr = "dbi:mysql:dbname=$db;host=$host;port=$port;";
-    return CoGeX->connect( $connstr, $user, $pass );
-}
+$restricted  = '1' unless (defined $restricted && (lc($restricted) eq 'false' || $restricted eq '0'));
 
 sub parse_files {
     my @logs = @_;
@@ -72,7 +59,6 @@ sub parse_files {
 
         while (<$fh>) {
             if ( $_ =~ /experiment id: (\d+)/i ) {
-                #XXX: Hard code to experiment
                 push @ids, [$1, $node_types->{experiment}];
             }
             elsif ( $_ =~ /log: error/i ) {
@@ -87,17 +73,26 @@ sub parse_files {
 }
 
 sub main {
-    my $coge = setup();
-    die "ERROR: couldn't connect to the database" unless $coge;
-
-    my $user = $coge->resultset("User")->find($userid);
-    die "ERROR: user could not be found for id: $userid" unless $user;
-
+    # Items being added to the notebook
+    @ITEMS = @ARGV;
+    unless (@ITEMS) {
+        print STDOUT "No log paths specified\n";
+        exit(-1);
+    }
     my @ids = parse_files(@ITEMS);
+    
+    # Connect to DB
+    my $conf = get_defaults($config_file);
+    my $db = CoGeX->dbconnect($conf);
+    die "ERROR: couldn't connect to the database" unless $db;
+
+    # Get user
+    my $user = $db->resultset("User")->find($userid);
+    die "ERROR: user could not be found for id: $userid" unless $user;
 
     # Create notebook
     my $notebook = create_notebook(
-        db         => $coge,
+        db         => $db,
         user       => $user,
         name       => $name,
         desc       => $description,
@@ -105,11 +100,14 @@ sub main {
         item_list  => \@ids,
         restricted => $restricted
     );
-    exit(-1) unless $notebook;
+    unless ($notebook) {
+        print STDERR "ERROR: couldn't create notebook\n";
+        exit(-1);
+    }
 
     # Create annotations
     if ($annotations) {
-        CoGe::Core::Metadata::create_annotations(db => $coge, target => $notebook, annotations => $annotations, locked => 1);
+        CoGe::Core::Metadata::create_annotations(db => $db, target => $notebook, annotations => $annotations, locked => 1);
     }
 
     # Save notebook ID in log file
@@ -121,6 +119,7 @@ sub main {
     if ($result_file) {
         my $result_dir = to_pathname($result_file);
         mkpath($result_dir);
+        
         my $rc = CoGe::Accessory::TDS::write(
             $result_file,
             {
