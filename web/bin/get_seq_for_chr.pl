39818c2735e8553c84c287d4458d5df6189e719f
sdavey <sdavey@email.arizona.edu>
2015-04-15 17:24:49 -0700

    more work on chromosome fasta, plus start on chromosome gff

--- web/bin/get_seq_for_chr.pl
+++ web/bin/get_seq_for_chr.pl
@@ -4,6 +4,7 @@ use strict;
 use CoGe::Accessory::Web;
 use CoGeX::Result::Genome qw( get_genome_seq );
 use Data::Dumper;
+use File::Spec::Functions;
 use CGI;
 
 my $q = CGI->new;
@@ -11,24 +12,21 @@ my $gid = $q->param('gid');
 my $chr = $q->param('chr');
 
 # Connect to the database
-my ( $db, $user, $conf ) = CoGe::Accessory::Web->init();
+my $conf = CoGe::Accessory::Web->get_defaults();
 
-my $genome = $db->resultset('Genome')->find($gid);
-my $chromosome = $db->resultset('GenomicSequence')->find({genome_id=>$gid,chromosome=>$chr});
+my $filename = $gid . "_" . $chr . ".faa";
 
-print "Content-Type: application/force-download	\n";
-print "Content-disposition: attachement; filename=chromosome_" . $gid . "_" . $chr . ".faa\n\n";
+print "Content-Type: application/force-download\n";
+print "Content-disposition: attachement; filename=chromosome_";
+print $filename;
+print "\n\n";
 
 print ">chromosome " . $chr . "\n";
 
-# Get sequence from file
-my $seq = $genome->get_genomic_sequence(
-	chr   => $chr,
-	start => 1,
-	stop  => $chromosome->sequence_length
-);
-
-for (my $i=0; $i<length($seq); $i+=70) {
-	print substr($seq, $i, 70);
-	print "\n";
+my $path = catfile($conf->{SECTEMPDIR}, "GenomeInfo/downloads", $gid);
+my $file = catfile($path, $filename);
+open(my $fh, $file);
+while (my $l = <$fh>) {
+  print $l;
 }
+close $fh;
