d03b886112155d52243f41f10110e476aa27f060
Eric Lyons <lyonsden@gmail.com>
2015-04-30 14:00:22 -0700

    added sub clean_locations to scrub bad location data; update call to ->locs to call clean_locations and updated calles from ->locations to ->locs to get scrubbed

--- modules/Database/lib/CoGeX/Result/Feature.pm
+++ modules/Database/lib/CoGeX/Result/Feature.pm
@@ -318,7 +318,7 @@ See Also   :
 
 sub locs {
 	my $self = shift;
-	return $self->locations();
+	return $self->clean_locations(@_);
 }
 
 ################################################ subroutine header begin ##
@@ -388,7 +388,7 @@ sub length {
 	my $self = shift;
 	my $length;
 	
-	map { $length += ( $_->stop - $_->start + 1 ) } $self->locations;
+	map { $length += ( $_->stop - $_->start + 1 ) } $self->locs;
 	
     unless (defined $length) { # mdb added 4/20/15 COGE-610 for cases where there are no locations
         $length = $self->stop - $self->start + 1;
@@ -933,7 +933,7 @@ sub genomic_sequence {
 	my @sequences;
 	my %locs =
 	  map { ( $_->start, $_->stop ) }
-	  $self->locations()
+	  $self->locs()
 	  ; #in case a mistake happened when loading locations and there are multiple ones with the same start
 	my @locs = map { [ $_, $locs{$_} ] } sort { $a <=> $b } keys %locs;
 	( $up, $down ) = ( $down, $up )
@@ -2009,6 +2009,37 @@ sub info {
 	         ', v' . $self->dataset->first_genome->version .
 	         ', ' .  $self->dataset->first_genome->genomic_sequence_type->name . ')';
 	return $info;
+}
+
+################################################ subroutine header begin ##
+
+=head2 clean_locations
+
+ Usage     : $self->clean_locations
+ Purpose   : returns wantarray of location objects.  Checks them for consistency due to some bad loads where locations had bad starts, stops, chromosomes and strands
+
+ Returns   : returns wantarray of location ojects
+ Argument  : none
+ Throws    :
+ Comments  : 
+
+See Also   :
+
+=cut
+
+################################################## subroutine header end ##
+
+sub clean_locations {
+        my $self = shift;
+	my @locs;
+	foreach my $loc ($self->locations(@_)) {
+		next if $loc->strand ne $self->strand;
+		next if $loc->chr ne $self->chr;
+		next if $loc->start < $self->start || $loc->start > $self->stop;
+		next if $loc->stop < $self->start || $loc->stop > $self->stop;
+		push @locs, $loc;
+	} 
+	return wantarray ? @locs : \@locs;
 }
 
 1;
