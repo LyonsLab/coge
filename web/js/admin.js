81b753917b0acbe96f3f339e21ec1cba2f5ba5d8
franka1 <franka1@email.arizona.edu>
2015-06-04 15:59:18 -0700

    Added preliminary version of User Graph in tab 4.

--- web/js/admin.js
+++ web/js/admin.js
@@ -7,6 +7,7 @@ var previous_search = ""; //indicates the previous search term, used to refresh 
 var jobs_updating = true;
 var jobs_init = false;
 var hist_init = false;
+var graph_init = false;
 var hist_updating = true;
 var hist_entries = 0;
 var last_hist_update;
@@ -25,6 +26,9 @@ $(function () {
 			}
 			if(current_tab == 2 && !hist_init) {
 				init_hist_grid();
+			}
+			if(current_tab == 3 && !graph_init) {
+				init_graph();
 			}
 		}
     });
@@ -1224,4 +1228,328 @@ function toggle_star(img) {
 			else { $(img).attr({src:"picts/star-full.png"}); }
 		}
 	});
+}
+
+
+//Tab 4, the User Graph
+var colors = [
+	{ name: 'list',       link: 'NotebookView.pl?nid=',   color: 'Tomato',      show: 1 },
+	{ name: 'genome',     link: 'GenomeInfo.pl?gid=',     color: 'YellowGreen', show: 1 },
+	{ name: 'experiment', link: 'ExperimentView.pl?eid=', color: 'Orchid',      show: 1 },
+	{ name: 'feature',    link: '',                       color: 'orange',      show: 1 },
+	{ name: 'user',       link: '',                       color: 'DeepSkyBlue', show: 1 },
+	{ name: 'group',      link: 'GroupView.pl?ugid=',     color: 'Turquoise',   show: 1 },
+];
+
+var w = Math.max(800, $(window).width()-200),
+	h = Math.max(800, $(window).height()),
+	node,
+	link,
+	root;
+
+/*function init_graph() {
+	
+	/* //Create legend
+	colors.forEach(function(element, index) {
+		var item =
+			$('<div class="link legend selected">'+colors[index].name+'</div>')
+			.css('color', 'white')
+			.css('background-color', colors[index].color)
+			.click(function() {
+				$(this).toggleClass('selected');
+				if ($(this).hasClass('selected')) {
+					$(this).css('color', 'white');
+					$(this).css('background-color', colors[index].color);
+				} else {
+					$(this).css('color', colors[index].color);
+					$(this).css('background-color', '');
+				}
+				colors[index].show = !colors[index].show;
+				update();
+			});
+
+     	$('#legend')
+     		.append(item);
+   	});* /
+
+	// Setup D3
+	force = d3.layout.force()
+		.on("tick", tick)
+		.size([w, h]);
+
+	vis = d3.select("#chart").append("svg:svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	d3.json("?fname=get_all_nodes", function(json) {
+		root = json;
+		console.log(json);
+		/*for(var i = 0; i < root.children.length; i++) {
+			//console.log(root.children[i].children.length);
+			for(var j = 0; j < root.children[i].children.length; j++) {
+				//console.log(j);
+				toggle_children(root.children[i].children[j]);
+			}
+		}* /
+		root.children[0].children.forEach(toggle_children);
+		root.children[1].children.forEach(toggle_children);
+		update();
+	});
+	graph_init = true;
+}
+
+function update() {
+	var nodes = flatten(root),
+	links = d3.layout.tree().links(nodes);
+
+	// Restart the force layout.
+	force
+		.nodes(nodes)
+		.links(links)
+		.start();
+
+	// Update the linksâ€¦
+	link = vis.selectAll("line.link")
+		.data(links, function(d) { return d.target.id; });
+	
+	// Exit any old links.
+	link.exit().remove();
+	
+	// Enter any new links.
+	link.enter().insert("svg:line", ".node")
+		.attr("class", "link")
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	// Update the nodesâ€¦
+	node = vis.selectAll("circle.node")
+		.data(nodes, function(d) { return d.id; })
+		.style("fill", color);
+	
+	//New bit
+	//node.transition()
+	//	.attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+
+	// Enter any new nodes.
+	node.enter()
+       	.append("svg:circle")
+       	.attr("class", "node")
+       	.attr("cx", function(d) { return d.x; })
+       	.attr("cy", function(d) { return d.y; })
+       	.attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
+       	.style("fill", color)
+       	.on("dblclick", click)
+       	.call(force.drag)
+       	.append("svg:title").text(function(d) { return d.info; });
+
+	// Exit any old nodes.
+	node.exit().remove();
+}
+
+function tick() {
+	link.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+ 	node.attr("cx", function(d) { return d.x; })
+   		.attr("cy", function(d) { return d.y; });
+}
+
+// Color nodes
+function color(d) {
+	if (d.type) {
+		return colors[d.type-1].color;
+    }
+	return 'white';
+}
+
+// Toggle children on click.
+function toggle_children(d) {
+	if (d.children) {
+		d._children = d.children;
+		d.children = null;
+	} else {
+      	d.children = d._children;
+      	d._children = null;
+	}
+	//update();
+
+	//var link = colors[d.type-1].link;
+	//if (link) {
+	//	window.open(link + d.name);
+	//}
+}
+
+function click(d) {
+	toggle_children(d);
+	update();
+	console.log(d.type);
+	console.log(d.info);
+}
+
+// Returns a list of all nodes under the root.
+function flatten(root) {
+	var nodes = [], i = 0;
+
+	function recurse(node) {
+		if (node.children) node.children.forEach(recurse);
+		if (!node.id) node.id = ++i;
+
+		var show = 1;
+		if (node.type) {
+			show = colors[node.type-1].show;
+		}
+		if (show) {
+			nodes.push(node);
+		}
+	}
+
+	recurse(root);
+	return nodes;
+	
+	/*var nodes = [], i = 0;
+
+	function recurse(node) {
+		if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+		if (!node.id) node.id = ++i;
+		nodes.push(node);
+		return node.size;
+	}
+
+	root.size = recurse(root);
+	return nodes;* /
+}*/
+function init_graph() {
+	root;
+
+	force = d3.layout.force()
+		.size([w, h])
+		.on("tick", tick);
+
+	svg = d3.select("#chart").append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	link = svg.selectAll(".link"),
+	node = svg.selectAll(".node");
+
+
+	d3.json("?fname=get_all_nodes", function(json) {
+		console.log(json);
+		root = json;
+		//root.children[0].children.forEach(click);
+		click(root.children[0]);
+		root.children[1].children.forEach(click);
+		//click(root.children[1]);
+		update();
+	});
+}
+
+function update() {
+      
+	var nodes = flatten(root),
+		links = d3.layout.tree().links(nodes);
+
+	// Restart the force layout.
+	force
+ 		.nodes(nodes)
+ 		.links(links)
+ 		.start();
+
+	// Update the linksÉ
+	link = link.data(links, function(d) { return d.target.id; });
+
+	// Exit any old links.
+	link.exit().remove();
+
+	// Enter any new links.
+	link.enter().insert("line", ".node")
+		.attr("class", "link")
+		.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	// Update the nodesÉ
+	node = node.data(nodes, function(d) { return d.id; }).style("fill", color)
+		.attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; });
+
+	// Exit any old nodes.
+	node.exit().remove();
+
+	// Enter any new nodes.
+	node.enter().append("circle")
+		.attr("class", "node")
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; })
+		.attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
+		.style("fill", color)
+		.on("dblclick", click)
+		.call(force.drag)
+		.append("svg:title").text(function(d) { return d.info; });
+}
+
+function tick() {
+	link.attr("x1", function(d) { return d.source.x; })
+		.attr("y1", function(d) { return d.source.y; })
+		.attr("x2", function(d) { return d.target.x; })
+		.attr("y2", function(d) { return d.target.y; });
+
+	node.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+}
+
+//Color leaf nodes orange, and packages white or blue.
+function color(d) {
+	//return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+	if (d.type) {
+		return colors[d.type-1].color;
+    }
+	return 'white';
+}
+
+//Toggle children on click.
+function toggle_children(d) {
+	if (d.children) {
+		d._children = d.children;
+		d.children = null;
+	} else {
+		d.children = d._children;
+		d._children = null;
+	}
+	
+	if (d._size) {
+		var temp = d._size;
+		d._size = d.size;
+		d.size = temp;
+	} else {
+		d._size = d.size;
+		d.size = 2025;
+	}
+	console.log(d.size);
+}
+
+function click(d) {
+	//if (!d3.event.defaultPrevented) {
+		toggle_children(d);
+		update();
+		//console.log(root);
+	//}
+}
+
+//Returns a list of all nodes under the root.
+function flatten(root) {
+	var nodes = [], i = 0;
+
+	function recurse(node) {
+		if (node.children) node.children.forEach(recurse);
+		if (!node.id) node.id = ++i;
+		nodes.push(node);
+	}
+
+	recurse(root);
+	return nodes;
 }
