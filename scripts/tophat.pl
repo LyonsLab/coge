e8f5ba5db1d0845b013068ed3fcc79283db096f7
mbomhoff <mbomhoff@email.arizona.edu>
2015-06-25 15:55:58 -0700

    Move paired-end detection into Utils

--- scripts/tophat.pl
+++ scripts/tophat.pl
@@ -9,17 +9,16 @@
 use strict;
 use warnings;
 
-use CoGe::Accessory::Utils qw(execute);
+use CoGe::Accessory::Utils qw(execute detect_paired_end);
 
 my $read_type = shift;  # 'single' or 'paired'
 my $cmd_args = shift;   # tophat executable and all command line arguments except for file lists
 my @files = @ARGV;      # fastq files
 
 if ($read_type eq 'paired') {
-    my @m1 = grep { $_ =~ /\_R1\./ } @files;
-    my @m2 = grep { $_ =~ /\_R2\./ } @files;
-    die "error: invalid paired-end files" unless (@m1 and @m2);
-    $cmd_args .= join(',', sort @m1) . ' ' . join(',', sort @m2);
+    my ($m1, $m2) = detect_paired_end(\@files);
+    die "error: invalid paired-end files: m1: @$m1 -- m2: @$m2" unless (@$m1 and @$m2);
+    $cmd_args .= join(',', sort @$m1) . ' ' . join(',', sort @$m2);
 }
 else {
     $cmd_args .= join(',', @files);
