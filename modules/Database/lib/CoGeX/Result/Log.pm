c2a18cd6a3173d76711ca5f5f42b0a3bb349c17f
mbomhoff <mbomhoff@email.arizona.edu>
2015-07-21 15:29:39 -0700

    Non-functional change

--- modules/Database/lib/CoGeX/Result/Log.pm
+++ modules/Database/lib/CoGeX/Result/Log.pm
@@ -9,14 +9,23 @@ use base 'DBIx::Class::Core';
 
 My::Schema::Result::Log
 
+=head1 AUTHOR
+
+Matt Bomhoff
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=head1 SEE ALSO
+
 =cut
 
 __PACKAGE__->table("log");
-
-=head1 ACCESSORS
-
-=cut
-
 __PACKAGE__->add_columns(
     "log_id",
     {
@@ -109,19 +118,3 @@ sub is_important {
 }
 
 1;
-
-=head1 AUTHOR
-
-Matt Bomhoff
-
-=head1 COPYRIGHT
-
-This program is free software; you can redistribute
-it and/or modify it under the same terms as Perl itself.
-
-The full text of the license can be found in the
-LICENSE file included with this module.
-
-=head1 SEE ALSO
-
-=cut
