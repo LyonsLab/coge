f1d8667972aa742645e277ea7e492d33748ee075
franka1 <franka1@email.arizona.edu>
2015-07-24 15:26:24 -0700

    Resolved a merge conflict in Admin.pl

--- web/Admin.pl
+++ web/Admin.pl
@@ -33,7 +33,7 @@ $JEX =
 
 $MAX_SEARCH_RESULTS = 400;
 
-my $node_types = CoGeX::node_types();
+our $node_types = CoGeX::node_types();
 my $filename = '/home/franka1/repos/coge/web/admin_error.log';
 open(my $fh, '>', $filename); #or die "Could not open file '$filename' $!";
 
@@ -1028,10 +1028,11 @@ sub modify_item {
     
     my $log_message;
     if ($mod eq "delete") {
-        $log_message = ( $item->deleted ? 'undelete' : 'delete' );
+        $log_message = ( $item->deleted ? 'undeleted' : 'deleted' );
         $item->deleted( !$item->deleted );    # do undelete if already deleted
-    } elsif ($mod eq "restrict") {
-    	$log_message = ( $item->restricted ? 'unrestrict' : 'restrict' );
+    } 
+    elsif ($mod eq "restrict") {
+    	$log_message = ( $item->restricted ? 'unrestricted' : 'restricted' );
     	$item->restricted( !$item->restricted );    # do undelete if already deleted
     }
     $item->update;
@@ -1041,7 +1042,7 @@ sub modify_item {
         db          => $coge,
         user_id     => $USER->id,
         page        => "Admin",
-        description => "$log_message $type id $id",
+        description => "$log_message $type " . $item->info_html,
         parent_id   => $id,
         parent_type => $item_type
     );
@@ -1116,7 +1117,7 @@ sub add_users_to_group {
                 db          => $coge,
                 user_id     => $USER->id,
                 page        => "Admin",
-                description => 'add user id' . $user->id . ' to group id' . $target_id,
+                description => 'added user ' . $user->info . ' to group ' . $target_group->info_html,
                 parent_id   => $target_id,
                 parent_type => 6 #FIXME magic number
             );
@@ -1169,7 +1170,7 @@ sub remove_user_from_group {
             db          => $coge,
             user_id     => $USER->id,
             page        => "Admin",
-            description => 'remove user id' . $user_id . ' from group id' . $target_id,
+            description => 'removed user ' . $user->info . ' from group ' . $target_group->info_html,
             parent_id   => $target_id,
             parent_type => 6
         );
