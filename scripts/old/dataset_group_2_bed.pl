66984ad63838b2b399dcc50ec2f91f1158d9ee61
sdavey <sdavey@email.arizona.edu>
2015-08-05 10:47:53 -0700

    moved another unused script to old

--- scripts/old/dataset_group_2_bed.pl
+++ scripts/old/dataset_group_2_bed.pl
@@ -2,8 +2,15 @@
 
 use strict;
 use CoGeX;
+use Data::Dumper;
+use CoGe::Accessory::Web;
+use Getopt::Long;
 
-my $dsgid = shift;
+use vars qw($dsgid $conffile);
+GetOptions(
+	   "dsgid=i"=>\$dsgid,
+	   "config_file|cf=s"=>\$conffile,
+	  );
 
 unless ( $dsgid )
   {
@@ -14,14 +21,21 @@ Will generate a dump of all features for a dataset_group in bed format
     exit;
   }
 
-my $coge = CoGeX->dbconnect();
-my $dsg = $coge->resultset('DatasetGroup')->find($dsgid);
+my $P = CoGe::Accessory::Web::get_defaults($conffile);
+my $DBNAME = $P->{DBNAME};
+my $DBHOST = $P->{DBHOST};
+my $DBPORT = $P->{DBPORT};
+my $DBUSER = $P->{DBUSER};
+my $DBPASS = $P->{DBPASS};
+my $connstr = "dbi:mysql:dbname=".$DBNAME.";host=".$DBHOST.";port=".$DBPORT;
+my $coge = CoGeX->connect($connstr, $DBUSER, $DBPASS );
+my $dsg = $coge->resultset('Genome')->find($dsgid);
 foreach my $ds ($dsg->datasets)
   {
     my $rs = $coge->resultset('Feature');
     $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
-    foreach my $ref ($rs->search({dataset_id => $ds->id}))
+    foreach my $ref ($rs->search({dataset_id => $ds->id, feature_type_id=>[3,5,8]}))
       {
-	print join ("\t", $ref->{chromosome}, $ref->{start}, $ref->{stop}, $ref->{feature_id}),"\n";
+	print join ("\t", $ref->chromosome, $ref->start, $ref->stop, $ref->feature_id),"\n";
       }
   }
