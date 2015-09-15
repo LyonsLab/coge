a02ee37d5a95c302fc6699e5d74c2b689df09309
mbomhoff <mbomhoff@email.arizona.edu>
2015-09-15 11:50:58 -0700

    Fix syntax error

--- web/SynMap.pl
+++ web/SynMap.pl
@@ -178,10 +178,8 @@ $PYTHON        = $config->{PYTHON};                         #this was for python
 $PYTHON26      = $config->{PYTHON};
 $DAG_TOOL      = 'nice ' . $SCRIPTDIR . 'dag_tools.py';
 $BLAST2BED     = $SCRIPTDIR . 'blast2bed.pl';
-$GENE_ORDER    = $SCRIPTDIR . 'gene_order.py";
-$TANDEM_FINDER = $config->{TANDEM_FINDER}
-  . " -d 5 -s -r"
-  ; #-d option is the distance (in genes) between dups -- not sure if the -s and -r options are needed -- they create dups files based on the input file name
+$GENE_ORDER    = $SCRIPTDIR . 'gene_order.py';
+$TANDEM_FINDER = $config->{TANDEM_FINDER} . " -d 5 -s -r"; #-d option is the distance (in genes) between dups -- not sure if the -s and -r options are needed -- they create dups files based on the input file name
 
 #$RUN_DAGHAINER = $DIR."/bin/dagchainer/DAGCHAINER/run_DAG_chainer.pl -E 0.05 -s";
 $RUN_DAGCHAINER = 'nice ' . $PYTHON26 . " " . $config->{DAGCHAINER};
