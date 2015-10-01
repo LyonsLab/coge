c858555dc4bae88a0162a1bd6c59f8c117e4e6e1
mbomhoff <mbomhoff@email.arizona.edu>
2015-09-30 17:39:39 -0700

    Non-functional change

--- modules/Core/lib/CoGe/Core/Storage.pm
+++ modules/Core/lib/CoGe/Core/Storage.pm
@@ -632,7 +632,7 @@ sub get_workflow_paths {
         my @wdir = grep { -d "$staging_dir/$_/$workflow_id" } read_dir($staging_dir);
         #print STDERR Dumper "wdir:\n", \@wdir, "\n";
         if (@wdir != 1) {
-            print STDERR "Storage::get_workflow_paths ERROR: ambiguous user directory\n";
+            print STDERR "Storage::get_workflow_paths WARNING: ambiguous user directory\n";
             return;
         }
         $user_name = $wdir[0];
