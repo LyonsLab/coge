f85d30374bf19f9eb96b6e25656e5ea4da42132a
sdavey <sdavey@email.arizona.edu>
2015-10-14 12:19:13 -0700

    renamed dotplots_dots to dotplot_dots

--- modules/Pipelines/lib/CoGe/Builder/Tools/DotplotDots.pm
+++ modules/Pipelines/lib/CoGe/Builder/Tools/DotplotDots.pm
@@ -1,6 +1,8 @@
-package CoGe::Builder::Tools::DotplotsDots;
+package CoGe::Builder::Tools::DotplotDots;
 
 use Moose;
+
+use CoGe::Core::Storage qw( get_workflow_paths );
 
 sub build {
 	my $self = shift;
@@ -13,7 +15,7 @@ sub build {
 	my $ksfile = $self->params->{ksfile};
 	return unless $ksfile;
 
-	$self->workflow( $self->jex->create_workflow( name => "DotplotsDots", init => 1 ) );
+	$self->workflow( $self->jex->create_workflow( name => "DotplotDots", init => 1 ) );
 	return unless ( $self->workflow && $self->workflow->id );
 	my ( $staging_dir, $result_dir ) = get_workflow_paths( $self->user->name, $self->workflow->id );
 	$self->workflow->logfile( catfile( $result_dir, "debug.log" ) );
@@ -24,9 +26,9 @@ sub build {
 
 	$self->workflow->add_job(
 		{
-			cmd         => catfile($SCRIPTDIR, 'dotplots_dots.py') . ' ' . $ksfile,
+			cmd         => catfile($SCRIPTDIR, 'dotplot_dots.py') . ' ' . $ksfile,
 			outputs     => [catfile($DIAGSDIR, $dir1, $dir2, 'log.json')],
-			description => "running dotplots_dots...",
+			description => "running dotplot_dots...",
 		}
 	);
 
