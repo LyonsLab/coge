93ed09df4c9fcf078570f0fa6b4c3688ad1ea60d
mbomhoff <mbomhoff@email.arizona.edu>
2015-11-05 14:33:07 -0700

    Change mpileup to depth, COGE-676

--- scripts/pileup_to_bed.pl
+++ scripts/pileup_to_bed.pl
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------
 # Purpose:  Convert a SAMTools pileup depth to BED format.
 # Usage:    samtools depth file.bam | pileup_to_bed.pl
-#           Note: used to accept output from "samtools mpileup"
+#           Note: used to accept output from "samtools mpileup", see COGE-676
 # Author:   mdb
 # Created:  2/24/14
 #-------------------------------------------------------------------------------
@@ -26,7 +26,8 @@ my ($start, $stop);
 my ($prevChr, $prevDepth);
 while (<>) {
     chomp;
-    my ($chr, $pos, undef, $depth) = split("\t");
+    #my ($chr, $pos, undef, $depth) = split("\t"); # mdb removed 11/4/15 COGE-676
+    my ($chr, $pos, $depth) = split("\t"); # mdb added 11/4/15 COGE-676
 
     if (!defined $start || ($pos-$stop > 1) || $depth != $prevDepth || $chr ne $prevChr) {
         # Print interval
