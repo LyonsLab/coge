d31087a5a7acc8a848c0b0588cf9e2b1753ad5af
sdavey <sdavey@email.arizona.edu>
2015-11-17 15:42:30 -0700

    generalized table for variable columns, same for export

---
+++ modules/Algos/PopGen/lib/CoGe/Algos/PopGen/Results.pm
@@ -0,0 +1,94 @@
+#-------------------------------------------------------------------------------
+# Purpose:	Functions for dealing with results files
+# Author:	Sean Davey
+# Created:	11/17/2015
+#-------------------------------------------------------------------------------
+package CoGe::Algos::PopGen::Results;
+
+use warnings;
+use strict;
+use Data::Dumper;
+use File::Spec::Functions qw( catfile );
+use JSON::XS;
+use base 'Exporter';
+
+our @EXPORT = qw( export get_data );
+
+sub export {
+	my $file = shift;
+	my $type = shift;
+	my @columns = map {substr $_, 3} split ',', shift;
+
+	print "Content-Disposition: attachment; filename=PopGen.txt\n\n";
+	my $done = 0;
+	my $in_type = 0;
+	open my $fh, '<', $file;
+	while (my $row = <$fh>) {
+		chomp $row;
+		if (substr($row, 0, 1) eq '#') {
+			return if $in_type;
+			my @tokens = split '\t', $row;
+			$in_type = 1 if (substr($tokens[0], 1) eq $type);
+			$row = <$fh>;
+			chomp $row;
+		}
+		if ($in_type) {
+			my @tokens = split '\t', $row;
+			my $line;
+			foreach (@columns) {
+				$line .= "\t" if $line;
+				$line .= $tokens[$_] if ($tokens[$_]);
+			}
+			print $line . "\n";
+		}
+	}
+}
+
+sub get_data {
+	my $file = shift;
+	my $columns = {};
+	my $data;
+	my $type = '';
+	my @types;
+	my $set;
+	open my $fh, '<', $file;
+	while (my $row = <$fh>) {
+		chomp $row;
+		my @tokens = split '\t', $row;
+		if (substr($tokens[0], 0, 1) eq '#') {
+			if ($type) {
+				$data .= ',' if $data;
+				$data .= '\'';
+				$data .= $type;
+				$data .= '\':[';
+				$data .= $set;
+				$data .= ']';
+				$set = '';
+			}
+			$type = substr $tokens[0], 1;
+			push @types, $type;
+			shift @tokens;
+			$columns->{$type} = [@tokens];
+			$row = <$fh>;
+			chomp $row;
+			@tokens = split '\t', $row;
+		}
+		$set .= ',' if $set;
+		$set .= encode_json(\@tokens);
+	}
+	$data .= ',' if $data;
+	$data .= '\'';
+	$data .= $type;
+	$data .= '\':[';
+	$data .= $set;
+	$data .= ']';
+	my $options;
+	for (@types) {
+		$options .= '<option>';
+		$options .= $_;
+		$options .= '</option>';
+	}
+	return encode_json($columns), '{' . $data . '}', $options;
+}
+
+1;
--- web/PopGen.pl
+++ web/PopGen.pl
@@ -1,12 +1,13 @@
 #! /usr/bin/perl -w
 use strict;
 use CoGe::Accessory::Web;
+use CoGe::Algos::PopGen::Results qw( export get_data );
 use CGI;
 use Data::Dumper;
 use HTML::Template;
 use JSON::XS;
 use POSIX;
-use File::Spec::Functions qw(catfile);
+use File::Spec::Functions qw( catfile );
 no warnings 'redefine';
 
 use vars qw($CONF $USER $FORM $ACCN $FID $DB $PAGE_NAME $PAGE_TITLE $LINK);
@@ -20,29 +21,8 @@ $FORM = new CGI;
 
 my $export = $FORM->Vars->{'export'};
 if ($export) {
-	print "Content-Disposition: attachment; filename=PopGen.txt\n\n";
-	my @columns = (index($export, 'name') != -1, index($export, 'pi') != -1, index($export, 'theta') != -1, index($export, 'td') != -1);
-	my $type = $FORM->Vars->{'type'};
-	my $done = 0;
-	open my $fh, '<', '/home/sdavey/test.txt';
-	while (my $row = <$fh>) {
-		chomp $row;
-		my @tokens = split '\t', $row;
-		if ($type eq $tokens[0]) {
-			my $line;
-			for (my $i=0; $i<4; $i++) {
-				if ($columns[$i]) {
-					$line .= "\t" if $line;
-					$line .= $tokens[$i];
-				}
-			}
-			print $line . "\n";
-			$done = 1;
-		}
-		elsif ($done) {
-			return;
-		}
-	}
+	export catfile($ENV{COGE_HOME}, 'test.txt'), $FORM->Vars->{'type'}, $export;
+	return;
 }
 
 ( $DB, $USER, $CONF, $LINK ) = CoGe::Accessory::Web->init(
@@ -53,51 +33,6 @@ if ($export) {
 my %FUNCTION = (
 );
 CoGe::Accessory::Web->dispatch( $FORM, \%FUNCTION, \&gen_html );
-
-sub gen_data {
-	my $html;
-	my $type = '';
-	my @types;
-	my $set;
-	print STDERR catfile($ENV{COGE_HOME}, 'test.txt'), "\n";
-	open my $fh, '<', catfile($ENV{COGE_HOME}, 'test.txt');
-	while (my $row = <$fh>) {
-		chomp $row;
-		my @tokens = split '\t', $row;
-		if ($type ne $tokens[0]) {
-			if ($type) {
-				$html .= ',' if $html;
-				$html .= '\'';
-				$html .= $type;
-				$html .= '\':[';
-				$html .= $set;
-				$html .= ']';
-				$set = '';
-			}
-			$type = $tokens[0];
-			push @types, $type;
-		}
-		$set .= ',' if $set;
-		shift @tokens;
-		$set .= encode_json(\@tokens);
-	}
-	$html .= ',' if $html;
-	$html .= '\'';
-	$html .= $type;
-	$html .= '\':[';
-	$html .= $set;
-	$html .= ']};var dt;$(document).ready(function(){dt=$(\'#main\').DataTable({data:data[\'';
-	$html .= $types[1];
-	$html .= '\'],lengthChange:false,order:[],ordering:false,pageLength:500,searching:false});});';
-	my $select = '<select id="types" onchange="var t=this.options[this.selectedIndex].text;dt.clear().rows.add(data[t]).draw();">';
-	for (@types) {
-		$select .= '<option>';
-		$select .= $_;
-		$select .= '</option>';
-	}
-	$select .= '</select>';
-	return 'var data={' . $html, $select;
-}
 
 sub gen_html {
     my $template = HTML::Template->new( filename => $CONF->{TMPLDIR} . 'generic_page.tmpl' );
@@ -121,10 +56,11 @@ sub gen_html {
 sub gen_body {
     my $template = HTML::Template->new( filename => $CONF->{TMPLDIR} . "$PAGE_TITLE.tmpl" );
     
-    my ($script, $select) = gen_data();
+    my ($columns, $data, $options) = get_data(catfile($ENV{COGE_HOME}, 'test.txt'));
     $template->param(
-        SCRIPT => $script,
-        SELECT => $select
+    	columns => $columns,
+    	data => $data,
+        options => $options
     );
 
     return $template->output;
