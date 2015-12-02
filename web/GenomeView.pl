f403efabb7599069d4702a9e30f5def96dede87c
sdavey <sdavey@email.arizona.edu>
2015-12-02 13:48:27 -0700

    added kludge to remove DOCTYPE on GenomeView page chmod -x on some jquery and jquery-ui css and js files

--- web/GenomeView.pl
+++ web/GenomeView.pl
@@ -42,7 +42,8 @@ sub gen_html {
             HELP       => 'GenomeView',
             WIKI_URL   => $CONF->{WIKI_URL} || '',
             ADMIN_ONLY => $USER->is_admin,
-            CAS_URL    => $CONF->{CAS_URL} || ''
+            CAS_URL    => $CONF->{CAS_URL} || '',
+            NO_DOCTYPE => 1
         );
         $template->param( LOGON => 1 ) unless ($USER->user_name eq 'public');
     }
