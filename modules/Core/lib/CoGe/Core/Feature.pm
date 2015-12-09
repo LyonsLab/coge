06bd33230f5e42ea6bf6ca7f6003a82a4a886f28
mbomhoff <mbomhoff@email.arizona.edu>
2015-12-09 14:27:13 -0700

    Use common get_feature function

--- modules/Core/lib/CoGe/Core/Feature.pm
+++ modules/Core/lib/CoGe/Core/Feature.pm
@@ -11,7 +11,7 @@ BEGIN {
 
     $VERSION = 0.1;
     @ISA = qw( Exporter );
-    @EXPORT = qw( search_features );
+    @EXPORT = qw( search_features get_feature );
 }
 
 sub search_features {
@@ -36,38 +36,68 @@ sub search_features {
     my %features;
     foreach my $fn (@results) {
         my $fid = $fn->feature_id;
-        my $feat = $fn->feature;
-        my $genome = $feat->dataset->first_genome;
-        next unless $genome;
-        
-        $features{$fid} = {
-            id => $fid,
-            type => $feat->type->name,
-            start => $feat->start,
-            stop => $feat->stop,
-            strand => $feat->strand,
-            chromosome => $feat->chromosome,
-            genome => {
-                id => $genome->id,
-                version => $genome->version,
-                
-            }
-        };
-        #$features{$fid}{primary_name} = $feat->primary_name->name if $feat->primary_name;
-        
-        my $feat_names = $feat->names;
-        my $num_names = scalar @$feat_names;
-        if ($num_names > 10) {
-            push @{$features{$fid}{warnings}}, "Names truncated, too many names to list ($num_names)";
-            #$feat_names = splice($feat_names, 0, 10);
-        }
-        else {
-            $features{$fid}{names} = $feat_names;
-        }
+        $features{$fid} = get_feature( feature => $fn->feature, fast => 1 );
     }
-    print STDERR Dumper \%features, "\n";
 
     return wantarray ? values %features : [ values %features ];
+}
+
+sub get_feature {
+    my %opts = @_;
+    my $db = $opts{db};
+    my $fid = $opts{fid};
+    my $feature = $opts{feature};
+    my $fast = $opts{fast};
+    
+    unless ($feature) {
+        $feature = $db->resultset("Feature")->find($fid);
+        return unless (defined $feature);
+    }
+    
+    my $genome = $feature->dataset->first_genome;
+    unless ($genome) {
+        return { error => { Error => "No genome for this feature" } };
+    }
+    
+    my %data = (
+        id => int($feature->id),
+        type => $feature->type->name,
+        start => $feature->start,
+        stop => $feature->stop,
+        strand => $feature->strand,
+        chromosome => $feature->chromosome,
+        genome => {
+            id => $genome->id,
+            version => $genome->version,
+            organism => $genome->organism->name
+        }
+    );
+    
+    my @names = $feature->names;
+    my $num_names = scalar @names;
+    if ($num_names > 10) {
+        push @{$data{warning}}, "Names truncated, too many names to list ($num_names)";
+        #@names = splice(@names, 0, 10);
+    }
+    else {
+        $data{names} = \@names;
+    }
+    $data{primary_name} = $feature->primary_name->name if $feature->primary_name;
+    
+    if (!$fast) {
+        $data{sequence} = $feature->genomic_sequence;
+        my @annos;
+        foreach ($feature->annos) {
+            my %anno;
+            $anno{text} = $_->annotation;
+            $anno{type} = $_->type->name;
+            $anno{category} = $_->type->group->name if ($_->type->group);
+            push @annos, \%anno;
+        }
+        $data{annotations} = \@annos;
+    }
+    
+    return \%data;
 }
 
 1;
