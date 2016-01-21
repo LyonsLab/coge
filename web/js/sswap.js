c40811483568fd02c92d2143f6bbbcd3f83c025b
mbomhoff <mbomhoff@email.arizona.edu>
2016-01-21 16:45:13 -0700

    Update all iPlant references to CyVerse, COGE-699

--- web/js/sswap.js
+++ web/js/sswap.js
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2010, iPlant Collaborative, University of Arizona, Cold Spring Harbor Laboratories, University of Texas at Austin
+* Copyright (c) 2010, CyVerse Collaborative, University of Arizona, Cold Spring Harbor Laboratories, University of Texas at Austin
 * This software is licensed under the CC-GNU GPL version 2.0 or later.
 * License: http://creativecommons.org/licenses/GPL/2.0/
 */
