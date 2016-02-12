5eb9972e18fb04d718da18b7ee0a3dd102286029
mbomhoff <mbomhoff@email.arizona.edu>
2016-02-12 15:03:59 -0700

    Fix gc wobble content

--- modules/Services/lib/CoGe/Services/Data/JBrowse/Annotation.pm
+++ modules/Services/lib/CoGe/Services/Data/JBrowse/Annotation.pm
@@ -2,14 +2,14 @@ package CoGe::Services::Data::JBrowse::Annotation;
 
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::JSON;
-use CoGe::Core::Storage qw( get_genome_seq );
+use CoGe::Core::Storage qw( get_genome_seq reverse_complement );
 use CoGeDBI qw(get_features_by_range);
 use URI::Escape qw(uri_unescape);
 use List::Util qw( min max );
 
 sub stats_global {
     my $self = shift;
-    print STDERR "JBrowse::Annotation::stats_global\n";
+    #print STDERR "JBrowse::Annotation::stats_global\n";
     $self->render(json => { 
         "featureDensity" => 0.02,
         "scoreMin" => 0,
@@ -23,12 +23,14 @@ sub features {
     my $gid  = $self->stash('gid');
     my $chr  = $self->stash('chr');
     $chr = uri_unescape($chr) if (defined $chr);
-    my $start = $self->param('start');
-    my $end   = $self->param('end');
-    my $scale = $self->param('scale');
+    my $start        = $self->param('start');
+    my $end          = $self->param('end');
+    my $feat_type    = $self->param('feat_type');
+    my $show_wobble  = $self->param('showWobble');
+    my $scale        = $self->param('scale');
     my $basesPerSpan = $self->param('basesPerSpan');
-    my $len   = $end - $start;
-    print STDERR "JBrowse::Annotation::features gid=$gid chr=$chr start=$start end=$end\n";
+    my $len = $end - $start;
+    #print STDERR "JBrowse::Annotation::features gid=$gid chr=$chr start=$start end=$end\n";
 
     # Check params
     my $null_response = $self->render(json => { "features" => [] });
@@ -64,15 +66,14 @@ sub features {
 
     my @results;
     if ($feat_type) { # feature type was specified
-        if results:
-            response_body["features"].append({"subfeatures" : []})
         my $i = 0;
         my $lastID = 0;
         my $lastEnd = 0;
         my $lastStart = 0;
+        my ($lastName, $lastStrand, $lastType);
         my $result;
         foreach my $f (@$features) {
-            if ($f->{feature_id} != lastID and lastID != 0) {
+            if ($f->{feature_id} != $lastID && $lastID != 0) {
                 $result = {
                     "chr" => $f->{chromosome},
                     "start" => $lastStart,
@@ -81,11 +82,11 @@ sub features {
                     "name" => $lastName,
                     "strand" => $lastStrand,
                     "type" => $lastType,
-                    "subfeatures" : []
+                    "subfeatures" => []
                 };
                 $i += 1;
             }
-            elsif (lastID == 0) {
+            elsif ($lastID == 0) {
                 $result = {
                     "chr" => $f->{chromosome},
                     "start" => $f->{start},
@@ -96,92 +97,148 @@ sub features {
                     "type" => $f->{type}
                 }
             }
-        }
 
-        if ($f->{strand} == $f->{locstrand} && $f->{locstart} >= $f->{start} && $f->{locstart} <= $f->{stop} && $f->{locstop} >= $f->{start} && $f->{locstop} <= $f->{stop}) {
-            push @{$result->{subfeatures}}, {
-                "chr" => $f->{chromosome}, # mdb added 11/18/13 issue 240 - add chr to FeatAnno.pl onclick url
-                "start" => $f->{locstart},
-                "end" => $f->{locstop},
-                "strand" => $f->{locstrand},
-                "type" => $f->{type},
-                "name" => $f->{name}
-            };
+            if ($f->{strand} == $f->{locstrand} && 
+                $f->{locstart} >= $f->{start} && $f->{locstart} <= $f->{stop} && 
+                $f->{locstop} >= $f->{start} && $f->{locstop} <= $f->{stop}) 
+            {
+                push @{$result->{subfeatures}}, {
+                    "chr" => $f->{chromosome}, # mdb added 11/18/13 issue 240 - add chr to FeatAnno.pl onclick url
+                    "start" => $f->{locstart},
+                    "end" => $f->{locstop},
+                    "strand" => $f->{locstrand},
+                    "type" => $f->{type},
+                    "name" => $f->{name}
+                };
+            }
+    
+            push @results, $result;
+    
+            $lastStrand = $f->{locstrand};
+            $lastType = $f->{type};
+            $lastName = $f->{name};
+            $lastStart = $f->{start};
+            $lastEnd = $f->{stop};
+            $lastID = $f->{feature_id};
         }
-
-        $lastStrand = $f->{locstrand};
-        $lastType = $f->{type};
-        $lastName = $f->{name};
-        $lastStart = $f->{start};
-        $lastEnd = $f->{stop};
-        $lastID = $f->{feature_id};
     }
-    else: # process all feature types
+    else { # process all feature types
         # Filter out redundant results due to multiple feature names
-        feats = {}
-        min_start = None
-        max_stop = None
-        for row in results:
-    if row[2] == row[12] and row[10] == row[11] and row[0] >= row[6] and row[0] <= row[7] and row[1] >= row[6] and row[1] <= row[7]:
+        my $feats = {};
+        my $min_start;
+        my $max_stop;
+        foreach my $f (@$features) {
+            if ($f->{locstrand} == $f->{strand} && 
+                $f->{locstart} >= $f->{start} && $f->{locstart} <= $f->{stop} && 
+                $f->{locstop} >= $f->{start} && $f->{locstop} <= $f->{stop}) 
+            {
                 # Find bounds for sequence retrieval later (show_wobble == 1)
-                if min_start is None or min_start > row[0]:
-                    min_start = row[0]
-                if max_stop is None or max_stop < row[1]:
-                    max_stop = row[1]
+                if (!defined($min_start) || $min_start > $f->{locstart}) {
+                    $min_start = $f->{locstart};
+                }
+                if (!defined($max_stop) || $max_stop < $f->{locstop}) {
+                    $max_stop = $f->{locstop};
+                }
             
                 # Hash unique features
-                location_id = row[5]
-                try:
-                    feat = feats[location_id]
-                    if row[9] == 1: # is feature name the primary name?
-                        feats[location_id] = row
-                except KeyError:
-                    feats[location_id] = row
+                my $location_id = $f->{location_id};
+                if (defined $feats->{$location_id}) {
+                    my $feat = $feats->{$location_id};
+                    if ($f->{primary_name} == 1) { # is feature name the primary name?
+                        $feats->{$location_id} = $f;
+                    }
+                }
+                else {
+                    $feats->{$location_id} = $f;
+                }
+            }
+        }
 
         # Calculate wobble GC for CDS features
-        wcount = {}
-        wtotal = {}
-        if show_wobble:
-            seq = '';
-            rcseq = '';
-            for row in feats.values():
-                if row[3] == 'CDS' and is_overlapping(int(row[0]), int(row[1]), int(start), int(end)):
-                    if not seq:
+        my (%wcount, %wtotal);
+        if ($show_wobble) {
+            my $seq = '';
+            my $rcseq = '';
+            foreach my $f (values %$feats) {
+                if ($f->{type} eq 'CDS' && _is_overlapping($f->{locstart}, $f->{locstop}, $start, $end)) {
+                    if (!$seq) {
                         # Get chromosome subsequence using interbase coordinates
-                        cookie = get_cookie(environ)
-                        seq = fetch_sequence(genome_id, chr_id, int(min_start), int(max_stop), cookie)
-                        
-                    if (int(row[2]) == 1): # plus strand
-                        count, total = calc_wobble(seq, int(row[0])-int(min_start), int(row[1])-int(min_start))
-                    else: # minus strand
+                        $seq = get_genome_seq(
+                            gid   => $gid,
+                            chr   => $chr,
+                            start => $min_start,
+                            stop  => $max_stop
+                        );
+                    }
+                    
+                    my ($count, $total);
+                    if ($f->{locstrand} eq '1') { # plus strand
+                        ($count, $total) = _calc_wobble($seq, $f->{locstart} - $min_start, $f->{locstop} - $min_start);
+                    }
+                    else { # minus strand
                         # Reverse complement the seq and coords
-                        if not rcseq:
-                            rcseq = reverse_complement(seq)
-                        rcstart = int(max_stop)-int(row[1])
-                        rcstop  = int(max_stop)-int(row[0])
-                        count, total = calc_wobble(rcseq, rcstart, rcstop)
-                        
-                    id = row[8]
-                    if id in wcount:
-                        wcount[id] += count
-                        wtotal[id] += total
-                    else:
-                        wcount[id] = count
-                        wtotal[id] = total
+                        if (!$rcseq) {
+                            $rcseq = reverse_complement($seq);
+                        }
+                        my $rcstart = $max_stop - $f->{locstop};
+                        my $rcstop  = $max_stop - $f->{locstart};
+                        ($count, $total) = _calc_wobble($rcseq, $rcstart, $rcstop);
+                    }
+                    
+                    my $id = $f->{feature_id};
+                    $wcount{$id} += $count;
+                    $wtotal{$id} += $total;
+                }
+            }
+        }
         
         # Build response
-        for row in feats.values():
-            response_body["features"].append({
-                "chr"    : chr_id, # mdb added 11/18/13 issue 240 - add chr to FeatAnno.pl onclick url
-                "start"  : row[0],
-                "end"    : row[1],
-                "strand" : row[2],
-                "type"   : row[3],
-                "name"   : row[4] if row[3] == 'gene' else None,
-                "wobble" : round(wcount[row[8]] / float(wtotal[row[8]]), 3) if (show_wobble and row[8] in wcount) else 0
-            })
+        foreach my $f (values %$feats) {
+            my $fid = $f->{feature_id};
+            my $wobble_gc = ($show_wobble && $wtotal{$fid}) ? ($wcount{$fid} / $wtotal{$fid}) : 0;
+            push @results, {
+                "chr"    => $chr, # mdb added 11/18/13 issue 240 - add chr to FeatAnno.pl onclick url
+                "start"  => $f->{locstart},
+                "end"    => $f->{locstop},
+                "strand" => $f->{locstrand},
+                "type"   => $f->{type},
+                "name"   => ($f->{type} eq 'gene' ? $f->{name} : undef),
+                "wobble" => $wobble_gc
+            };
+        }
+    }
 
     $self->render(json => { "features" => \@results });
+}
+
+sub _is_overlapping {
+    my ($s1, $e1, $s2, $e2) = @_;
+    return ($s1 <= $e2 and $s2 <= $e1);
+}
+
+# mdb added 11/4/13 issue 246 - add wobble shading
+sub _calc_wobble {
+    my ($seq, $start_offset, $stop_offset) = @_;
+    #print STDERR "_calc_wobble: $start_offset-$stop_offset, $seq\n";
+    
+    if ($start_offset < 0) {
+        $start_offset = 0;
+    }
+    if ($stop_offset < 0 or $start_offset == $stop_offset) {
+        return 0;
+    }
+
+    # Calculate GC percent in wobble position
+    my ($count, $total) = (0, 0);
+    for (my $i = $start_offset+2;  $i < $stop_offset; $i += 3) { #for i in xrange(start_offset+2, stop_offset, 3):
+        $total += 1;
+        my $c = uc(substr($seq, $i, 1));
+        if ($c eq 'G' or $c eq 'C') {
+            $count += 1;
+        }
+    }
+
+    return ($count, $total);
 }
 
 1;
