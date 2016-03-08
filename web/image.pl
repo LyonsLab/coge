9e166ed1810f696780b533064199cc117e55b641
mbomhoff <mbomhoff@email.arizona.edu>
2016-03-07 17:27:27 -0700

    Add check for null image

--- web/image.pl
+++ web/image.pl
@@ -7,12 +7,14 @@ use CoGe::Accessory::Web;
 my $form = new CGI;
 my $id = $form->param('id');
 my ( $coge, $user ) = CoGe::Accessory::Web->init;
+print STDERR "image.pl: ", $user->name, "\n";
 if (!$id) {
 	$id	 = $user->image_id;
 }
 exit unless $id;
 
 my $img = $coge->resultset('Image')->find($id);
+exit unless $img;
 
 $| = 1;    # turn off buffering
 print "Cache-Control: public\nContent-type: image/png\n\n", $img->image; # mdb added caching 11/24/15
