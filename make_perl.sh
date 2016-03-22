7632663435c5e2795ad194235546b7dea65b1c76
mbomhoff <mbomhoff@email.arizona.edu>
2016-03-22 10:41:03 -0700

    Undo unnecessary change

--- make_perl.sh
+++ make_perl.sh
@@ -1,2 +1,1 @@
-cd ~/repos/coge/modules; perl Makefile.PL lib=./perl; make install; 
-#cd ./modules; perl Makefile.PL lib=./perl; make install; 
+cd ./modules; perl Makefile.PL lib=./perl; make install; 
