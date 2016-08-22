e7aa3bb42b0db89343c8abb1b8a5bd880c8a9162
sdavey <sdavey@email.arizona.edu>
2016-08-22 14:07:14 -0700

    added role param to search

--- modules/Core/lib/CoGe/Core/Search.pm
+++ modules/Core/lib/CoGe/Core/Search.pm
@@ -17,17 +17,35 @@ BEGIN {
     @EXPORT = qw( search );
 }
 
+sub add_join {
+	my $attributes = shift || {};
+	my $new_join = shift;
+
+	my $join = $attributes->{'join'};
+	if ($join) {
+		if (ref($join) eq 'ARRAY') {
+			push @$join, $new_join;
+		}
+		else {
+			$attributes->{'join'} = [$join, $new_join];
+		}
+	}
+	else {
+		$attributes->{'join'} = { join => $new_join };
+	}
+	$attributes->{'distinct'} = 1;
+	return $attributes;
+}
+
 sub do_search {
-    my ($db, $type, $search, $attributes, $deleted, $restricted, $metadata_key, $metadata_value) = @_;
+    my ($db, $user, $type, $search, $attributes, $deleted, $restricted, $metadata_key, $metadata_value, $role) = @_;
     
     my $and = [];
 	push @$and,  $search if $search;
     push @$and, { 'me.deleted' => $deleted } if defined $deleted;
     push @$and, { 'me.restricted' => $restricted } if defined $restricted;
 	if ($metadata_key || $metadata_value) {
-		$attributes = {} unless $attributes;
-		$attributes->{'distinct'} = 1;
-		$attributes->{'join'} = lc($type) . '_annotations';
+		$attributes = add_join($attributes, lc($type) . '_annotations');
 		if ($metadata_key) {
 			my $dbh = $db->storage->dbh;
 			my @row = $dbh->selectrow_array('SELECT annotation_type_id FROM annotation_type WHERE name=' . $dbh->quote($metadata_key));
@@ -37,7 +55,11 @@ sub do_search {
 			push @$and, { 'annotation' => $metadata_value };
 		}
 	}
-	warn Dumper $and;
+	if ($role && $user) {
+		$attributes = add_join($attributes, 'user_connectors');
+		push @$and, { 'user_connectors.parent_id' => $user->id };		
+		push @$and, { 'user_connectors.role_id' => $role eq 'owner' ? 2 : $role eq 'editor' ? 3 : 4 };		
+	}
 	warn Dumper $attributes;
 	return $db->resultset($type)->search({ -and => $and }, $attributes);
 }
@@ -83,12 +105,12 @@ sub search {
 
     my @idList;
     my @results;
+	my $deleted;
     my $metadata_key;
     my $metadata_value;
-
-	my $type = "none";
 	my $restricted;
-	my $deleted;
+	my $role;
+	my $type = "none";
 
     my @searchArray = parse_line('\s+', 0, $search_term);
     for ( my $i = 0 ; $i < @searchArray ; $i++ ) {
@@ -109,6 +131,10 @@ sub search {
 			}
 			elsif ($splitTerm[0] eq 'restricted') {
 				$restricted = $splitTerm[1];
+				$handled = 1;
+			}
+			elsif ($splitTerm[0] eq 'role') {
+				$role = $splitTerm[1];
 				$handled = 1;
 			}
 			elsif ($splitTerm[0] eq 'type') {
@@ -203,12 +229,12 @@ sub search {
 	# Perform genome search (corresponding to Organism results)
 	if ($type eq 'none' || $type eq 'genome') {
 		my @genome_results;
-		my @genomes = do_search($db, 'Genome', @idList ? { organism_id => { -in => \@idList }} : undef, undef, $deleted, $restricted, $metadata_key, $metadata_value);
+		my @genomes = do_search($db, $user, 'Genome', @idList ? { organism_id => { -in => \@idList }} : undef, undef, $deleted, $restricted, $metadata_key, $metadata_value, $role);
 		push_results(\@genome_results, \@genomes, 'genome', $user, 'has_access_to_genome');
 
 		# Perform direct genome search (by genome ID)
 		if (scalar @searchArray) {
-			my @genomes = do_search($db, 'Genome', { genome_id => { -in => grep(/d+/, @searchArray) }}, undef, $deleted, $restricted, $metadata_key, $metadata_value);
+			my @genomes = do_search($db, $user, 'Genome', { genome_id => { -in => grep(/d+/, @searchArray) }}, undef, $deleted, $restricted, $metadata_key, $metadata_value, $role);
 			push_results(\@genome_results, \@genomes, 'genome', $user, 'has_access_to_genome');
 		}
 		push @results, sort name_cmp @genome_results;
@@ -237,7 +263,7 @@ sub search {
 				]
 			  ];
 		}
-		my @experiments = do_search($db, 'Experiment', scalar @expArray ? \@expArray : undef, { join => { 'genome' => 'organism' } }, $deleted, $restricted, $metadata_key, $metadata_value);
+		my @experiments = do_search($db, $user, 'Experiment', scalar @expArray ? \@expArray : undef, { join => { 'genome' => 'organism' } }, $deleted, $restricted, $metadata_key, $metadata_value, $role);
 		@experiments = sort info_cmp @experiments;
 		push_results(\@results, \@experiments, 'experiment', $user, 'has_access_to_experiment');
 	}
@@ -261,7 +287,7 @@ sub search {
 				]
 			  ];
 		}
-		my @notebooks = do_search($db, 'List', scalar @noteArray ? \@noteArray : undef, undef, $deleted, $restricted, $metadata_key, $metadata_value);
+		my @notebooks = do_search($db, $user, 'List', scalar @noteArray ? \@noteArray : undef, undef, $deleted, $restricted, $metadata_key, $metadata_value, $role);
 		@notebooks = sort { lc($a->name) cmp lc($b->name) } @notebooks;
 		push_results(\@results, \@notebooks, 'notebook', $user, 'has_access_to_list');
 	}
