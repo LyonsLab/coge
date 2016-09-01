92a6f3b7391d3800a1d9984d89497a7bc25cabe2
Stephanie Zawada <szawada@geco.iplantcollaborative.org>
2016-09-01 15:41:05 -0700

    Add commas

--- web/js/coge/utils.js
+++ web/js/coge/utils.js
@@ -142,12 +142,8 @@ var coge = window.coge = (function(ns) {
     		}
     	},
 
-	function numberWithCommas(x) {
-		x = x.toString();
-		var pattern = /(-?\d+)(\d{3})/;
-		while (pattern.test(x))
-			x = x.replace(pattern, "$1,$2");
-		return x;		
+	numberWithCommas: function(x) {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");		
 	}
     };
 
