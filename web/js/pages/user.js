f991d137d90342533fb7c45ad8e2faaf6982ab2a
mbomhoff <mbomhoff@email.arizona.edu>
2016-09-09 11:21:40 -0700

    Refresh notebook search dialog every time it is opened

--- web/js/pages/user.js
+++ web/js/pages/user.js
@@ -1275,11 +1275,8 @@ function comment_job(id, comment) {
 function add_to_notebook_dialog() {
 	var selected = contentPanel.grid.getSelectedItems();
 	if (selected.length) {
-		// Initialize add-to-notebook dialog first time only
-		if (!window.init_search_notebooks) {
-			window.setTimeout(search_notebooks, 1000);
-			window.init_search_notebooks++;
-		}
+		// Initialize add-to-notebook dialog
+		window.setTimeout(search_notebooks, 1000);
 		
 		// Open dialog window
 		$('#add_to_notebook_dialog').dialog({width:500}).dialog('open');
