8e9ca0a4f1ab95a53d4e0af1780d1f31a092211b
mbomhoff <mbomhoff@email.arizona.edu>
2016-09-28 18:01:54 -0700

    Add metadata command

--- scripts/irods.pl
+++ scripts/irods.pl
@@ -1,16 +1,16 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
-use v5.14;
-
+use Switch;
 use Carp qw(croak);
 use Getopt::Long qw(GetOptions);
 use Data::Dumper;
 
 use CoGe::Accessory::IRODS;
-use CoGe::Accessory::TDS:
+use CoGe::Accessory::TDS;
 
-our ($envFile, $command, $metaString, $metaFile, $dest, $src);
+my ($envFile, $command, $metaString, $metaFile, $dest, $src);
 
 GetOptions(
     "cmd|c=s"       => \$command,
@@ -34,30 +34,30 @@ if ($metaString) {
 }
 elsif ($metaFile) {
     croak "The specified metadata file does not exist" unless -r $metaFile;
-    my $md = TDS::read($metaFile);
+    my $md = CoGe::Accessory::TDS::read($metaFile);
     %metadata = %$md;
 }
 
-given($command) {
-    when(/submit/i) { # iput and imeta
+switch ($command) {
+    case /submit/i { # iput and imeta
         croak "The src file was not specified" unless $src;
         croak "The specified src file does not exist" unless -r $src;
         CoGe::Accessory::IRODS::irods_iput($src, $dest);
         CoGe::Accessory::IRODS::irods_imeta($dest, \%metadata) if (keys %metadata);
     }
 
-    when(/fetch/i) { # iget
+    case /fetch/i { # iget
         croak "The src file was not specified" unless $src;
         CoGe::Accessory::IRODS::irods_iget($src, $dest);
         croak "The file $src could not be fetched" unless -r $dest;
     }
     
-    when(/metadata/i) { # imeta
+    case /metadata/i { # imeta
         croak "Medata was not specified" unless (keys %metadata);
         CoGe::Accessory::IRODS::irods_imeta($dest, \%metadata);
     }
 
-    default {
+    else {
         croak "Unknown command: $command";
     }
 }
