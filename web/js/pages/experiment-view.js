530cf62daad7405f08ce997f40b59ff2344c2cda
mbomhoff <mbomhoff@email.arizona.edu>
2016-10-26 10:25:54 -0700

    Fix dialog sizes

--- web/js/pages/experiment-view.js
+++ web/js/pages/experiment-view.js
@@ -22,7 +22,7 @@ function edit_experiment_info() {
         },
         success : function(data) {
             var obj = jQuery.parseJSON(data);
-            $("#experiment_info_edit_box").html(obj.output).dialog('open');
+            $("#experiment_info_edit_box").html(obj.output).dialog('open').dialog({width: '32em'});
         }
     });
 }
@@ -113,7 +113,7 @@ function add_experiment_type () {
                         $("#edit_tag_name").autocomplete('close');
                     }
             });
-            $("#experiment_tag_edit_box").html(data).dialog('open');
+            $("#experiment_tag_edit_box").html(data).dialog('open').dialog({'width': '32em'});
         }
     });
 }
