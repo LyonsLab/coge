b20d311f74d534036bba0cdf0e89b84196cc138b
mbomhoff <mbomhoff@email.arizona.edu>
2016-12-09 16:35:13 -0700

    Add Entrez and SRA modules

---
+++ modules/Accessory/lib/CoGe/Accessory/Entrez.pm
@@ -0,0 +1,115 @@
+package CoGe::Accessory::Entrez;
+
+=head1 NAME
+
+CoGe::Accessory::SRA
+
+=head1 SYNOPSIS
+
+Interface to NCBI-SRA
+
+=head1 DESCRIPTION
+
+Provide simple API for accessing NCBI-SRA via Entrez
+
+=head1 AUTHOR
+
+Matt Bomhoff
+
+=head1 COPYRIGHT
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=head1 SEE ALSO
+
+=cut
+
+use strict;
+use warnings;
+
+use LWP::Simple qw($ua get);
+use XML::Simple qw(XMLin);
+use Data::Dumper;
+
+$ua->timeout(10); # Set LWP timeout to 10 seconds
+
+BEGIN {
+    use vars qw ($VERSION @ISA @EXPORT $BASE_URL $ESEARCH_URL $ESUMMARY_URL $EFETCH_URL $RETMAX );
+    require Exporter;
+
+    $BASE_URL     = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+    $ESEARCH_URL  = $BASE_URL . 'esearch.fcgi';
+    $ESUMMARY_URL = $BASE_URL . 'esummary.fcgi';
+    $EFETCH_URL   = $BASE_URL . 'efetch.fcgi';
+    $RETMAX       = 100_000; # maximum number of search results (Entrez max value)
+
+    $VERSION = 0.1;
+    @ISA     = qw( Exporter );
+    @EXPORT  = qw( esearch esummary efetch );
+}
+
+sub esearch {
+    my ($db, $term) = @_;
+    $db = lc($db);
+
+    my $result = get($ESEARCH_URL . '?db=' . $db . '&term=' . $term . '&retmax=' . $RETMAX);
+    my $record = XMLin($result, ForceArray => ['Id']);
+    #warn Dumper $record;
+    my $id_list = $record->{IdList}->{Id};
+    #warn Dumper $id;
+
+    return $id_list; # arrayref of IDs
+}
+
+sub esummary {
+    my ($db, $id) = @_;
+    $db = lc($db);
+
+    my $response = get($ESUMMARY_URL . '?db=' . $db . '&id=' . $id);
+
+    if ($db eq 'sra') {
+        my %result;
+
+        my $record = XMLin($response, KeyAttr => {Item => 'Name'}, ForceArray => ['Item'], ContentKey => '-content');
+        unless ($record) {
+            return;
+        }
+
+        # Parse name
+        my $exp = '<Exp>' . $record->{DocSum}->{Item}->{ExpXml}->{content} . '</Exp>'; # Kludge for NCBI, see COGE-778
+        if ($exp) {
+            my $record = XMLin($exp);
+            @result{keys %$record} = values %$record;
+        }
+
+        # Parse runs
+        my $runs = '<Runs>' . $record->{DocSum}->{Item}->{Runs}->{content} . '</Runs>'; # Kludge for NCBI, see COGE-778
+        if ($runs) {
+            my $record = XMLin($runs, KeyAttr => {Run => 'acc'});
+            @result{keys %$record} = values %$record;
+        }
+
+        #warn Dumper \%result;
+        return \%result;
+    }
+
+    return $response;
+}
+
+sub efetch {
+    my ($db, $id) = @_;
+    $db = lc($db);
+
+    my $result = get($EFETCH_URL . '?db=' . $db . '&id=' . $id);
+    warn $result;
+
+    if ($db eq 'sra') {
+        my $record = XMLin($result);
+        warn Dumper $record;
+    }
+
+    return $result;
+}
+
+1;
---
+++ modules/Pipelines/lib/CoGe/Builder/Load/SRA.pm
@@ -0,0 +1,184 @@
+package CoGe::Builder::Load::SRA;
+
+use Moose;
+with qw(CoGe::Builder::Buildable);
+
+use Data::Dumper qw(Dumper);
+use File::Spec::Functions qw(catfile catdir);
+use File::Basename qw(basename);
+use Hash::Flatten qw(flatten);
+
+use CoGe::Accessory::Web qw(url_for);
+use CoGe::Accessory::Entrez;
+use CoGe::Core::Storage qw(get_upload_path);
+use CoGe::Builder::CommonTasks;
+
+use constant MAX_SRA_EXPERIMENTS => 2;
+
+sub get_name {
+    my $self = shift;
+    return "Import from SRA"; #TODO add accn(s)
+}
+
+sub get_site_url {
+    my $self = shift;
+    return url_for('LoadExperiment.pl', wid => $self->workflow->id);
+}
+
+sub build {
+    my $self = shift;
+    
+    # Validate inputs
+    my $gid = $self->params->{genome_id} || $self->params->{gid};
+    return unless $gid;
+    my $source_data = $self->params->{source_data};
+    return unless (defined $source_data && @$source_data);
+#    my $metadata = $self->params->{metadata}; # primary metadata is exctracted from SRA record
+#    return unless $metadata;
+    my $additional_metadata = $self->params->{additional_metadata}; # optional
+
+    # Convert from Mojolicious boolean: bless( do{\\(my $o = 1)}, 'Mojo::JSON::_Bool' )
+#    $metadata->{restricted} = $metadata->{restricted} ? 1 : 0;
+
+    # Get genome
+    my $genome = $self->db->resultset('Genome')->find($gid);
+    return unless $genome;
+
+    # Extract SRA accessions and verify only SRA data types present
+    my @accns;
+    foreach (@$source_data) {
+        if (!$_->{type} || lc($_->{type}) ne 'sra') {
+            warn "Data type is undefined or not 'sra'";
+            return;
+        }
+
+        if (!$_->{path}) {
+            warn "Data path is undefined";
+            return;
+        }
+
+        push @accns, $_->{path};
+    }
+
+    # Retrieve IDs and metdata from SRA
+    my $records = sra_retrieve(\@accns);
+
+    if (@$records >= MAX_SRA_EXPERIMENTS) { # limit the max number of SRA experiments a user can load at one time
+        warn "Too many experiments for accessions ", join(', ', @accns);
+        return;
+    }
+
+    #
+    # Build workflow
+    #
+
+    my $wait_file;
+    foreach my $record (@$records) {
+        # Format metadata
+        my ($data, $metadata, $additional_metadata, $read_params) = convert_metadata($record);
+        print STDERR 'SRA: Building workflow for "', $metadata->{name}, '"\n';
+
+        my $expBuilder = CoGe::Builder::Load::Experiment->new({
+            params      => {
+                genome_id           => $gid,
+                metadata            => $metadata,
+                additional_metadata => $additional_metadata,
+                source_data         => $data,
+                read_params         => $read_params
+            },
+            db          => $self->db,
+            user        => $self->user,
+            conf        => $self->conf,
+            workflow    => $self->workflow,
+            staging_dir => $self->staging_dir,
+            result_dr   => $self->result_dir,
+            inputs      => ($wait_file ? [ $wait_file ] : undef),
+            #outputs     => $self->outputs
+        });
+        $expBuilder->build();
+
+        # Add wait task to serialize experiment loads -- #FIXME kludge until this can be generalized and incorporated into Buildable
+#        $expBuilder->add_task_chain_all( # won't work because Experiment.pm doesn't use Buildable::add_job
+#            $self->create_wait()
+#        );
+#        $wait_file = $self->previous_output;
+        my @all_outputs = $self->workflow->get_outputs();
+        my $wait_task = $self->create_wait();
+        push @{$wait_task->{inputs}}, @all_outputs;
+        $self->workflow->add_job($wait_task);
+        $wait_file = $wait_task->{outputs}->[0];
+
+        last; #debug
+    }
+    
+    return 1;
+}
+
+sub sra_retrieve {
+    my $accns = shift;
+
+    my @results;
+    foreach (@$accns) {
+        print STDERR "Querying SRA for $_\n";
+        my $id_list = esearch('sra', $_);
+        return unless ($id_list && @$id_list);
+
+        foreach (@$id_list) { #FIXME speed this up by fetching all in a single query using command-separated list of IDs
+            print STDERR "   fetch summary for $_\n";
+            my $record = esummary('sra', $_);
+            push @results, $record;
+        }
+    }
+
+    return \@results;
+}
+
+sub convert_metadata { #TODO rename
+    my $record = shift;
+    return unless $record;
+
+    # Flatten SRA record metadata
+    my $flattened = flatten($record);
+    $flattened = {} unless $flattened;
+    #warn Dumper $record;
+    #warn Dumper $flattened;
+
+    # Extract primary metadata fields
+    my $metadata = {
+        name       => $flattened->{'Summary.Title'} // '',
+        version    => $flattened->{'Experiment.ver'} // 1,
+        source     => 'NCBI-SRA',
+        restricted => 0, # data is from a public source
+        tags       => [ 'SRA' ]
+    };
+
+    # Format other fields as additional metadata
+    my @additional_metadata = map +{ type => $_, text => $flattened->{$_} }, keys(%$flattened);
+    warn Dumper \@additional_metadata;
+
+    # Extract accession number(s) and determine single-ended or paired-end
+    my (@accns, $read_params);
+    if ($record->{Run}->{acc}) { # single run
+        push @accns, $record->{Run}->{acc};
+    }
+    else { # multiple runs
+        foreach (keys %{$record->{Run}}) {
+            push @accns, $_ if ($_ =~ /^SRR/i);
+        }
+
+        if ($record->{'Library_descriptor'}->{'LIBRARY_LAYOUT'}->{'PAIRED'}) { # paired-end
+            $read_params->{read_type} = 'paired';
+        }
+        # Else default to single-ended
+    }
+
+    # Create data element
+    my @data;
+    foreach (@accns) {
+        push @data, { file_type => 'sra', type => 'sra', path => $_ };
+    }
+
+    return (\@data, $metadata, \@additional_metadata, $read_params);
+}
+
+1;
