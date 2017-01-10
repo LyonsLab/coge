0eda8584e15d2223a99326b9807cffdc2a67ead3
mbomhoff <mbomhoff@email.arizona.edu>
2017-01-10 12:39:01 -0700

    Migrate to recent pipeline refactor

--- modules/Pipelines/lib/CoGe/Builder/Tools/SynMap3D.pm
+++ modules/Pipelines/lib/CoGe/Builder/Tools/SynMap3D.pm
@@ -15,18 +15,25 @@ use JSON qw( encode_json );
 use POSIX;
 
 sub pre_build { # override superclass method
-	my ($self, %params) = @_;
+	my $self = shift;
+
+	# Connect to JEX
+    my $jex = CoGe::JEX::Jex->new( host => $self->conf->{JOBSERVER}, port => $self->conf->{JOBPORT} );
+    unless ($jex) {
+        CoGe::Exception::Generic->throw(message => "Couldn't connect to JEX");
+    }
+    $self->jex($jex);
 
 	# Initialize workflow -- NOTE: init => 0 means that a previous identical workflow will be reused when submitted
-    $self->workflow( $params{jex}->create_workflow(name => $self->get_name, init => 0 ) );
+    $self->workflow( $jex->create_workflow(name => $self->get_name, init => 0 ) );
     return unless $self->workflow;
 
 	# Set site_url attribute
 	my %opts = ( %{ defaults() }, %{ $self->params } );
 	$self->site_url( $opts{tinylink} || get_query_link( $self->conf, $self->db, %opts ) );
-
-    if ($params{requester}) { # request is from internal web page - external API requests will not have a 'requester' field
-        my $page = $params{requester}->{page}; # page name used for logging
+	my $requester = $self->request->requester;
+    if ($requester) { # request is from internal web page - external API requests will not have a 'requester' field
+        my $page = $requester->{page}; # page name used for logging
         $self->page($page) if $page;
     }
 
