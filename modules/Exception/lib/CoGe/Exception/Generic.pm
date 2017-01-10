f89c7d2496295c4ea977e353c90f78b167e716fb
mbomhoff <mbomhoff@email.arizona.edu>
2017-01-10 14:59:54 -0700

    Add optional "details" attribute

--- modules/Exception/lib/CoGe/Exception/Generic.pm
+++ modules/Exception/lib/CoGe/Exception/Generic.pm
@@ -1,12 +1,14 @@
-package CoGe::Exception::Custom;
+package CoGe::Exception::Generic;
 
 use Moose;
 extends 'Throwable::Error';
 
 has message => (is => 'ro', isa => 'Str'); # set in throw()
+has details => (is => 'ro', isa => 'Str'); # set in throw()
 
 sub as_string {
-    shift->message;
+    my $self = shift;
+    $self->message . ($self->details ? ': ' . $self->details : '');
 }
 
 1;
