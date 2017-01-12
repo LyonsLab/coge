14f5e0258a0c578ef834557d61cb3e4ee6c224ef
mbomhoff <mbomhoff@email.arizona.edu>
2017-01-12 13:23:30 -0700

    Resolve merge

--- modules/Exception/lib/CoGe/Exception/Generic.pm
+++ modules/Exception/lib/CoGe/Exception/Generic.pm
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 package CoGe::Exception::Generic;
-=======
-package CoGe::Exception::Custom;
->>>>>>> 64ad5e911b0cc224edca76011b04410d5f95858d
 
 use Moose;
 extends 'Throwable::Error';
 
 has message => (is => 'ro', isa => 'Str'); # set in throw()
-<<<<<<< HEAD
 has details => (is => 'ro', isa => 'Str'); # set in throw()
 
 sub as_string {
@@ -17,11 +12,3 @@ sub as_string {
 }
 
 1;
-=======
-
-sub as_string {
-    shift->message;
-}
-
-1;
->>>>>>> 64ad5e911b0cc224edca76011b04410d5f95858d
