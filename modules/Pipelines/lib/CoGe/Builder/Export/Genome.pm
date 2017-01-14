c8663281851ede2e30d6a8a83738ab98b6959a51
mbomhoff <mbomhoff@email.arizona.edu>
2017-01-13 17:38:41 -0700

    Migrate export pipelines to pipeline refactor

--- modules/Pipelines/lib/CoGe/Builder/Export/Genome.pm
+++ modules/Pipelines/lib/CoGe/Builder/Export/Genome.pm
@@ -13,32 +13,18 @@ sub get_name {
 sub build {
     my $self = shift;
     
-    my $request = { #FIXME better way to do this? See Moose constructors
-        params      => $self->params,
-        db          => $self->db,
-        user        => $self->user,
-        conf        => $self->conf,
-        workflow    => $self->workflow,
-        staging_dir => $self->staging_dir,
-        result_dr   => $self->result_dir,
-        outputs     => $self->outputs
-    };
-
     my $outputs = $self->params->{output_types};
-    my %outputs;
-    %outputs = map { lc($_) => 1 } @$outputs if $outputs;
+    my %outputs = map { lc($_) => 1 } @$outputs if $outputs;
 
     if (!$outputs || $outputs{'fasta'}) {
-        my $fasta = CoGe::Builder::Export::Fasta->new( $request );
+        my $fasta = CoGe::Builder::Export::Fasta->new($self);
         $fasta->build();
     }
 
     if (!$outputs || $outputs{'gff'} || $outputs{'bed'} || $outputs{'tbl'}) {
-        my $gff = CoGe::Builder::Export::Gff->new( $request );
+        my $gff = CoGe::Builder::Export::Gff->new($self);
         $gff->build();
     }
-
-    return 1;
 }
 
 __PACKAGE__->meta->make_immutable;
