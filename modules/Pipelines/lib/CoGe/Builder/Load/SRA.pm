5129e61364e6fc8ba6439368c8432ee7acbfba2b
mbomhoff <mbomhoff@email.arizona.edu>
2017-01-13 17:37:50 -0700

    Migrate SRA retrieval to pipeline refactor

--- modules/Pipelines/lib/CoGe/Builder/Load/SRA.pm
+++ modules/Pipelines/lib/CoGe/Builder/Load/SRA.pm
@@ -5,18 +5,19 @@ extends 'CoGe::Builder::Buildable';
 
 use Data::Dumper qw(Dumper);
 use Hash::Flatten qw(flatten);
+use File::Spec::Functions qw(catdir catfile);
+use String::ShellQuote;
 
 use CoGe::Accessory::Web qw(url_for);
 use CoGe::Accessory::Entrez;
-use CoGe::Core::Storage qw(get_upload_path);
-use CoGe::Builder::CommonTasks;
+use CoGe::Core::Storage qw(get_sra_cache_path);
 
 use constant MAX_SRA_ACCESSIONS  => 10;
 use constant MAX_SRA_EXPERIMENTS => 10;
 
 sub get_name {
     my $self = shift;
-    my @accns = _extract_accns($self->params->{source_data});
+    my @accns = map { $_->{path} } @{$self->params->{source_data}};
     return "Import from SRA: " . join(', ', @accns);
 }
 
@@ -28,77 +29,58 @@ sub get_site_url {
 sub build {
     my $self = shift;
     my %opts = @_;
-#    my $input_files = $opts{data_files}; #FIXME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#
-#    # Extract SRA accessions
-#    my @accns = _extract_accns($source_data);
-#    return unless @accns;
-#
-#    # Retrieve IDs and metdata from SRA
-#    my $records = _sra_retrieve(\@accns);
-#
-#    # Limit the max number of SRA accessions a user can load at one time
-#    if (@$records >= MAX_SRA_ACCESSIONS && !$self->user->is_poweruser) {
-#        CoGe::Exception::Generic->throw(message => 'Too many accessions', details => Dumper \@accns);
-#    }
-#
-#    #
-#    # Build workflow
-#    #
-#
-#    my $wait_file;
-#    foreach my $record (@$records) {
-#        # Format metadata
-#        my ($data, $metadata, $additional_metadata, $read_type) = _convert_metadata($record);
-#        print STDERR 'SRA: Building workflow for "', $metadata->{name}, '"\n';
-#
-#        # Limit the max number of SRA experiments a user can load at one time
-#        if (@$data > MAX_SRA_EXPERIMENTS && !$self->user->is_poweruser) {
-#            warn "Too many experiments for accessions ", join(', ', @accns);
-#            return;
-#        }
-#
-#        # Add load experiment pipeline
-#        $self->params->{metadata} = $metadata;
-#        $self->params->{additional_metadata} = $additional_metadata;
-#        $self->params->{read_params}{read_type} = $read_type;
-#        $self->params->{source_data} = $data;
-#
-#        my $exp = CoGe::Builder::Load::Experiment->new();
-#        $exp->build();
-#
-#        # Add wait task to serialize experiment loads -- #FIXME kludge until this can be generalized and incorporated into Buildable
-#        $exp->add_task_chain_all(
-#            $self->create_wait()
-#        );
-##        my @all_outputs = $self->workflow->get_outputs();
-##        my $wait_task = $self->create_wait();
-##        push @{$wait_task->{inputs}}, @all_outputs;
-##        $self->workflow->add_job($wait_task);
-##        $wait_file = $wait_task->{outputs}->[0];
-#    }
-}
-
-sub _extract_accns {
-    my $source_data = shift;
-
-    # Extract SRA accessions and verify only SRA data types present
-    my @accns;
-    foreach (@$source_data) {
-        if (!$_->{type} || lc($_->{type}) ne 'sra') {
-            warn "Data type is undefined or not 'sra'";
-            return;
-        }
-
-        if (!$_->{path}) {
-            warn "Data path is undefined";
-            return;
-        }
-
-        push @accns, $_->{path};
+    my $toplevel_accns = $opts{ncbi_accns};
+    unless ($toplevel_accns && @$toplevel_accns) {
+        CoGe::Exception::Generic->throw(message => 'Missing inputs');
     }
 
-    return wantarray ? @accns : \@accns;
+    # Retrieve IDs and metdata from SRA
+    my $records = _sra_retrieve($toplevel_accns);
+    #warn Dumper $records;
+
+    # Limit the max number of SRA accessions a user can load at one time
+    if (@$records >= MAX_SRA_ACCESSIONS && !$self->user->is_poweruser) {
+        CoGe::Exception::Generic->throw(message => 'Too many accessions', details => Dumper $toplevel_accns);
+    }
+
+    #
+    # Build workflow
+    #
+
+    my $wait_file;
+    foreach my $record (@$records) {
+        # Extract metadata from SRA record
+        my ($accns, $metadata, $additional_metadata, $read_type) = _extract_metadata($record);
+        print STDERR 'SRA: Building workflow for "', $metadata->{name}, '"\n';
+
+        # Limit the max number of SRA experiments a user can load at one time
+        if (@$accns > MAX_SRA_EXPERIMENTS && !$self->user->is_poweruser) {
+            warn "Too many experiments for accessions ", join(', ', @$accns);
+            return;
+        }
+
+        # Data retrieval is done here instead of Extractor so can be downloaded serially,
+        # that is for one experiment at a time (instead of downloading all files up front).
+        my @fastq;
+        foreach (@$accns) {
+            $self->add_task_chain(
+                $self->fastq_dump($_, $read_type),
+                $wait_file ? [ $wait_file ] : undef
+            );
+            push @fastq, grep { $_ =~ /\.fastq$/ } @{$self->previous_outputs};
+        }
+
+        # Add load experiment pipeline
+        $self->params->{metadata} = $metadata;
+        $self->params->{additional_metadata} = $additional_metadata;
+        $self->params->{read_params}{read_type} = $read_type;
+        CoGe::Builder::Load::Experiment->new($self)->build(data_files => \@fastq);
+
+        # Add wait task to serialize experiment loads -- #FIXME kludge until this can be generalized and incorporated into Buildable
+        ($wait_file) = $self->add_task_chain_all(
+            $self->wait()
+        );
+    }
 }
 
 sub _sra_retrieve {
@@ -118,14 +100,13 @@ sub _sra_retrieve {
     return \@results;
 }
 
-sub _convert_metadata { #TODO rename
+sub _extract_metadata {
     my $record = shift;
     return unless $record;
 
     # Flatten SRA record metadata
     my $flattened = flatten($record);
     $flattened = {} unless $flattened;
-    #warn Dumper $record;
     #warn Dumper $flattened;
 
     # Extract primary metadata fields
@@ -154,19 +135,52 @@ sub _convert_metadata { #TODO rename
     }
 
     # Determine single-ended or paired-end
-    my $read_type;
+    my $read_type = 'single';
     if ($record->{'Library_descriptor'}->{'LIBRARY_LAYOUT'}->{'PAIRED'}) { # paired-end
         $read_type = 'paired';
     }
-    # Else default to single-ended
 
-    # Create data element
-    my @data;
-    foreach (@accns) {
-        push @data, { file_type => 'sra', type => 'sra', path => $_ };
+    return (\@accns, $metadata, \@additional_metadata, $read_type);
+}
+
+sub fastq_dump {
+    my $self = shift;
+    my $accn = shift;
+    my $read_type = shift // 'single';
+
+    my $dest_path = get_sra_cache_path();
+
+    my $cmd = $self->conf->{FASTQ_DUMP} || 'fastq-dump';
+    $cmd .= ' --split-files' if ($read_type eq 'paired');
+
+    my $output_filepath = catfile($dest_path, $accn);
+
+    my (@output_files, @done_files);
+    if ($read_type eq 'paired') {
+        @output_files = (
+            $output_filepath . '_1.fastq',
+            $output_filepath . '_2.fastq'
+        );
+        @done_files = (
+            $output_filepath . '_1.fastq.done',
+            $output_filepath . '_2.fastq.done'
+        );
+    }
+    else {
+        @output_files = ( $output_filepath . '.fastq');
+        @done_files   = ( $output_filepath . '.fastq.done' );
     }
 
-    return (\@data, $metadata, \@additional_metadata, $read_type);
+    return {
+        cmd => "mkdir -p $dest_path && $cmd --outdir $dest_path " . shell_quote($accn) . " && touch " . join(' ', @done_files),
+        args => [],
+        inputs => [],
+        outputs => [
+            @output_files,
+            @done_files
+        ],
+        description => "Fetching $accn from NCBI-SRA"
+    };
 }
 
 __PACKAGE__->meta->make_immutable;
