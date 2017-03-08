16dd5686e06d8df016f6955c2d7c3ca0aea67281
mbomhoff <mbomhoff@email.arizona.edu>
2017-03-08 10:53:52 -0700

    Comment-out unused code

--- modules/Pipelines/lib/CoGe/Builder/Trimming/BBDuk.pm
+++ modules/Pipelines/lib/CoGe/Builder/Trimming/BBDuk.pm
@@ -11,7 +11,7 @@ use CoGe::Exception::Generic;
 use CoGe::Exception::MissingField;
 
 # Settings
-has MAX_MEM_SIZE => (is => 'ro', isa => 'Int', default => 20); # Java max memory size in GB -- value set for TERRA data
+my $JAVA_MAX_MEM = '20g'; # Java max memory size in GB -- value set for TERRA data
 
 sub build {
     my $self = shift;
@@ -67,18 +67,18 @@ sub bbduk {
     my $fasta = shift;
 
     my $trimming_params = $self->params->{trimming_params} // {};
-    my $k         = $trimming_params->{k} // 0;
-    my $mink      = $trimming_params->{mink} // -1;
-    my $hdist     = $trimming_params->{hdist} // 0;
-    my $tpe       = $trimming_params->{tpe} // 'f';
-    my $tbo       = $trimming_params->{tbo} // 'f';
-    my $qtrim     = $trimming_params->{qtrim} // 'f';
-    my $trimq     = $trimming_params->{trimq} // 6;
-    my $minlength = $trimming_params->{minlength} // 10;
+#    my $k         = $trimming_params->{k} // 0;
+#    my $mink      = $trimming_params->{mink} // -1;
+#    my $hdist     = $trimming_params->{hdist} // 0;
+#    my $tpe       = $trimming_params->{tpe} // 'f';
+#    my $tbo       = $trimming_params->{tbo} // 'f';
+#    my $qtrim     = $trimming_params->{qtrim} // 'f';
+#    my $trimq     = $trimming_params->{trimq} // 6;
+#    my $minlength = $trimming_params->{minlength} // 10;
     my $adapters  = $trimming_params->{adapters} // 'none';
 
     my $read_params = $self->params->{read_params} // {};
-    my $encoding = $read_params->{encoding} // 33;
+#    my $encoding = $read_params->{encoding} // 33;
     my $read_type = $read_params->{read_type} // 'single';
 
     my @outputs = map { catfile($self->staging_dir, basename(remove_fastq_ext($_) . '.trimmed.fastq' . to_compressed_ext($_))) } @$fastq;
@@ -91,7 +91,7 @@ sub bbduk {
     my $cmd = join(' ',
         'nice',
         catfile($self->conf->{BBMAP}, 'bbduk2.sh'),
-        '-Xmx'.$self->MAX_MEM_SIZE.'g',
+        qq[-Xmx$JAVA_MAX_MEM],
         qq[in=$fastq->[0]],
         qq[out=$outputs[0]],
         qq[ref=$fasta]
