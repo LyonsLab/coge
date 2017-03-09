a74e179836f64267967e51ab091dc93a2de8213a
mbomhoff <mbomhoff@email.arizona.edu>
2017-03-09 13:39:06 -0700

    Remove commented code

--- modules/Pipelines/lib/CoGe/Builder/Buildable.pm
+++ modules/Pipelines/lib/CoGe/Builder/Buildable.pm
@@ -663,19 +663,9 @@ sub picard_deduplicate {
 
     return {
         cmd => "$cmd MarkDuplicates REMOVE_DUPLICATES=true INPUT=$bam_file METRICS_FILE=$bam_file.metrics OUTPUT=$output_file.tmp && mv $output_file.tmp $output_file",
-        args => [
-#            ['MarkDuplicates', '', 0],
-#            ['REMOVE_DUPLICATES=true', '', 0],
-#            ["INPUT=$bam_file", '', 0],
-#            ["METRICS_FILE=$bam_file.metrics", '', 0],
-#            ["OUTPUT=$output_file", '', 0],
-        ],
-        inputs => [
-            $bam_file
-        ],
-        outputs => [
-            $output_file
-        ],
+        args => [],
+        inputs => [ $bam_file ],
+        outputs => [ $output_file ],
         description => "Deduplicating BAM file"
     };
 }
