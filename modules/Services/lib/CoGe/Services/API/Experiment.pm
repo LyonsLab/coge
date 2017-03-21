aa88f5a32a746cd3d9e69d2cf35eecce0215880f
sdavey <sdavey@email.arizona.edu>
2017-03-21 15:50:40 -0700

    Moved storage to bisque directory instead of bisque_data. Added API registration with BisQue instead of waiting for IRODs metadata to be updated. Added busy animation when deleting an annotation. Fixed bug with uploading images for experiments.

--- modules/Services/lib/CoGe/Services/API/Experiment.pm
+++ modules/Services/lib/CoGe/Services/API/Experiment.pm
@@ -167,6 +167,7 @@ sub add_annotation {
         db => $db,
         filename => $self->param('filename'),
         group_name => $self->param('group_name'),
+        image => $self->param('image'),
         link => $self->param('link'),
         target_id => int($self->stash('id')),
         target_type => 'experiment',
