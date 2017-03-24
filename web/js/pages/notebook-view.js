cc832abd8cf31a3cd284996feada9cbb939e011e
mbomhoff <mbomhoff@email.arizona.edu>
2017-03-23 17:25:13 -0700

    Add "Tools" menu to NotebookView for SNP merge feature

--- web/js/pages/notebook-view.js
+++ web/js/pages/notebook-view.js
@@ -332,8 +332,58 @@ function toggle_favorite(img) {
 	});
 }
 
-function snp_merge() {
+function snp_merge(method) {
+        var experiments = contents.grid.dataTable.api().rows().data() //TODO add getRows() to DataGrid
+            .filter(function(item) { return (item.data_type === 'polymorphism') })
+            .map(function(item) { return item.id })
+            .toArray();
 
+        if (typeof experiments === 'undefined' || experiments.length == 0) {
+            alert("No SNP experiments exist in this notebook");
+            return;
+        }
+
+		coge.progress.begin({
+			title: "Merging " + method + " SNP experiments ...",
+			width: '60%',
+	    	height: '50%'
+		});
+
+		// Build request
+		var request = {
+			type: 'merge_snps',
+			requester: {
+				page: PAGE_NAME,
+				url: PAGE_NAME + "?nid=" + NOTEBOOK_ID
+			},
+			parameters: {
+			    method: method,
+				experiments: experiments,
+				metadata: {
+				    name: "test" // FIXME
+				}
+			}
+		};
+
+		// Submit request
+		coge.services.submit_job(request)
+			.done(function(response) {
+		  		if (!response) {
+		  			coge.progress.failed("Error: empty response from server");
+		  			return;
+		  		}
+		  		else if (!response.success || !response.id) {
+		  			coge.progress.failed("Error: failed to start workflow");
+		  			return;
+		  		}
+
+		        // Start status update
+		  		window.history.pushState({}, "Title", PAGE_NAME + "?nid=" + NOTEBOOK_ID + "&wid=" + response.id); // Add workflow id to browser URL
+		  		coge.progress.update(response.id, response.site_url);
+		    })
+		    .fail(function(jqXHR, textStatus, errorThrown) {
+		    	coge.progress.failed("Couldn't talk to the server: " + textStatus + ': ' + errorThrown);
+		    });
 }
 
 class SendToMenu {
