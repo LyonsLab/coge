2214acf0a34566f483af7da8c016ab55cd78a0fe
sdavey <sdavey@email.arizona.edu>
2017-07-12 17:08:45 -0700

    gc calc

--- bin/gcat/src/main.rs
+++ bin/gcat/src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     let mut stderr = std::io::stderr();
 
     if args.len() < 3 {
-        writeln!(&mut stderr, "not enough arguments");
+        writeln!(&mut stderr, "not enough arguments").unwrap();
         process::exit(1);
     }
 
     let fasta_path = &args[1];
-    let feat_type = &args[2];
+//    let feat_type = &args[2];
 
     let fasta = read_fasta(fasta_path).unwrap();
 
@@ -23,7 +23,23 @@ fn main() {
 }
 
 fn calc_percentages(fasta: &str) {
-
+    let bytes = fasta.as_bytes();
+    let mut gc = 0;
+    let mut at = 0;
+    let mut nx = 0;
+    let total = bytes.len();
+    print!("{} ", total);
+    for i in 0.. total {
+        let c = bytes[i];
+        if c == b'G' || c == b'g' || c == b'C' || c == b'c' {
+            gc += 1;
+        } else if c == b'A' || c == b'a' || c == b'T' || c == b't' {
+            at += 1;
+        } else if c == b'N' || c == b'n' || c == b'X' || c == b'x' {
+            nx += 1;
+        }
+    }
+    print!("{{\"gc\": {:.4}, \"at\": {:.4}, \"nx\": {:.4}}}", gc as f64 / total as f64 * 100.0, at  as f64/ total as f64 * 100.0, nx as f64 / total as f64 * 100.0);
 }
 
 // fn get_feature_indices() {
