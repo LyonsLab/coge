0471c69a02a6d60f7325f15edb9fd5cc15cf3626
Eric Lyons <elyons.uoa@gmail.com>
2019-09-12 16:37:41 -0700

    perl modules need to be installed in web and system level

--- make_perl.sh
+++ make_perl.sh
@@ -1,1 +1,2 @@
-cd ./modules; perl Makefile.PL lib=./perl; make install; 
+cd ./modules; perl Makefile.PL lib=./perl; make install; perl Makefile.PL lib=/usr/local/lib/perl/5.18.2/; make install;
+ 
