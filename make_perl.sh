3cb9bfcc5f88a3f4bd3fc9aff900a232b16a93c0
Eric Lyons <lyonsden@gmail.com>
2021-12-21 12:56:00 -0700

    updated lib paths for installing coge modules to use local directories only

--- make_perl.sh
+++ make_perl.sh
@@ -1,2 +1,4 @@
-cd ./modules; perl Makefile.PL lib=./perl; make install; perl Makefile.PL lib=/usr/local/lib/perl/5.18.2/; make install;
+#cd ./modules; perl Makefile.PL lib=./perl; make install; perl Makefile.PL lib=/usr/local/lib/perl/5.18.2/; make install;
+#cd ./modules; perl Makefile.PL lib=./perl; make install; 
+cd ./modules; perl Makefile.PL INSTALL_BASE=./perl; make install; 
  
